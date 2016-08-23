904dbdcd09a20c92af358cef63213e2477ec123d
Vivek Singh <petmongrels@gmail.com>
2016-08-23 14:40:35 +0530

    extracted into modules

--- fs/config/Sample without control flow.js
+++
@@ -1,8 +0,0 @@
-var getDecision = function (questionnaireAnswers) {
-    var decision = {};
-    decision.name = "Treatment";
-    decision.code = "ABC001";
-    decision.value = "The patient should be referred to the hospital immediately as he may having tuberculosis";
-    decision.alert = "ALERT MESSAGE";
-    return [decision];
-};
