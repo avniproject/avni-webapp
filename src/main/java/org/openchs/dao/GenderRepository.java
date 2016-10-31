9df3994093727a596de2192e580a7dd82a4fe7db
Vivek Singh <petmongrels@gmail.com>
2016-11-01 00:53:47 +0530

    Custom repository-rest-method to get records newer than lastmodifieddatetime

--- src/main/java/org/openchs/dao/GenderRepository.java
+++ src/main/java/org/openchs/dao/GenderRepository.java
@@ -1,8 +1,14 @@
 package org.openchs.dao;
 
+import org.joda.time.DateTime;
 import org.openchs.domain.Gender;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import javax.transaction.Transactional;
@@ -11,4 +17,6 @@ import javax.transaction.Transactional;
 @Repository
 @RepositoryRestResource(collectionResourceRel = "gender", path = "gender")
 public interface GenderRepository extends PagingAndSortingRepository<Gender, Long> {
+    @RestResource(path = "lastModified", rel = "lastModified")
+    Page<Gender> findByLastModifiedDateTimeGreaterThanEqual(@Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime lastModifiedDateTime, Pageable pageable);
 }
