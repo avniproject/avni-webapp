b9ea8436c111adcf4c4f3c5e9cfc9ee1be5af7f1
Arjun <arjunk@thoughtworks.com>
2016-12-20 13:02:23 +0530

    Arjun : adding some more test data

--- src/test/resources/test-data.sql
+++ src/test/resources/test-data.sql
@@ -26,14 +26,15 @@ VALUES ('Sample Encounter Type', '3a1535d0-81fd-48fc-85b5-dc9da81064a3', 1, 1, 1
 
 ALTER SEQUENCE program_id_seq RESTART WITH 1;
 INSERT INTO program (name, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
-VALUES ('Sample Program', 'db62a322-0ec2-4bb0-ac24-296dc7216c9a', 1, 1, 1, current_timestamp, current_timestamp);
+VALUES ('Diabetes', 'db62a322-0ec2-4bb0-ac24-296dc7216c9a', 1, 1, 1, current_timestamp, current_timestamp);
 
 ALTER SEQUENCE address_level_id_seq RESTART WITH 1;
 INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time) VALUES ('Nijhma', 1, 'ae35fe6d-910e-47bd-a0c7-0c10182a4085', 1, 1, 1, current_timestamp, current_timestamp);
 INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time) VALUES ('Naya Gaon', 1, 'a62d5ff9-4480-44f8-ab9f-9fe12e2e1a91', 1, 1, 1, current_timestamp, current_timestamp);
 
 ALTER SEQUENCE individual_id_seq RESTART WITH 1;
-INSERT INTO individual (uuid, address_id, catchment_id, version, date_of_birth, date_of_birth_estimated, name, gender_id, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time) VALUES ('4378dce3-247e-4393-8dd5-032c6eb0a655', 1, 1, 1, current_timestamp, FALSE, 'Prabhu', 2, 1, 1, current_timestamp, current_timestamp);
+INSERT INTO individual (uuid, address_id, catchment_id, version, date_of_birth, date_of_birth_estimated, name, gender_id, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('4378dce3-247e-4393-8dd5-032c6eb0a655', 1, 1, 1, current_timestamp, FALSE, 'Prabhu', 2, 1, 1, current_timestamp, current_timestamp);
 
 ALTER SEQUENCE program_enrolment_id_seq RESTART WITH 1;
 INSERT INTO program_enrolment (individual_id, program_id, enrolment_date_time, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
@@ -46,3 +47,18 @@ VALUES (1, 1, '[{"conceptUUID": "95c4b174-6ce6-4d9a-b223-1f9000b60006", "value":
 ALTER SEQUENCE encounter_id_seq RESTART WITH 1;
 INSERT INTO encounter (individual_id, encounter_type_id, observations, encounter_date_time, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
 VALUES (1, 1, '[{"conceptUUID": "95c4b174-6ce6-4d9a-b223-1f9000b60006", "value": 98.9}]'::JSONB, current_timestamp, '63a7d615-b965-4830-9dd2-e8f533d9a4e9', 1, 1, 1, current_timestamp, current_timestamp);
+
+INSERT INTO program (name, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES ('Nutrition', 'db62a322-0ec2-4bb0-ac24-296dc7216c9a', 1, 1, 1, current_timestamp, current_timestamp);
+
+insert into individual (address_id, catchment_id, date_of_birth, date_of_birth_estimated, name, gender_id, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1,1,'1950-09-17',FALSE, 'Ramesh Kumar', 2, '8d3d49af-f776-4cca-8413-ee571d9042fd', 1, 1, 1, current_timestamp, current_timestamp);
+
+INSERT INTO program_enrolment (individual_id, program_id, enrolment_date_time, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 1, current_timestamp, '0ae41288-78c5-4ed4-af60-68d4ad2af1d0', 1, 1, 1, current_timestamp, current_timestamp);
+
+INSERT INTO program_enrolment (individual_id, program_id, enrolment_date_time, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (2, 2, current_timestamp, '529aa9ed-46bc-4530-9768-6ec941c0e2e0', 1, 1, 1, current_timestamp, current_timestamp);
+
+insert into individual (address_id, catchment_id, date_of_birth, date_of_birth_estimated, name, gender_id, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
+VALUES (1,1,'1955-01-05',FALSE, 'Ram Kumari', 1, 'c415ef96-8ff9-4cbb-8407-e7618c90a055', 1, 1, 1, current_timestamp, current_timestamp);
