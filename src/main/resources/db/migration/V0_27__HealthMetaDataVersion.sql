a183ab5ca202eeffa7b1d33ad3c87acdbbb63062
Vivek Singh <petmongrels@gmail.com>
2017-04-06 00:44:35 +0530

    introduced a separate migration table to manage running of scripts

---
+++ src/main/resources/db/migration/V0_27__HealthMetaDataVersion.sql
@@ -0,0 +1,4 @@
+CREATE TABLE health_metatadata_version (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) UNIQUE NOT NULL
+);
---
+++ src/main/resources/db/migration/V0_28__RenameSpellingMistakeInTableName.sql
@@ -0,0 +1,1 @@
+ALTER TABLE openchs.health_metatadata_version RENAME TO health_metadata_version;
