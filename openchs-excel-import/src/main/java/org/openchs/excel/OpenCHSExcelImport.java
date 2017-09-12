ecc5604a775b07d98cdc62c12c9e6f489f588f88
mihirk <mihir.khatwani@gmail.com>
2017-09-12 08:48:20 +0530

    Adding out to gitignore, bringing back the Excel Import App

--- openchs-excel-import/src/main/java/org/openchs/excel/OpenCHSExcelImport.java
+++ openchs-excel-import/src/main/java/org/openchs/excel/OpenCHSExcelImport.java
@@ -5,9 +5,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+<<<<<<< 25d6abb9f82f0744993e7ea0585ffcba1e5990a0
 @ComponentScan(basePackages={"org.openchs"})
+=======
+@ComponentScan({"org.openchs"})
+>>>>>>> Adding out to gitignore, bringing back the Excel Import App
 public class OpenCHSExcelImport {
     public static void main(String[] args) {
         SpringApplication.run(OpenCHSExcelImport.class, args).close();
     }
+<<<<<<< 25d6abb9f82f0744993e7ea0585ffcba1e5990a0
 }
+=======
+
+}
+>>>>>>> Adding out to gitignore, bringing back the Excel Import App
