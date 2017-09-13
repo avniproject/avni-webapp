8460f22efc89cee67cd84a93c1f4442d1e5b3eb0
mihirk <mihir.khatwani@gmail.com>
2017-09-14 04:38:32 +0530

    fixing rpm making mistakes

--- infra/rpm/scripts/postInstall.sh
+++ infra/rpm/scripts/postInstall.sh
@@ -16,7 +16,7 @@ ln -s /opt/openchs/config /etc/openchs
 ln -s /opt/openchs/scripts/openchs /etc/init.d/openchs
 ln -s /opt/openchs/run /var/run/openchs
 ln -s /opt/openchs/log /var/log/openchs
-
+chmod a+x /opt/openchs/scripts/openchs
 chkconfig openchs on
 
 chown -R openchs:openchs /opt/openchs
