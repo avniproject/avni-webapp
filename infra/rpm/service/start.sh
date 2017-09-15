fe002f6eaaff94a94f1ee2d0e5caa71bee47f262
mihirk <mihir.khatwani@gmail.com>
2017-09-15 07:51:17 +0530

    Fixing the path to the jar file

--- infra/rpm/service/start.sh
+++ infra/rpm/service/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java $OPENCHS_SERVER_OPTS -jar /opt/openchs/lib/openchs.jar >> /var/log/openchs/openchs.log 2>&1 &
+nohup java $OPENCHS_SERVER_OPTS -jar /opt/openchs/bin/openchs.jar >> /var/log/openchs/openchs.log 2>&1 &
 echo $! > /var/run/openchs/openchs.pid
