4b882cc1a0ec0349f10fd0a8b68f96deb1076604
mihirk <mihir.khatwani@gmail.com>
2017-12-08 11:50:33 +0530

    Fixing previous tests and adding new ones

--- openchs-server-api/src/test/java/org/openchs/web/request/application/FormElementContractValidationTest.java
+++ openchs-server-api/src/test/java/org/openchs/web/request/application/FormElementContractValidationTest.java
@@ -2,10 +2,7 @@ package org.openchs.web.request.application;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.openchs.application.KeyType;
-import org.openchs.application.KeyValue;
 import org.openchs.application.KeyValues;
-import org.openchs.application.ValueType;
 import org.openchs.domain.ConceptDataType;
 import org.openchs.web.request.ConceptContract;
 
@@ -27,28 +24,27 @@ public class FormElementContractValidationTest {
     }
 
     @Test
-    public void validate_form_element_contract_with_select_specified() {
-        keyValues.add(new KeyValue(KeyType.Select, ValueType.Multi.toString()));
-        assertEquals(false, formElementContract.validate().isFailure());
-    }
-
-    @Test
-    public void validate_form_element_contract_without_select_specified() {
+    public void validate_form_element_contract_with_type_not_specified() {
         assertEquals(true, formElementContract.validate().isFailure());
     }
 
     @Test
     public void validate_form_element_cannot_contain_concept_when_conceptUUID_present() {
-        keyValues.add(new KeyValue(KeyType.Select, ValueType.Multi.toString()));
         formElementContract.setConceptUUID("someConceptUUID");
         assertEquals(true, formElementContract.validate().isFailure());
     }
 
     @Test
     public void validate_form_element_cannot_contain_empty_concept_and_conceptUUID() {
-        keyValues.add(new KeyValue(KeyType.Select, ValueType.Multi.toString()));
         formElementContract.setConceptUUID(null);
         formElementContract.setConcept(null);
         assertEquals(true, formElementContract.validate().isFailure());
     }
+
+    @Test
+    public void validate_form_element_contract_with_type_specified() {
+        formElementContract.setType("SingleSelect");
+        assertEquals(false, formElementContract.validate().isFailure());
+    }
+
 }
