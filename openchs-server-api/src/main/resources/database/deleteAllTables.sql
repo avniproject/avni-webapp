0a3f9ec1c03c2ddc93023b4034f11ec9555c2d0a
mihirk <mihir.khatwani@gmail.com>
2018-01-25 11:28:34 +0530

    Adding new views and tables to deleteAllTables.sql

--- openchs-server-api/src/main/resources/database/deleteAllTables.sql
+++ openchs-server-api/src/main/resources/database/deleteAllTables.sql
@@ -1,6 +1,10 @@
+
 DROP VIEW IF EXISTS mother_program_encounters;
 DROP VIEW IF EXISTS mother_program_enrolments;
 DROP VIEW IF EXISTS checklist_items;
+DROP VIEW IF EXISTS adolescents;
+DROP VIEW IF EXISTS adolescent_visit;
+DROP VIEW IF EXISTS adolescent_visit_summary;
 
 DROP FUNCTION IF EXISTS coded_obs( ANYELEMENT, TEXT );
 DROP FUNCTION IF EXISTS coded_obs_exists( ANYELEMENT, TEXT );
@@ -9,6 +13,23 @@ DROP FUNCTION IF EXISTS date_obs( ANYELEMENT, TEXT );
 DROP FUNCTION IF EXISTS is_overdue_visit( PROGRAM_ENCOUNTER );
 DROP FUNCTION IF EXISTS numeric_obs( ANYELEMENT, TEXT );
 DROP FUNCTION IF EXISTS text_obs( ANYELEMENT, TEXT );
+DROP FUNCTION IF EXISTS coded_obs_contains(ANYELEMENT, TEXT, TEXT);
+DROP FUNCTION IF EXISTS one_of_coded_obs_contains(ANYELEMENT, TEXT[], TEXT);
+DROP FUNCTION IF EXISTS in_one_entity_coded_obs_contains(program_enrolment, program_encounter, TEXT, TEXT);
+DROP FUNCTION IF EXISTS one_of_coded_obs_exists(ANYELEMENT, TEXT[]);
+DROP FUNCTION IF EXISTS has_dropped_out(program_enrolment, program_encounter);
+DROP FUNCTION IF EXISTS has_problem(JSONB);
+DROP FUNCTION IF EXISTS is_counselled(JSONB);
+DROP FUNCTION IF EXISTS has_dropped_out(JSONB, JSONB);
+DROP FUNCTION IF EXISTS numeric_obs(JSONB, TEXT);
+DROP FUNCTION IF EXISTS coded_obs(JSONB, TEXT);
+DROP FUNCTION IF EXISTS coded_obs_exists(JSONB, TEXT);
+DROP FUNCTION IF EXISTS coded_obs_contains(ANYELEMENT, TEXT, TEXT);
+DROP FUNCTION IF EXISTS one_of_coded_obs_contains(JSONB, TEXT[], TEXT);
+DROP FUNCTION IF EXISTS in_one_entity_coded_obs_contains(JSONB, JSONB, TEXT, TEXT);
+DROP FUNCTION IF EXISTS one_of_coded_obs_exists(ANYELEMENT, TEXT[]);
+DROP FUNCTION IF EXISTS has_problem(program_encounter);
+DROP FUNCTION IF EXISTS is_counselled( program_encounter );
 
 DROP TABLE IF EXISTS program_encounter;
 DROP TABLE IF EXISTS encounter;
