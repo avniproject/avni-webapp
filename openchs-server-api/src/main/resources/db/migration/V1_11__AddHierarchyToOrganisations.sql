088f2b9cdf13847a089fff36612446e30aea9d1e
Vinay Venu <ijkvinay@gmail.com>
2018-04-02 16:58:53 +0530

    Fix order of migrations to take care of stuff in production

--- openchs-server-api/src/main/resources/db/migration/V1_11__AddHierarchyToOrganisations.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE organisation
-  ADD COLUMN parent_organisation_id INTEGER REFERENCES organisation (id);
-
-UPDATE organisation
-SET parent_organisation_id = (SELECT id
-                              FROM organisation
-                              WHERE name = 'OpenCHS')
-WHERE name <> 'OpenCHS';
