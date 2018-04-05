7a0448656208fbbf03271d217b370de5f7c3723e
mihirk <mihir.khatwani@gmail.com>
2018-04-05 13:23:34 +0530

    Fixing application properties and also adding parent org to demo

--- make-scripts/create_demo_organisation.sql
+++ make-scripts/create_demo_organisation.sql
@@ -1,4 +1,6 @@
-CREATE ROLE demo NOINHERIT PASSWORD 'password';
+CREATE ROLE demo
+  NOINHERIT
+  PASSWORD 'password';
 
 GRANT demo TO openchs;
 
@@ -6,5 +8,5 @@ GRANT ALL ON ALL TABLES IN SCHEMA public TO demo;
 GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO demo;
 GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO demo;
 
-INSERT INTO organisation (name, db_user, uuid)
-VALUES ('demo', 'demo', '4dafc869-2507-4946-9365-3a82c77fa122');
+INSERT INTO organisation (name, db_user, uuid, parent_organisation_id)
+VALUES ('demo', 'demo', '4dafc869-2507-4946-9365-3a82c77fa122', 1);
