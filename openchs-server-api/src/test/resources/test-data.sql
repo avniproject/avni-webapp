0b7504b73a1e704faabba5a19c7eb9a2527e9055
Charles John <charlesj@gmail.com>
2018-04-30 15:34:47 +0530

    Fixed tests failing because of catchment_address_mapping change

--- openchs-server-api/src/test/resources/test-data.sql
+++ openchs-server-api/src/test/resources/test-data.sql
@@ -171,8 +171,8 @@ VALUES ('Nijhma', 1, 'ae35fe6d-910e-47bd-a0c7-0c10182a4085', 1, 1, 1, current_ti
 INSERT INTO address_level (title, level, uuid, version, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
 VALUES ('Naya Gaon', 1, 'a62d5ff9-4480-44f8-ab9f-9fe12e2e1a91', 1, 1, 1, current_timestamp, current_timestamp);
 
-INSERT INTO catchment_address_mapping (catchment_id, addresslevel_id, organisation_id)
-VALUES (1, 1, 1);
+INSERT INTO catchment_address_mapping (catchment_id, addresslevel_id)
+VALUES (1, 1);
 
 INSERT INTO individual (uuid, address_id, version, date_of_birth, date_of_birth_verified, first_name, last_name, gender_id, created_by_id, last_modified_by_id, created_date_time, last_modified_date_time)
 VALUES ('4378dce3-247e-4393-8dd5-032c6eb0a655', 1, 1, current_timestamp, FALSE, 'Prabhu', 'Kumar', 2, 1, 1,
