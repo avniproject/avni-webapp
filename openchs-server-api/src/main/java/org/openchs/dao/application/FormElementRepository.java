aec9e2ec6f0c95b754eb653663f6419c0e1be7c8
Vinay Venu <ijkvinay@gmail.com>
2018-05-06 17:13:22 +0530

    Put back method removed by mistake

--- openchs-server-api/src/main/java/org/openchs/dao/application/FormElementRepository.java
+++ openchs-server-api/src/main/java/org/openchs/dao/application/FormElementRepository.java
@@ -1,11 +1,16 @@
 package org.openchs.dao.application;
 
+import org.joda.time.DateTime;
 import org.openchs.application.FormElement;
 import org.openchs.dao.ReferenceDataRepository;
 import org.openchs.domain.Concept;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
@@ -14,5 +19,7 @@ import org.springframework.stereotype.Repository;
 @PreAuthorize(value = "hasAnyAuthority('user', 'admin')")
 public interface FormElementRepository extends PagingAndSortingRepository<FormElement, Long>, ReferenceDataRepository<FormElement> {
     @RestResource(path = "lastModified", rel = "lastModified")
+    Page<FormElement> findByAuditLastModifiedDateTimeGreaterThanAndNonApplicableOrganisationsIsNullOrderByAuditLastModifiedDateTimeAscIdAsc(@Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime lastModifiedDateTime, Pageable pageable);
+
     FormElement findFirstByConcept(Concept concept);
 }
