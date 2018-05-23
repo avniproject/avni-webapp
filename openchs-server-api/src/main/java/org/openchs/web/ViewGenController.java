0b185c17be8f9e170c6d6f7562fb1150bf91495b
mihirk <mihir.khatwani@gmail.com>
2018-05-23 09:56:13 +0530

    Adding auth to the viewgen controller

--- openchs-server-api/src/main/java/org/openchs/web/ViewGenController.java
+++ openchs-server-api/src/main/java/org/openchs/web/ViewGenController.java
@@ -2,6 +2,7 @@ package org.openchs.web;
 
 import org.openchs.reporting.SqlGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +18,7 @@ public class ViewGenController {
     SqlGenerationService sqlGenerationService;
 
     @RequestMapping(value = "/query/{programName}", method = RequestMethod.GET)
+    @PreAuthorize(value = "hasAnyAuthority('user', 'admin', 'organisation_admin')")
     public String query(@PathVariable("programName") String programName) throws IOException, URISyntaxException {
         return sqlGenerationService.getSqlFor(StringUtils.capitalize(programName.toLowerCase()));
     }
