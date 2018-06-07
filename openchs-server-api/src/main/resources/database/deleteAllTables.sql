89fda19d25ca480a184c7253b607b5cd0e75b824
mihirk <mihir.khatwani@gmail.com>
2018-06-07 14:50:37 +0530

    Adding other tables to delete all tables

--- openchs-server-api/src/main/resources/database/deleteAllTables.sql
+++ openchs-server-api/src/main/resources/database/deleteAllTables.sql
@@ -58,6 +58,8 @@ DROP TABLE IF EXISTS non_applicable_form_element CASCADE ;
 DROP TABLE IF EXISTS users CASCADE ;
 DROP TABLE IF EXISTS individual_relation_master CASCADE ;
 DROP TABLE IF EXISTS individual_relative CASCADE ;
+DROP TABLE IF EXISTS program_organisation_config CASCADE ;
+DROP TABLE IF EXISTS audit CASCADE ;
 DROP TABLE IF EXISTS organisation CASCADE ;
 DROP TABLE IF EXISTS schema_version CASCADE ;
 DROP TABLE IF EXISTS flyway_schema_history CASCADE ;
