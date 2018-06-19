4b149e80a31a4b0462558f8bca9b8037fb9457f8
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-06-19 10:49:36 +0530

    Revert "656 | When moving a concept from Impl. to Health-modules update the concept.organisation to OpenCHS"
    This reverts commit 9eaa81a97a60ab126a1ec48161de7c00a0b8cf52.

--- openchs-server-api/src/main/java/org/openchs/framework/hibernate/UpdateOrganisationHibernateInterceptor.java
+++ openchs-server-api/src/main/java/org/openchs/framework/hibernate/UpdateOrganisationHibernateInterceptor.java
@@ -13,12 +13,7 @@ import java.io.Serializable;
 public class UpdateOrganisationHibernateInterceptor extends EmptyInterceptor {
     @Override
     public boolean onSave(Object entity, Serializable id, Object[] state, String[] propertyNames, Type[] types) throws CallbackException {
-        return updateOrganisationIdIfNeeded(entity, state, propertyNames) != null;
-    }
-
-    @Override
-    public int[] findDirty(Object entity, Serializable id, Object[] currentState, Object[] previousState, String[] propertyNames, Type[] types) {
-        return updateOrganisationIdIfNeeded(entity, currentState, propertyNames);
+        return updateOrganisationId(entity, state, propertyNames);
     }
 
     @Override
@@ -30,7 +25,7 @@ public class UpdateOrganisationHibernateInterceptor extends EmptyInterceptor {
             audit.setLastModifiedDateTime(new DateTime());
             somethingChanged = true;
         }
-        return somethingChanged;
+        return updateOrganisationId(entity, currentState, propertyNames) || somethingChanged;
     }
 
     private int getIndexOf(String[] propertyNames, String propertyName) {
@@ -40,18 +35,15 @@ public class UpdateOrganisationHibernateInterceptor extends EmptyInterceptor {
         return -1;
     }
 
-    private int[] updateOrganisationIdIfNeeded(Object entity, Object[] currentState, String[] propertyNames) {
+    private boolean updateOrganisationId(Object entity, Object[] currentState, String[] propertyNames) {
         if (entity instanceof OrganisationAwareEntity) {
             int organisationIdIndex = getIndexOf(propertyNames, "organisationId");
             Long organisationId = UserContextHolder.getUserContext().getOrganisation().getId();
-            if (!organisationId.equals(currentState[organisationIdIndex])) {
+            if (currentState[organisationIdIndex] == null) {
                 currentState[organisationIdIndex] = organisationId;
-                return new int[]{organisationIdIndex};
+                return true;
             }
         }
-        //used by onFlushDirty
-        //this is to keep the default behaviour when we don't want to explicitly mark entity as dirty
-        //null - use Hibernate's default dirty-checking algorithm
-        return null;
+        return false;
     }
 }
