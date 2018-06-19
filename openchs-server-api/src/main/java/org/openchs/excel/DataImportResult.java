2d2ec29d47e64d16cbbb0fdff358c975e4e053fb
Charles John <charlesj@gmail.com>
2018-06-19 17:27:10 +0530

    Added a count to importer errors

--- openchs-server-api/src/main/java/org/openchs/excel/DataImportResult.java
+++ openchs-server-api/src/main/java/org/openchs/excel/DataImportResult.java
@@ -1,28 +1,32 @@
 package org.openchs.excel;
 
+import org.openchs.util.O;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.util.StringUtils;
 
 import java.util.*;
 
 public class DataImportResult {
-    private Set<DataImportError> uniqueErrors = new HashSet<>();
+    private Map<DataImportError, Integer> uniqueErrors = new HashMap<>();
     private List<DataImportError> allErrors = new ArrayList<>();
 
     private static Logger logger = LoggerFactory.getLogger(DataImportResult.class);
 
     public DataImportError exceptionHappened(Map<String, String> info, Exception exception) {
         DataImportError error = new DataImportError(exception, info);
-        uniqueErrors.add(error);
+        Integer currentErrorCount = uniqueErrors.get(error);
+        uniqueErrors.put(error, (currentErrorCount == null ? 0 : currentErrorCount) + 1);
         allErrors.add(error);
         return error;
     }
 
     public void report() {
         logger.info(String.format("FAILED ROWS: %d; UNIQUE ERRORS: %d", allErrors.size(), uniqueErrors.size()));
-        uniqueErrors.forEach(e -> {
-            logger.error(e.toString());
-            logger.error("Exception", e.getException());
+        uniqueErrors.forEach((key, value) -> {
+            logger.error(key.toString());
+            logger.error("Error Count: " + value.toString());
+            logger.error("Exception", key.getException());
         });
     }
 }
