490812d78210621840072f8da0bd9b4ec42f723a
mihirk <mihir.khatwani@gmail.com>
2018-06-29 13:09:05 +0530

    Adding voiding to rules

--- openchs-server-api/src/main/java/org/openchs/web/RuleController.java
+++ openchs-server-api/src/main/java/org/openchs/web/RuleController.java
@@ -15,6 +15,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RuleController {
     private final Logger logger;
@@ -34,9 +36,17 @@ public class RuleController {
                 HttpStatus.CREATED);
     }
 
+    @RequestMapping(value = "/rules", method = RequestMethod.POST)
+    @PreAuthorize(value = "hasAnyAuthority('user', 'admin')")
+    public ResponseEntity<?> saveRules(@RequestBody List<RuleRequest> ruleRequests) {
+        logger.info(String.format("Creating rules for: %s", UserContextHolder.getUserContext().getOrganisation().getName()));
+        ruleService.createRules(ruleRequests);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
     @RequestMapping(value = "/rule", method = RequestMethod.POST)
     @PreAuthorize(value = "hasAnyAuthority('user', 'admin')")
-    public ResponseEntity<?> saveRules(@RequestBody RuleRequest ruleRequest) {
+    public ResponseEntity<?> saveRule(@RequestBody RuleRequest ruleRequest) {
         logger.info(String.format("Creating rules for: %s", UserContextHolder.getUserContext().getOrganisation().getName()));
         ruleService.createRule(ruleRequest.getRuleDependencyUUID(), ruleRequest);
         return new ResponseEntity<>(HttpStatus.CREATED);
