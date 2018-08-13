d06a6d33061e5f78648e976f35c92c3af67ad830
Vivek Singh <petmongrels@gmail.com>
2018-08-13 14:48:34 +0530

    sql to create demo organisation on top of any organisation

---
+++ openchs-server-api/src/main/resources/database/create_demo_organisation_for_implementation.sql
@@ -0,0 +1,12 @@
+-- NOTE you have to put db_user twice, once in quotes
+CREATE ROLE :db_user NOINHERIT NOLOGIN;
+
+GRANT :db_user TO openchs;
+
+GRANT ALL ON ALL TABLES IN SCHEMA public TO :db_user;
+GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO :db_user;
+GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO :db_user;
+
+
+INSERT into organisation(name, db_user, uuid, parent_organisation_id)
+  SELECT '' || :org_name || ' Demo', :db_user_string, :uuid, :parent_organisation_id;
