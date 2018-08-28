b7047d730802a86658f610036abcbbd6241432f5
Arjun <arjunk@thoughtworks.com>
2018-08-28 15:54:21 +0530

    Adding a new domain entity facility

---
+++ openchs-server-api/src/main/java/org/openchs/domain/Facility.java
@@ -0,0 +1,39 @@
+package org.openchs.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "facility")
+public class Facility extends OrganisationAwareEntity {
+    @NotNull
+    private String name;
+    
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "address_id")
+    private AddressLevel addressLevel;
+    
+    public AddressLevel getAddressLevel() {
+        return addressLevel;
+    }
+
+    public void setAddressLevel(AddressLevel addressLevel) {
+        this.addressLevel = addressLevel;
+    }
+
+
+    public static Facility create(String name, AddressLevel address) {
+        Facility facility = new Facility();
+        facility.name = name;
+        facility.addressLevel = address;
+        return facility;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
