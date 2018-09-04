06fef51aefea0227cdcd40675e63906296319c19
A.N.Sidtharthan <sidthunagarajan@gmail.com>
2018-09-04 17:55:08 +0530

    #importer Add sample regex patterns to ImportCalculatedFieldTests

--- openchs-server-api/src/test/java/org/openchs/excel/metadata/ImportCalculatedFieldTest.java
+++ openchs-server-api/src/test/java/org/openchs/excel/metadata/ImportCalculatedFieldTest.java
@@ -11,6 +11,8 @@ public class ImportCalculatedFieldTest {
         assertEquals("John Doe", getActual("John Doe Danish", "\\w+\\s+\\w+"));
         assertEquals("३ दिवस\nपॅरासिटामॉल १ टॅबलेट दिवसातून तीन वेळा जेवणानंतर\n", getActual("३ दिवस\nपॅरासिटामॉल १ टॅबलेट दिवसातून तीन वेळा जेवणानंतर\n05/10/2016 18:56:56", "[^0-9:/]*"));
         assertEquals("३ दिवस\nपॅरासिटामॉल १ टॅबलेट दिवसातून तीन वेळा जेवणानंतर\n", getActual("३ दिवस\nपॅरासिटामॉल १ टॅबलेट दिवसातून तीन वेळा जेवणानंतर\n05/10/2016 18:56:56", "[^0-9:/]*"));
+        assertEquals("80", getActual("80/50", "([0-9]+)(?:/[0-9]+)"));
+        assertEquals("80", getActual("120/80", "(?:[0-9]+/)([0-9]+)"));
     }
 
     private String getActual(String name, String regex) {
