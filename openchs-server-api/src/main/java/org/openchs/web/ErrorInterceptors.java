6cd12cb927cbb19795b9ee830943ba5fb2b3db4a
Hiren Thacker <hithacker@gmail.com>
2018-10-26 17:25:21 +0530

    [openchs/#945] Setup bug snag for staging

--- openchs-server-api/src/main/java/org/openchs/web/ErrorInterceptors.java
+++ openchs-server-api/src/main/java/org/openchs/web/ErrorInterceptors.java
@@ -19,30 +19,24 @@ import com.bugsnag.Bugsnag;
 public class ErrorInterceptors {
     private final Logger logger;
     private final Bugsnag bugsnag;
-
-    private Environment environment;
-
+    
     @Autowired
     public ErrorInterceptors(Environment environment) {
         this.logger = LoggerFactory.getLogger(this.getClass());
-        this.environment = environment;
         String bugsnagAPIKey = environment.getProperty("openchs.bugsnag.apiKey");
+        String bugsnagReleaseStage = environment.getProperty("openchs.bugsnag.releaseStage");
         logger.info(String.format("bugsnagAPIKey is: %s", bugsnagAPIKey));
-        bugsnag = new Bugsnag(bugsnagAPIKey);
+        logger.info(String.format("bugsnagReleaseStage is: %s", bugsnagReleaseStage));
+        bugsnag = new Bugsnag(bugsnagAPIKey, false);
+        bugsnag.setReleaseStage(bugsnagReleaseStage);
+        bugsnag.setNotifyReleaseStages("prod", "staging");
     }
 
     @ExceptionHandler(value = {Exception.class})
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ResponseEntity<String> unknownException(Exception e, WebRequest req, HttpServletResponse res) {
-        if(!isDev()) {
-            bugsnag.notify(e);
-        }
+        bugsnag.notify(e);
         logger.error(e.getMessage(), e);
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(e.getMessage());
-    }
-
-    private boolean isDev() {
-        String[] activeProfiles = environment.getActiveProfiles();
-        return activeProfiles.length == 1 && (activeProfiles[0].equals("dev") || activeProfiles[0].equals("test"));
     }
 }
