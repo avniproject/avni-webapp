735f4c45a9fabf24adb614226951c8606f820b03
Sidtharthan A N <sidthunagarajan@gmail.com>
2018-10-29 17:15:58 +0530

    Importer. Refactor. Code align and improve readability in `ExcelUtil/possibleDateFormatters`

--- openchs-server-api/src/main/java/org/openchs/excel/ExcelUtil.java
+++ openchs-server-api/src/main/java/org/openchs/excel/ExcelUtil.java
@@ -14,12 +14,25 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
 
-import java.util.Date;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class ExcelUtil {
     private static Logger logger = LoggerFactory.getLogger(ExcelUtil.class);
     //Do not remove any pattern because this is built up based on formats we have encountered so far in various migration data
-    private static DateTimeFormatter[] possibleDateFormatters = new DateTimeFormatter[]{DateTimeFormat.forPattern("dd/MMM/yyyy"), DateTimeFormat.forPattern("dd/M/yyyy"), DateTimeFormat.forPattern("dd-MMM-yyyy"), DateTimeFormat.forPattern("dd-M-yyyy"), DateTimeFormat.forPattern("dd.M.yyyy"), DateTimeFormat.forPattern("yyyy-MMM-dd"), DateTimeFormat.forPattern("yyyy-MM-dd"), DateTimeFormat.forPattern("yyyy-MM-dd H:m:s.S"),DateTimeFormat.forPattern("yyyy-MM-dd H:m:s"),DateTimeFormat.forPattern("H:m:s")};
+    private static List<DateTimeFormatter> possibleDateFormatters = Stream.of(
+            "dd/MMM/yyyy",
+            "dd/M/yyyy",
+            "dd-MMM-yyyy",
+            "dd-M-yyyy",
+            "dd.M.yyyy",
+            "yyyy-MMM-dd",
+            "yyyy-MM-dd",
+            "yyyy-MM-dd H:m:s.S",
+            "yyyy-MM-dd H:m:s",
+            "H:m:s"
+    ).map(DateTimeFormat::forPattern).collect(Collectors.toList());
     private static String NAN = "NAN";
 
     public static String getText(Row row, int cellNum) {
