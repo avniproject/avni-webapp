75cc8c0d9d3f230f922c2dcb729c20bcec0670b7
mihirk <mihir.khatwani@gmail.com>
2018-11-02 16:04:26 +0530

    Remove problematic migration

--- openchs-server-api/src/main/resources/db/migration/V1_74__ModifyPrivilegeOfOrgUserOnUsers.sql
+++
@@ -1,4 +0,0 @@
-DROP POLICY users_user ON users;
-DROP POLICY users_orgs ON users;
-CREATE POLICY users_orgs ON users USING ((organisation_id = (select id from organisation where db_user = current_user))) WITH CHECK ((organisation_id = (select id from organisation where db_user = current_user)));
-ALTER TABLE users ENABLE ROW LEVEL SECURITY;
