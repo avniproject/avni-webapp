persistent-avni-contribution/avni-server

SqlGenerationWebService Update the api to generate sql per encountertype
apis:
For a program sqls for all encountertypes
For a program sql for an encountertype

openchs-server-api/src/main/java/org/openchs/reporting/SqlGenerationService.java
@@ -1,130 +1,131 @@
 package org.openchs.reporting;
 
-import org.openchs.application.Form;
 import org.openchs.application.FormElement;
-import org.openchs.application.FormMapping;
 import org.openchs.application.FormType;
+import org.openchs.dao.OperationalEncounterTypeRepository;
 import org.openchs.dao.OperationalProgramRepository;
-import org.openchs.dao.ProgramEnrolmentRepository;
 import org.openchs.dao.application.FormMappingRepository;
 import org.openchs.domain.ConceptDataType;
+import org.openchs.domain.OperationalEncounterType;
+import org.openchs.domain.OperationalProgram;
 import org.openchs.domain.Program;
-import org.openchs.framework.security.UserContextHolder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.AbstractMap.SimpleEntry;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static java.util.Collections.singletonList;
 
 @Service
 public class SqlGenerationService {
-    private String VIEW_TEMPLATE;
+    private final String VIEW_TEMPLATE;
+
+    private final OperationalProgramRepository operationalProgramRepository;
+    private final OperationalEncounterTypeRepository operationalEncounterTypeRepository;
+    private final FormMappingRepository formMappingRepository;
 
     @Autowired
-    private OperationalProgramRepository operationalProgramRepository;
+    public SqlGenerationService(OperationalProgramRepository operationalProgramRepository, OperationalEncounterTypeRepository operationalEncounterTypeRepository, FormMappingRepository formMappingRepository) throws IOException {
+        this.operationalProgramRepository = operationalProgramRepository;
+        this.operationalEncounterTypeRepository = operationalEncounterTypeRepository;
+        this.formMappingRepository = formMappingRepository;
 
-    @Autowired
-    private FormMappingRepository formMappingRepository;
-
-    public String getSqlFor(String programName) throws IOException, URISyntaxException {
         VIEW_TEMPLATE = new BufferedReader(new InputStreamReader(new ClassPathResource("/pivot/pivot.sql").getInputStream()))
                 .lines()
                 .collect(Collectors.joining("\n"));
-        List<Program> programs = new ArrayList<>();
-        operationalProgramRepository.findAll().forEach(op -> programs.add(op.getProgram()));
-        return programs.stream().filter(program -> program.getName().equals(programName))
-                .map(this::getSqlForProgram)
-                .findFirst()
-                .orElse("");
     }
 
-    private Form getRegistrationForm() {
+    public Map<String, String> getSqlsFor(String operationalProgramName, String operationalEncounterTypeName) {
+        OperationalProgram operationalProgram = operationalProgramRepository.findByNameIgnoreCase(operationalProgramName);
+        Iterable<OperationalEncounterType> operationalEncounterTypes = operationalEncounterTypeName == null ?
+                operationalEncounterTypeRepository.findAll() :
+                singletonList(operationalEncounterTypeRepository.findByNameIgnoreCase(operationalEncounterTypeName));
+        if (operationalProgram != null && operationalProgram.getProgram() != null) {
+            return getSqlFor(operationalProgram, operationalEncounterTypes);
+        }
+        throw new IllegalArgumentException(String.format("Not found OperationalProgram{name='%s'}", operationalProgramName));
+    }
+
+    private List<FormElement> getRegistrationFormElements() {
         return formMappingRepository
                 .findByEntityIdAndObservationsTypeEntityIdAndFormFormType(null, null, FormType.IndividualProfile)
-                .getForm();
+                .getForm()
+                .getAllFormElements();
     }
 
-    private Form getProgramEnrolmentForm(Program program) {
+    private List<FormElement> getProgramEnrolmentFormElements(Program program) {
         return formMappingRepository
                 .findByEntityIdAndObservationsTypeEntityIdAndFormFormType(program.getId(), null, FormType.ProgramEnrolment)
-                .getForm();
+                .getForm()
+                .getAllFormElements();
     }
 
-    private List<Form> getProgramEncounterForms(Program program) {
+    private List<FormElement> getProgramEncounterFormElements(OperationalProgram operationalProgram, OperationalEncounterType type) {
         return formMappingRepository
-                .findByEntityIdAndOrganisationIdIsNotNull(program.getId())
+                .findAllByEntityIdAndObservationsTypeEntityIdAndFormFormType(operationalProgram.getProgram().getId(), type.getEncounterType().getId(), FormType.ProgramEnrolment)
                 .stream()
-                .filter(formMapping -> formMapping.getForm().getFormType().equals(FormType.ProgramEncounter))
-                .map(FormMapping::getForm)
+                .flatMap(fm -> fm.getForm().getAllFormElements().stream())
                 .collect(Collectors.toList());
     }
 
-    private Map<String, List<FormElement>> getAllFormConcepts(Program program) {
-        Map<String, List<FormElement>> conceptMap = new HashMap<>();
-        List<FormElement> registrationFormElements = getRegistrationForm().getAllFormElements();
-        List<FormElement> enrolmentFormElements = getProgramEnrolmentForm(program).getAllFormElements();
-        List<FormElement> encounterFormElements = getProgramEncounterForms(program).stream()
-                .flatMap(f -> f.getAllFormElements().stream())
-                .collect(Collectors.toList());
-        conceptMap.put("individual", registrationFormElements);
-        conceptMap.put("programEnrolment", enrolmentFormElements);
-        conceptMap.put("programEncounter", encounterFormElements);
-        return conceptMap;
+    private Map<String, String> getSqlFor(OperationalProgram operationalProgram, Iterable<OperationalEncounterType> types) {
+        final String mainViewQuery;
+        List<FormElement> registrationFormElements = getRegistrationFormElements();
+        List<FormElement> enrolmentFormElements = getProgramEnrolmentFormElements(operationalProgram.getProgram());
+
+        mainViewQuery = VIEW_TEMPLATE.replace("${individual}", buildObsevationSelection("individual", registrationFormElements))
+                .replace("${programEnrolment}", buildObsevationSelection("programEnrolment", enrolmentFormElements))
+                .replace("${operationalProgramUuid}", operationalProgram.getUuid());
+
+        return StreamSupport.stream(types.spliterator(), true).map(type -> {
+            List<FormElement> programEncounterFormElements = getProgramEncounterFormElements(operationalProgram, type);
+            String encounterSpecificSql = mainViewQuery.replace("${programEncounter}", buildObsevationSelection("programEncounter", programEncounterFormElements));
+            return new SimpleEntry<>(type.getName(), encounterSpecificSql);
+        }).collect(Collectors.toConcurrentMap(SimpleEntry::getKey, SimpleEntry::getValue));
     }
 
-    private String getSqlForProgram(Program program) {
-        AtomicReference<String> viewQuery = new AtomicReference<>(VIEW_TEMPLATE);
-        getAllFormConcepts(program).forEach((key, value) -> {
-            String selectString = value.parallelStream().map(formElement -> {
-                StringBuilder stringBuilder = new StringBuilder();
-                switch (ConceptDataType.valueOf(formElement.getConcept().getDataType())) {
-                    case Numeric:
-                    case Text:
-                    case Notes:
-                    case Date:
-                        stringBuilder
-                                .append(key)
+    private String buildObsevationSelection(String tableName, List<FormElement> value) {
+        return value.parallelStream().map(formElement -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            switch (ConceptDataType.valueOf(formElement.getConcept().getDataType())) {
+                case Numeric:
+                case Text:
+                case Notes:
+                case Date:
+                    stringBuilder
+                            .append(tableName)
+                            .append(".observations->>'")
+                            .append(formElement.getConcept().getUuid())
+                            .append("'");
+                    break;
+                case Coded: {
+                    if (formElement.isSingleSelect()) {
+                        stringBuilder.append("single_select_coded(")
+                                .append(tableName)
                                 .append(".observations->>'")
                                 .append(formElement.getConcept().getUuid())
-                                .append("'");
-                        break;
-                    case Coded: {
-                        if (formElement.isSingleSelect()) {
-                            stringBuilder.append("single_select_coded(")
-                                    .append(key)
-                                    .append(".observations->>'")
-                                    .append(formElement.getConcept().getUuid())
-                                    .append("')");
-                        } else {
-                            stringBuilder.append("multi_select_coded(")
-                                    .append(key)
-                                    .append(".observations->'")
-                                    .append(formElement.getConcept().getUuid())
-                                    .append("')");
-                        }
-                        break;
+                                .append("')");
+                    } else {
+                        stringBuilder.append("multi_select_coded(")
+                                .append(tableName)
+                                .append(".observations->'")
+                                .append(formElement.getConcept().getUuid())
+                                .append("')");
                     }
+                    break;
                 }
-                stringBuilder.append("::TEXT as \"").append(formElement.getConcept().getName()).append("\"");
-                return stringBuilder.toString();
-            }).collect(Collectors.joining(","));
-            viewQuery.set(viewQuery.get().replace("${" + key + "}", selectString));
-        });
-        viewQuery.set(viewQuery.get().replace("${programName}", program.getName()));
-        return viewQuery.get();
+            }
+            stringBuilder.append("::TEXT as \"").append(formElement.getConcept().getName()).append("\"");
+            return stringBuilder.toString();
+        }).collect(Collectors.joining(","));
     }
-
 
 }

avniproject/avni-webapp

openchs-server-api/src/main/java/org/openchs/reporting/SqlGenerationService.java
@@ -1,130 +1,147 @@
 package org.openchs.reporting;
 
-import org.openchs.application.Form;
 import org.openchs.application.FormElement;
 import org.openchs.application.FormMapping;
 import org.openchs.application.FormType;
+import org.openchs.dao.OperationalEncounterTypeRepository;
 import org.openchs.dao.OperationalProgramRepository;
-import org.openchs.dao.ProgramEnrolmentRepository;
 import org.openchs.dao.application.FormMappingRepository;
 import org.openchs.domain.ConceptDataType;
-import org.openchs.domain.Program;
-import org.openchs.framework.security.UserContextHolder;
+import org.openchs.domain.OperationalEncounterType;
+import org.openchs.domain.OperationalProgram;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
-import org.springframework.util.ResourceUtils;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.net.URISyntaxException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
+
+import static java.util.Collections.singletonList;
 
 @Service
 public class SqlGenerationService {
-    private String VIEW_TEMPLATE;
+    private final String VIEW_TEMPLATE;
+
+    private final OperationalProgramRepository operationalProgramRepository;
+    private final OperationalEncounterTypeRepository operationalEncounterTypeRepository;
+    private final FormMappingRepository formMappingRepository;
+    private final String PLACE_HOLDER_FOR_COUNSELLING_FORM_ELEMENT = "b4e5a662-97bf-4846-b9b7-9baeab4d89c4";
 
     @Autowired
-    private OperationalProgramRepository operationalProgramRepository;
+    public SqlGenerationService(OperationalProgramRepository operationalProgramRepository, OperationalEncounterTypeRepository operationalEncounterTypeRepository, FormMappingRepository formMappingRepository) throws IOException {
+        this.operationalProgramRepository = operationalProgramRepository;
+        this.operationalEncounterTypeRepository = operationalEncounterTypeRepository;
+        this.formMappingRepository = formMappingRepository;
 
-    @Autowired
-    private FormMappingRepository formMappingRepository;
-
-    public String getSqlFor(String programName) throws IOException, URISyntaxException {
         VIEW_TEMPLATE = new BufferedReader(new InputStreamReader(new ClassPathResource("/pivot/pivot.sql").getInputStream()))
                 .lines()
                 .collect(Collectors.joining("\n"));
-        List<Program> programs = new ArrayList<>();
-        operationalProgramRepository.findAll().forEach(op -> programs.add(op.getProgram()));
-        return programs.stream().filter(program -> program.getName().equals(programName))
-                .map(this::getSqlForProgram)
-                .findFirst()
-                .orElse("");
     }
 
-    private Form getRegistrationForm() {
-        return formMappingRepository
-                .findByEntityIdAndObservationsTypeEntityIdAndFormFormType(null, null, FormType.IndividualProfile)
-                .getForm();
+    public Map<String, String> getSqlsFor(String operationalProgramName, String operationalEncounterTypeName) {
+        OperationalProgram operationalProgram = operationalProgramRepository.findByNameIgnoreCase(operationalProgramName);
+        List<OperationalEncounterType> operationalEncounterTypes = operationalEncounterTypeName == null ?
+                operationalEncounterTypeRepository.findAll() :
+                singletonList(operationalEncounterTypeRepository.findByNameIgnoreCase(operationalEncounterTypeName));
+        if (operationalProgram != null && operationalProgram.getProgram() != null) {
+            return getSqlsFor(operationalProgram, operationalEncounterTypes);
+        }
+        throw new IllegalArgumentException(String.format("Not found OperationalProgram{name='%s'}", operationalProgramName));
     }
 
-    private Form getProgramEnrolmentForm(Program program) {
-        return formMappingRepository
-                .findByEntityIdAndObservationsTypeEntityIdAndFormFormType(program.getId(), null, FormType.ProgramEnrolment)
-                .getForm();
+    private Map<String, String> getSqlsFor(OperationalProgram operationalProgram, List<OperationalEncounterType> types) {
+        List<FormElement> registrationFormElements = getRegistrationFormElements();
+        List<FormElement> enrolmentFormElements = getProgramEnrolmentFormElements(operationalProgram);
+
+        String mainViewQuery = VIEW_TEMPLATE.replace("${operationalProgramUuid}", operationalProgram.getUuid())
+                .replace("${individual}", buildObservationSelection("individual", registrationFormElements, "Ind"))
+                .replace("${programEnrolment}", buildObservationSelection("programEnrolment", enrolmentFormElements,"Enl"));
+
+        return types.stream()
+                .map(type -> new SimpleEntry<>(type, getProgramEncounterFormElements(operationalProgram, type)))
+                .filter(pair -> !pair.getValue().isEmpty())
+                .map(pair -> {
+                    OperationalEncounterType type = pair.getKey();
+                    List<FormElement> formElements = pair.getValue();
+                    List<FormElement> cancelFormElements = getProgramEncounterCancelFormElements(operationalProgram, type);
+                    return new SimpleEntry<>(type.getName(), getSqlForProgramEncounter(mainViewQuery, type, formElements, cancelFormElements));
+                }).collect(Collectors.toMap(SimpleEntry::getKey, SimpleEntry::getValue));
     }
 
-    private List<Form> getProgramEncounterForms(Program program) {
-        return formMappingRepository
-                .findByEntityIdAndOrganisationIdIsNotNull(program.getId())
-                .stream()
-                .filter(formMapping -> formMapping.getForm().getFormType().equals(FormType.ProgramEncounter))
-                .map(FormMapping::getForm)
-                .collect(Collectors.toList());
+    private String getSqlForProgramEncounter(String mainViewQuery, OperationalEncounterType operationalEncounterType, List<FormElement> formElements, List<FormElement> cancelFormElements) {
+        return mainViewQuery
+                .replace("${operationalEncounterTypeUuid}", operationalEncounterType.getUuid())
+                .replace("${programEncounter}", buildObservationSelection("programEncounter", formElements, "Enc"))
+                .replace("${programEncounterCancellation}", buildObservationSelection("programEncounter", cancelFormElements, "EncCancel", true));
     }
 
-    private Map<String, List<FormElement>> getAllFormConcepts(Program program) {
-        Map<String, List<FormElement>> conceptMap = new HashMap<>();
-        List<FormElement> registrationFormElements = getRegistrationForm().getAllFormElements();
-        List<FormElement> enrolmentFormElements = getProgramEnrolmentForm(program).getAllFormElements();
-        List<FormElement> encounterFormElements = getProgramEncounterForms(program).stream()
-                .flatMap(f -> f.getAllFormElements().stream())
-                .collect(Collectors.toList());
-        conceptMap.put("individual", registrationFormElements);
-        conceptMap.put("programEnrolment", enrolmentFormElements);
-        conceptMap.put("programEncounter", encounterFormElements);
-        return conceptMap;
+    private List<FormElement> getRegistrationFormElements() {
+        return getFormElements(null, null, FormType.IndividualProfile);
     }
 
-    private String getSqlForProgram(Program program) {
-        AtomicReference<String> viewQuery = new AtomicReference<>(VIEW_TEMPLATE);
-        getAllFormConcepts(program).forEach((key, value) -> {
-            String selectString = value.parallelStream().map(formElement -> {
-                StringBuilder stringBuilder = new StringBuilder();
-                switch (ConceptDataType.valueOf(formElement.getConcept().getDataType())) {
-                    case Numeric:
-                    case Text:
-                    case Notes:
-                    case Date:
-                        stringBuilder
-                                .append(key)
-                                .append(".observations->>'")
+    private List<FormElement> getProgramEnrolmentFormElements(OperationalProgram operationalProgram) {
+        return getFormElements(operationalProgram.getProgram().getId(), null, FormType.ProgramEnrolment);
+    }
+
+    private List<FormElement> getProgramEncounterFormElements(OperationalProgram operationalProgram, OperationalEncounterType type) {
+        return getFormElements(operationalProgram.getProgram().getId(), type.getEncounterType().getId(), FormType.ProgramEncounter);
+    }
+
+    private List<FormElement> getProgramEncounterCancelFormElements(OperationalProgram operationalProgram, OperationalEncounterType type) {
+        return getFormElements(operationalProgram.getProgram().getId(), type.getEncounterType().getId(), FormType.ProgramEncounterCancellation);
+    }
+
+    private List<FormElement> getFormElements(Long programId, Long typeId, FormType formType) {
+        FormMapping formMapping = formMappingRepository.findByEntityIdAndObservationsTypeEntityIdAndFormFormType(programId, typeId, formType);
+        List<FormElement> formElements = formMapping == null ? Collections.EMPTY_LIST : formMapping.getForm().getApplicableFormElements();
+        return formElements.stream().filter(fe -> !PLACE_HOLDER_FOR_COUNSELLING_FORM_ELEMENT.equals(fe.getConcept().getUuid())).collect(Collectors.toList());
+    }
+
+    private String buildObservationSelection(String entity, List<FormElement> elements, String columnPrefix) {
+        return buildObservationSelection(entity, elements, columnPrefix, false);
+    }
+
+    private String buildObservationSelection(String entity, List<FormElement> elements, String columnPrefix, Boolean forCancelled) {
+        String obsColumn = entity + (forCancelled ? ".cancel_observations": ".observations");
+        return elements.parallelStream().map(formElement -> {
+            StringBuilder stringBuilder = new StringBuilder();
+            switch (ConceptDataType.valueOf(formElement.getConcept().getDataType())) {
+                case Numeric:
+                case Text:
+                case Notes:
+                case Date:
+                    stringBuilder
+                            .append(obsColumn)
+                            .append("->>'")
+                            .append(formElement.getConcept().getUuid())
+                            .append("'");
+                    break;
+                case Coded: {
+                    if (formElement.isSingleSelect()) {
+                        stringBuilder.append("single_select_coded(")
+                                .append(obsColumn)
+                                .append("->>'")
                                 .append(formElement.getConcept().getUuid())
-                                .append("'");
-                        break;
-                    case Coded: {
-                        if (formElement.isSingleSelect()) {
-                            stringBuilder.append("single_select_coded(")
-                                    .append(key)
-                                    .append(".observations->>'")
-                                    .append(formElement.getConcept().getUuid())
-                                    .append("')");
-                        } else {
-                            stringBuilder.append("multi_select_coded(")
-                                    .append(key)
-                                    .append(".observations->'")
-                                    .append(formElement.getConcept().getUuid())
-                                    .append("')");
-                        }
-                        break;
+                                .append("')");
+                    } else {
+                        stringBuilder.append("multi_select_coded(")
+                                .append(obsColumn)
+                                .append("->'")
+                                .append(formElement.getConcept().getUuid())
+                                .append("')");
                     }
+                    break;
                 }
-                stringBuilder.append("::TEXT as \"").append(formElement.getConcept().getName()).append("\"");
-                return stringBuilder.toString();
-            }).collect(Collectors.joining(","));
-            viewQuery.set(viewQuery.get().replace("${" + key + "}", selectString));
-        });
-        viewQuery.set(viewQuery.get().replace("${programName}", program.getName()));
-        return viewQuery.get();
+            }
+            stringBuilder.append("::TEXT as \"").append(columnPrefix).append(".").append(formElement.getConcept().getName()).append("\"");
+            return stringBuilder.toString();
+        }).collect(Collectors.joining(","));
     }
-
 
 }