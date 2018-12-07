c7531d04fb390f660603ba4d1c4317f026b6b9d0
Hiren Thacker <hithacker@gmail.com>
2018-12-07 17:18:02 +0530

    Fixing access issue on ProgramRuleRepository

--- openchs-server-api/src/main/java/org/openchs/dao/ProgramRuleRepository.java
+++ openchs-server-api/src/main/java/org/openchs/dao/ProgramRuleRepository.java
@@ -3,20 +3,25 @@ package org.openchs.dao;
 import org.openchs.domain.Program;
 import org.openchs.domain.ProgramRule;
 import org.openchs.domain.Rule;
-import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 @RepositoryRestResource(collectionResourceRel = "programRule", path = "programRule")
-@PreAuthorize(value = "hasAnyAuthority('user', 'admin')")
-public interface ProgramRuleRepository extends PagingAndSortingRepository<ProgramRule, Long>, CHSRepository<ProgramRule> {
+public interface ProgramRuleRepository extends ImplReferenceDataRepository<ProgramRule> {
     List<ProgramRule> findByOrganisationId(Long id);
     List<ProgramRule> findByProgram(Program program);
     ProgramRule findByRule(Rule rule);
     ProgramRule findByRule_Uuid(String uuid);
     ProgramRule findByProgramAndRule(Program program, Rule rule);
+
+    default ProgramRule findByName(String name) {
+        throw new UnsupportedOperationException("No field 'name' in ProgramRule");
+    }
+
+    default ProgramRule findByNameIgnoreCase(String name) {
+        throw new UnsupportedOperationException("No field 'name' in ProgramRule");
+    }
 }
