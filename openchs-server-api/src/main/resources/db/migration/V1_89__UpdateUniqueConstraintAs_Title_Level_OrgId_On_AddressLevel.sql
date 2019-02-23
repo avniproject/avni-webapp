1ea69d501db55c0f5d7704f60c3aac8fc0e3e89b
Sidtharthan A N <sidthunagarajan@gmail.com>
2019-02-23 14:29:57 +0530

    1198 | Remove address_level table unique constraint uniq(title,level)

--- openchs-server-api/src/main/resources/db/migration/V1_89__UpdateUniqueConstraintAs_Title_Level_OrgId_On_AddressLevel.sql
+++ openchs-server-api/src/main/resources/db/migration/V1_89__UpdateUniqueConstraintAs_Title_Level_OrgId_On_AddressLevel.sql
@@ -1,3 +1,2 @@
 
 alter table address_level drop constraint address_level_unique_titel_level;
-alter table address_level add constraint address_level_unique_title_level_org_id unique (title, level, organisation_id);
