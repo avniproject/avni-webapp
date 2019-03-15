5b997c407c73ba579fe5342cf43bcf7dd2a4d052
Hiren Thacker <hithacker@gmail.com>
2019-03-15 10:02:30 +0530

    983 | Permission for database views to specific organisation db user only

--- openchs-server-api/src/main/resources/db/migration/afterMigrate.sql
+++ openchs-server-api/src/main/resources/db/migration/afterMigrate.sql
@@ -1,11 +1,37 @@
 CREATE OR REPLACE FUNCTION grant_all_on_all(rolename text)
-    RETURNS text AS $body$
+    RETURNS text AS
+$body$
 BEGIN
-    EXECUTE 'GRANT ALL ON ALL TABLES IN SCHEMA public TO ' || quote_ident(rolename) || '' ;
-    EXECUTE 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ' || quote_ident(rolename) || '' ;
-    EXECUTE 'GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO ' || quote_ident(rolename) || '' ;
+    EXECUTE (
+        SELECT 'GRANT ALL ON TABLE '
+                   || string_agg(format('%I.%I', table_schema, table_name), ',')
+                   || ' TO ' || quote_ident(rolename) || ''
+        FROM information_schema.tables
+        WHERE table_schema = 'public'
+          AND table_type = 'BASE TABLE'
+    );
+    EXECUTE 'GRANT ALL ON ALL SEQUENCES IN SCHEMA public TO ' || quote_ident(rolename) || '';
+    EXECUTE 'GRANT ALL ON ALL FUNCTIONS IN SCHEMA public TO ' || quote_ident(rolename) || '';
     RETURN 'ALL PERMISSIONS GRANTED TO ' || quote_ident(rolename);
 END;
 $body$ LANGUAGE plpgsql;
 
-SELECT grant_all_on_all(a.rolname) FROM pg_roles a WHERE pg_has_role('openchs', a.oid, 'member');
+CREATE OR REPLACE FUNCTION grant_all_on_views(view_names text[], role text)
+    RETURNS text AS
+$body$
+DECLARE
+    view_names_string text;
+    each record;
+BEGIN
+    view_names_string := array_to_string(view_names, ',');
+    FOR each IN SELECT rolname FROM pg_roles where pg_has_role('openchs', oid, 'member') LOOP
+        EXECUTE 'REVOKE ALL ON ' || view_names_string || ' FROM ' || quote_ident(each.rolname) || '';
+    END LOOP;
+    EXECUTE 'GRANT ALL ON ' || view_names_string || ' TO ' || quote_ident(role) || '';
+    RETURN 'EXECUTE GRANT ALL ON ' || view_names_string || ' TO ' || quote_ident(role) || '';
+END;
+$body$ LANGUAGE plpgsql;
+
+SELECT grant_all_on_all(a.rolname)
+FROM pg_roles a
+WHERE pg_has_role('openchs', a.oid, 'member');
