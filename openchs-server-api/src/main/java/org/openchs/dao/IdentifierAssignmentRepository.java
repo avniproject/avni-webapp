97c1c002947ee447820c74c75bf4c704aad1df11
Vinay Venu <ijkvinay@gmail.com>
2019-04-09 10:55:19 +0530

    Make response consistent to fix sync of IdentifierAssignment

--- openchs-server-api/src/main/java/org/openchs/dao/IdentifierAssignmentRepository.java
+++ openchs-server-api/src/main/java/org/openchs/dao/IdentifierAssignmentRepository.java
@@ -10,7 +10,7 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 @Repository
-@RepositoryRestResource(collectionResourceRel = "identifierAssignmentPool", path = "identifierAssignmentPool")
+@RepositoryRestResource(collectionResourceRel = "identifierAssignment", path = "identifierAssignment")
 public interface IdentifierAssignmentRepository extends TransactionalDataRepository<IdentifierAssignment>, FindByLastModifiedDateTime<IdentifierAssignment> {
     Page<IdentifierAssignment> findByAssignedToAndAuditLastModifiedDateTimeGreaterThanAndIsVoidedFalseAndIndividualIsNullAndProgramEnrolmentIsNullOrderByAssignmentOrderAsc(User currentUser, DateTime lastModifiedDateTime, Pageable pageable);
 
