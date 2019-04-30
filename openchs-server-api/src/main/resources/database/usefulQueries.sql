b882097c284873d4732e3e7f12b0cec0c136a2d4
Hiren Thacker <hithacker@gmail.com>
2019-04-30 11:46:49 +0530

    0000 | Add useful query to detect blocking queries

--- openchs-server-api/src/main/resources/database/usefulQueries.sql
+++ openchs-server-api/src/main/resources/database/usefulQueries.sql
@@ -322,3 +322,40 @@ with updates(audit_id) as (
 update audit
 set last_modified_date_time = current_timestamp
 where id in (select audit_id from updates);
+
+-- Find long running queries
+SELECT
+    pid,
+    now() - pg_stat_activity.query_start AS duration,
+    query,
+    state
+FROM pg_stat_activity
+WHERE (now() - pg_stat_activity.query_start) > interval '5 minutes';
+
+-- See what queries are in waiting to acquire locks
+SELECT relation::regclass, * FROM pg_locks WHERE NOT GRANTED;
+
+-- See what processes are blocking what queries (these only find row-level locks, not object-level locks)
+SELECT blocked_locks.pid     AS blocked_pid,
+       blocked_activity.usename  AS blocked_user,
+       blocking_locks.pid     AS blocking_pid,
+       blocking_activity.usename AS blocking_user,
+       blocked_activity.query    AS blocked_statement,
+       blocking_activity.query   AS current_statement_in_blocking_process
+FROM  pg_catalog.pg_locks         blocked_locks
+        JOIN pg_catalog.pg_stat_activity blocked_activity  ON blocked_activity.pid = blocked_locks.pid
+        JOIN pg_catalog.pg_locks         blocking_locks
+             ON blocking_locks.locktype = blocked_locks.locktype
+               AND blocking_locks.DATABASE IS NOT DISTINCT FROM blocked_locks.DATABASE
+               AND blocking_locks.relation IS NOT DISTINCT FROM blocked_locks.relation
+               AND blocking_locks.page IS NOT DISTINCT FROM blocked_locks.page
+               AND blocking_locks.tuple IS NOT DISTINCT FROM blocked_locks.tuple
+               AND blocking_locks.virtualxid IS NOT DISTINCT FROM blocked_locks.virtualxid
+               AND blocking_locks.transactionid IS NOT DISTINCT FROM blocked_locks.transactionid
+               AND blocking_locks.classid IS NOT DISTINCT FROM blocked_locks.classid
+               AND blocking_locks.objid IS NOT DISTINCT FROM blocked_locks.objid
+               AND blocking_locks.objsubid IS NOT DISTINCT FROM blocked_locks.objsubid
+               AND blocking_locks.pid != blocked_locks.pid
+
+        JOIN pg_catalog.pg_stat_activity blocking_activity ON blocking_activity.pid = blocking_locks.pid
+WHERE NOT blocked_locks.GRANTED;
