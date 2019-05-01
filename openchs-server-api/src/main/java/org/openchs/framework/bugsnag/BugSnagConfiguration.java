3be04929ed6df04c7373f6c9366b92b999298f2a
Hiren Thacker <hithacker@gmail.com>
2019-05-01 15:56:39 +0530

    0000 | remove unnecessary library and add some extra useful info

--- openchs-server-api/src/main/java/org/openchs/framework/bugsnag/BugSnagConfiguration.java
+++ openchs-server-api/src/main/java/org/openchs/framework/bugsnag/BugSnagConfiguration.java
@@ -1,17 +1,14 @@
 package org.openchs.framework.bugsnag;
 
 import com.bugsnag.Bugsnag;
-import com.bugsnag.BugsnagSpringConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.core.env.Environment;
 
 @Configuration
-@Import(BugsnagSpringConfiguration.class)
 public class BugSnagConfiguration {
 
     @Autowired
