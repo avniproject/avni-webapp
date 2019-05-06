6c7b740d0a18717f816b08058dadfcbf9fba3fb4
nupoorkhandelwal <nupoor.khadelwal@gmail.com>
2019-05-06 15:38:38 +0530

    catchment display changes

--- src/orgManager/OrgManager.js
+++ src/orgManager/OrgManager.js
@@ -7,7 +7,7 @@ import { connect } from "react-redux";
 import { authProvider, LogoutButton } from "../admin";
 import { store, adminHistory } from "../store";
 import { UserList, UserDetail, UserCreate, UserEdit } from './user';
-import { CatchmentDetail } from "./catchment";
+import { CatchmentDetail, CatchmentList, CatchmentCreate, CatchmentEdit } from "./catchment";
 
 class OrgManager extends Component {
     static childContextTypes = {
@@ -20,14 +20,16 @@ class OrgManager extends Component {
 
     render() {
         const _UserList = props => <UserList {...props} organisation={this.props.organisation} />;
+        const _CatchmentList = props => <CatchmentList {...props} organisation={this.props.organisation}/>;
+       
         return (
             <Admin title="Manage Organisation"
                    authProvider={authProvider}
                    history={adminHistory}
                    logoutButton={LogoutButton}>
                 <Resource name="user" list={_UserList} show={UserDetail} create={UserCreate} edit={UserEdit} />
-                <Resource name="catchment" show={CatchmentDetail} />
-            </Admin>
+                <Resource name="catchment" list={_CatchmentList} show={CatchmentDetail} create={CatchmentCreate} edit={CatchmentEdit}  />
+           </Admin>
         );
     }
 }
