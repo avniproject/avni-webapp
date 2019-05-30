95cd70d92ccbb34f57c1b597b3b72889162e9743
sidtharthan <sidthunagarajan@gmail.com>
2019-05-30 17:59:57 +0530

    0000 | Dont expose deprecated api

--- openchs-server-api/src/main/java/org/openchs/dao/LocationRepository.java
+++ openchs-server-api/src/main/java/org/openchs/dao/LocationRepository.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -26,11 +25,10 @@ public interface LocationRepository extends ReferenceDataRepository<AddressLevel
     @RestResource(path = "", rel = "overriddenGET")
     Page<AddressLevel> findByIsVoidedFalse(Pageable pageable);
 
-    @RestResource(path = "byCatchmentAndLastModified", rel = "byCatchmentAndLastModified")
     Page<AddressLevel> findByVirtualCatchmentsIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(
-            @Param("catchmentId") long catchmentId,
-            @Param("lastModifiedDateTime") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime lastModifiedDateTime,
-            @Param("now") @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) DateTime now,
+            long catchmentId,
+            DateTime lastModifiedDateTime,
+            DateTime now,
             Pageable pageable);
 
     AddressLevel findByTitleAndCatchmentsUuid(String title, String uuid);
