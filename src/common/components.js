b5ab35c52064c11d56674512c7659db6af20a273
Charles <charlesj@gmail.com>
2019-05-30 15:06:50 +0530

    1429 | Refactor Location detail view to work with new parent field

--- src/common/components.js
+++ src/common/components.js
@@ -15,3 +15,15 @@ export const AccessDenied = () =>
     <div className="centerContainer"><h2>Access denied</h2></div>;
 
 export const LineBreak = ({ num=1 }) => _(num).times(idx => <br key={idx}/>);
+
+export const None = ({ displayText="None" }) =>
+    <div><span>{displayText}</span></div>;
+
+export const NoneWithLabel = ({ noneText="None", labelText }) =>
+    <div>
+        <label style={{ fontSize: '0.7em', color: '#666' }}>
+            {labelText}
+        </label>
+        <br/>
+        <None displayText={noneText} />
+    </div>;
--- src/orgManager/locations.js
+++ src/orgManager/locations.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {
     Datagrid, List, TextField,
-    Show, TabbedShowLayout, Tab,
+    Show, SimpleShowLayout, ReferenceManyField,
+    ReferenceField, FunctionField,
 } from 'react-admin';
-
+import { isEmpty } from 'lodash';
+import { None } from "../common/components";
 
 export const LocationList = props => (
-    <List {...props} sort={{ field: 'level', order: 'DESC'}}
-          filter={{ childrenOf: ""}}>
+    <List {...props} sort={{ field: 'id', order: 'ASC'}} filter={{ searchURI: "findByParent"}}>
         <Datagrid rowClick="show">
             <TextField source="title"/>
             {/*
@@ -19,28 +20,31 @@ export const LocationList = props => (
     </List>
 );
 
-
-const SubLocationList = (props) =>
-    <List {...props} sort={{ field: 'level', order: 'DESC'}}
-          filter={{ childrenOf: props.record.lineage }}>
-        <Datagrid rowClick="show">
+const SubLocationsGrid = props =>
+    isEmpty(props.data) ?
+        <None />
+        :
+        <Datagrid rowClick="show" {...props}>
             <TextField source="title"/>
-            <TextField label="Type" source="typeString" />
-        </Datagrid>
-    </List>;
-
+            <TextField label="Type" source="typeString"/>
+        </Datagrid>;
 
 export const LocationDetail = props =>{
     return (
         <Show {...props}>
-            <TabbedShowLayout>
-                <Tab label="Details">
-                    <TextField source="title" label="Name" />
-                    <TextField source="typeString" label="Type" />
-                </Tab>
-                <Tab label="SubLocations">
-                    <SubLocationList {...props}/>
-                </Tab>
-            </TabbedShowLayout>
+            <SimpleShowLayout>
+                <TextField source="title" label="Name" />
+                <TextField source="typeString" label="Type" />
+                <ReferenceField label="Parent location"
+                                source="parentId"
+                                reference="locations" allowEmpty>
+                    <FunctionField render={record=> record && `${record.title} (${record.typeString})`}/>
+                </ReferenceField>
+                <ReferenceManyField label="Contains locations"
+                                    reference="locations"
+                                    target="parentId">
+                    <SubLocationsGrid/>
+                </ReferenceManyField>
+            </SimpleShowLayout>
         </Show>
 )};
