f067c4fbef3ed5bbc9fa5466a456d420e85eec7d
Hiren Thacker <hithacker@gmail.com>
2019-06-07 15:03:27 +0530

    Remove Migration 110 as 114 supersedes it. Remove 111 as it is no longer relevent

--- openchs-server-api/src/main/resources/db/migration/V1_111__AddLevelColumnToAddressLevelType.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE address_level_type
-  ADD COLUMN level double precision default 0;
-
-update address_level_type
-set level = (select distinct level from address_level where type_id = address_level_type.id);
