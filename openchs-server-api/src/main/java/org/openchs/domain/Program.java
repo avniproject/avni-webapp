a210a4c742ff2c21b6c665547f0fe005ffcbf991
sidtharthan <sidtharthann@samanvayfoundation.org>
2019-07-10 01:55:01 +0530

    #3 | Put OperationProgramInfo into ProgramProjection

--- openchs-server-api/src/main/java/org/openchs/domain/Program.java
+++ openchs-server-api/src/main/java/org/openchs/domain/Program.java
@@ -1,19 +1,26 @@
 package org.openchs.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import org.openchs.application.projections.BaseProjection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.rest.core.config.Projection;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "program")
+@JsonIgnoreProperties({"operationalPrograms"})
 public class Program extends OrganisationAwareEntity {
     @NotNull
     @Column
     private String name;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "program")
+    private Set<OperationalProgram> operationalPrograms = new HashSet<>();
 
     private String colour;
 
@@ -33,8 +40,25 @@ public class Program extends OrganisationAwareEntity {
         this.colour = colour;
     }
 
+    public Set<OperationalProgram> getOperationalPrograms() {
+        return operationalPrograms;
+    }
+
+    public void setOperationalPrograms(Set<OperationalProgram> operationalPrograms) {
+        this.operationalPrograms = operationalPrograms;
+    }
+
+    @JsonIgnore
+    public String getOperationalProgramName() {
+        return operationalPrograms.stream().map(OperationalProgram::getName).findFirst().orElse(null);
+    }
+
     @Projection(name = "ProgramProjection", types = {Program.class})
     public interface ProgramProjection extends BaseProjection {
         String getName();
+
+        String getColour();
+
+        String getOperationalProgramName();
     }
 }
