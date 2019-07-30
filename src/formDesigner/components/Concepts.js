9132dfb309bb619ba6b2d09d21f52604458846ec
Swapnil106111 <swap106111@gmail.com>
2019-07-30 16:22:58 +0530

    Replace remote data with single call API.

--- src/formDesigner/components/Concepts.js
+++ src/formDesigner/components/Concepts.js
@@ -12,22 +12,24 @@ class Concepts extends Component {
         { title: 'DataType', field: 'dataType' },
         { title: 'UUID', field: 'uuid', type: 'numeric' },
       ],
-      finalData : []
+      data : []
     }
   }
 
   componentDidMount(){
     axios
-    .get("/concept?size=10")
+    .get("/concept?size=1000") //Need to change API here to get user specific concepts
     .then((response)=>{
         const result = response.data._embedded.concept;
         const data1 = [];
-        console.log("Component Did Mount",result)
-       
+       for(var i=0;i<result.length;i++){
+         if(!result[i].voided){
+           data1.push(result[i])
+         }
+       }
 
         this.setState({
           data:data1,
-          totalCount : response.data.page.totalElements
         })
     })
     .catch((error) =>{
@@ -42,36 +44,7 @@ class Concepts extends Component {
         <MaterialTable
           title=''
           columns={this.state.columns}
-          
-          data={query =>
-            new Promise((resolve, reject) => {
-              const filteredVoided = [];
-              let url = '/concept?'
-              url += 'size=' + query.pageSize
-              url += '&page=' + (query.page + 1)
-              fetch(url)
-                .then(response => response.json())
-                .then(result => {
-
-                  for (var i=0;i<result._embedded.concept.length;i++){
-
-                    // Handling voided true concepts below. Once API starts returning data with voided false then we can remove if statement.
-                    if(!result._embedded.concept[i].voided){
-                      filteredVoided.push({name: result._embedded.concept[i].name, dataType:result._embedded.concept[i].dataType, uuid:result._embedded.concept[i].uuid })
-                    }
-                }
-                this.setState({
-                  finalData : filteredVoided,
-                })
-                  resolve({
-                    data: this.state.finalData,
-                    page: result.page.number - 1,
-                    totalCount: result.page.totalElements,
-                  })
-                })
-            })
-          }
-          
+          data={this.state.data}
           options ={{
             pageSize : 10,
             pageSizeOptions :[10, 15, 20],
@@ -99,37 +72,25 @@ class Concepts extends Component {
             //   }),
             onRowDelete: oldData =>
               new Promise(resolve => {
-                const data = this.state.finalData
                 setTimeout(() => {
-                  
+                  resolve();
                   axios
                   .post('/concepts',[{
                     uuid: oldData.uuid,
                     voided: true,
                     }
                     ])
-                  .then(function (response) {
-                    console.log(response)
+                  .then((response)=> {
+                    console.log(response);
                     if(response.status===200){
-                      for (var i=0;i<data.length;i++){
-                        if(data[i].uuid===oldData.uuid){
-                            data.splice(i,1)
-                        }
-                      }
-                      resolve({
-                        data: data
-                      });
-
-                    }
-                        
-                    })
-                  .catch(function (error) {
-                        console.log(error);
-                  });
+                      const data = [...this.state.data];
+                      data.splice(data.indexOf(oldData), 1);
+                      this.setState({ ...this.state, data });
+                    }             
                 }, 600);
-                
-              }),
-          }}
+              })
+            })
+        }}
         />
       </div>
     );
