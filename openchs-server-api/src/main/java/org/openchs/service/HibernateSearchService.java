0059a861d21f45dfbf7ca642737c71404aabeed8
Hiren Thacker <hithacker@gmail.com>
2019-08-22 14:52:22 +0530

    #21 | Improve /search/concept api

--- openchs-server-api/src/main/java/org/openchs/service/HibernateSearchService.java
+++ openchs-server-api/src/main/java/org/openchs/service/HibernateSearchService.java
@@ -1,34 +1,29 @@
 package org.openchs.service;
 
-import javax.persistence.EntityManager;
-
+import org.apache.lucene.analysis.core.StopAnalyzer;
 import org.apache.lucene.search.Query;
-import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.FullTextEntityManager;
+import org.hibernate.search.jpa.FullTextQuery;
 import org.hibernate.search.jpa.Search;
 import org.hibernate.search.query.dsl.QueryBuilder;
-
+import org.openchs.domain.Concept;
 import org.openchs.web.request.ConceptContract;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
 import java.util.ArrayList;
 import java.util.List;
-import org.openchs.domain.Concept;
 
 @Service
 public class HibernateSearchService {
     private final EntityManager entityManager;
-    private final Logger logger;
 
     @Autowired
     public HibernateSearchService(EntityManager entityManager) {
         super();
         this.entityManager = entityManager;
-        logger = LoggerFactory.getLogger(this.getClass());
     }
 
     void initializeHibernateSearch() {
@@ -38,28 +33,35 @@ public class HibernateSearchService {
 
     @Transactional
     public List<ConceptContract> searchConcepts(String searchTerm, String dataType) {
+
+        if (searchTerm.length() < 2 || StopAnalyzer.ENGLISH_STOP_WORDS_SET.contains(searchTerm))
+            return new ArrayList<>();
+
         FullTextEntityManager fullTextEntityManager = Search.getFullTextEntityManager(entityManager);
 
         QueryBuilder qb = fullTextEntityManager.getSearchFactory()
-                                                .buildQueryBuilder()
-                                                .forEntity(Concept.class).get();
+                .buildQueryBuilder()
+                .forEntity(Concept.class)
+                .overridesForField("name", "edgeNGram_query")
+                .get();
 
-        Query luceneQuery = qb.keyword()
-                            .onFields("name")
-                            .matching(searchTerm)
-                            .createQuery();
+        Query luceneQuery = qb
+                .phrase()
+                .withSlop(3)
+                .onField("name")
+                .sentence(searchTerm)
+                .createQuery();
 
         FullTextQuery jpaQuery = fullTextEntityManager.createFullTextQuery(luceneQuery, Concept.class);
 
         List resultList = jpaQuery.getResultList();
         List<ConceptContract> searchResult = new ArrayList<>();
         resultList.forEach(o -> {
-            if(dataType == null) {
+            if (dataType == null) {
                 searchResult.add(((Concept) o).toConceptContract());
-            }
-            else {
+            } else {
                 Concept concept = (Concept) o;
-                if(concept.getDataType().equals(dataType)) {
+                if (concept.getDataType().equals(dataType)) {
                     searchResult.add(((Concept) o).toConceptContract());
                 }
             }
