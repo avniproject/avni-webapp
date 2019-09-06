19dfd0e8d19b53157a626291c49df552c0e786d6
Vinod Deolal <vinoddeolal@gmail.com>
2019-09-06 17:49:05 +0530

    #189 | add IndividualEncounterCancellation form type

--- openchs-server-api/src/main/java/org/openchs/application/FormType.java
+++ openchs-server-api/src/main/java/org/openchs/application/FormType.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 
 public enum FormType {
     BeneficiaryIdentification,
+    IndividualEncounterCancellation,
     IndividualProfile,
     Encounter,
     ProgramEncounter,
