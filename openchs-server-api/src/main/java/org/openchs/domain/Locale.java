6e0afff04619292ec8a86821c9aadad3512edad4
Vinod Deolal <vinoddeolal@gmail.com>
2019-09-23 13:06:16 +0530

    0000 | add more locale options

--- openchs-server-api/src/main/java/org/openchs/domain/Locale.java
+++ openchs-server-api/src/main/java/org/openchs/domain/Locale.java
@@ -4,5 +4,14 @@ public enum Locale {
     en,
     hi_IN,
     mr_IN,
-    gu_IN
+    gu_IN,
+    be_IN,
+    te_IN,
+    ta_IN,
+    ka_IN,
+    od_IN,
+    ma_IN,
+    pu_IN,
+    sa_IN,
+    ur_IN
 }
