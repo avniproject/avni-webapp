b1e14cd78112dae2cfbc07d79683af99a6776cc5
Hiren Thacker <hithacker@gmail.com>
2019-10-18 16:44:30 +0530

    #0 | Try fix styling using solution provided in https://github.com/marmelab/react-admin/issues/1782\#issue-317565973

--- src/adminApp/OrgManager.js
+++ src/adminApp/OrgManager.js
@@ -34,6 +34,36 @@ import Concepts from "../formDesigner/views/Concepts";
 import UploadImpl from "../formDesigner/views/UploadImpl";
 import customRoutes from "./customRoutes";
 import Translations from "../translations";
+import { JssProvider } from "react-jss";
+
+const escapeRegex = /([[\].#*$><+~=|^:(),"'`\s])/g;
+let classCounter = 0;
+
+// Heavily inspired of Material UI:
+// @see https://github.com/mui-org/material-ui/blob/9cf73828e523451de456ba3dbf2ab15f87cf8504/src/styles/createGenerateClassName.js
+// The issue with the MUI function is that is create a new index for each
+// new `withStyles`, so we handle have to write our own counter
+export const generateClassName = (rule, styleSheet) => {
+  classCounter += 1;
+
+  if (process.env.NODE_ENV === "production") {
+    return `c${classCounter}`;
+  }
+
+  if (styleSheet && styleSheet.options.classNamePrefix) {
+    let prefix = styleSheet.options.classNamePrefix;
+    // Sanitize the string as will be used to prefix the generated class name.
+    prefix = prefix.replace(escapeRegex, "-");
+
+    if (prefix.match(/^Mui/)) {
+      return `${prefix}-${rule.key}`;
+    }
+
+    return `${prefix}-${rule.key}-${classCounter}`;
+  }
+
+  return `${rule.key}-${classCounter}`;
+};
 
 class OrgManager extends Component {
   static childContextTypes = {
@@ -47,81 +77,83 @@ class OrgManager extends Component {
   render() {
     const { organisation, user } = this.props;
     return (
-      <Admin
-        title="Manage Organisation"
-        authProvider={authProvider}
-        history={adminHistory}
-        logoutButton={WithProps({ user }, LogoutButton)}
-        customRoutes={customRoutes}
-      >
-        <Resource
-          name="user"
-          list={WithProps({ organisation }, UserList)}
-          create={WithProps({ organisation }, UserCreate)}
-          show={WithProps({ user }, UserDetail)}
-          edit={WithProps({ user }, UserEdit)}
-        />
-        <Resource
-          name="catchment"
-          list={CatchmentList}
-          show={CatchmentDetail}
-          create={CatchmentCreate}
-          edit={CatchmentEdit}
-        />
-        <Resource
-          name="addressLevelType"
-          options={{ label: "Location Types" }}
-          list={LocationTypeList}
-          show={LocationTypeDetail}
-          create={LocationTypeCreate}
-          edit={LocationTypeEdit}
-        />
-        <Resource
-          name="locations"
-          options={{ label: "Locations" }}
-          list={LocationList}
-          show={LocationDetail}
-          create={LocationCreate}
-          edit={LocationEdit}
-        />
-        <Resource
-          name="program"
-          options={{ label: "Programs" }}
-          list={ProgramList}
-          show={ProgramDetail}
-          create={ProgramCreate}
-          edit={ProgramEdit}
-        />
-        <Resource
-          name="subjectType"
-          options={{ label: "Subject Types" }}
-          list={SubjectTypeList}
-          show={SubjectTypeDetail}
-          create={SubjectTypeCreate}
-          edit={SubjectTypeEdit}
-        />
-        <Resource
-          name="encounterType"
-          options={{ label: "Encounter Types" }}
-          list={EncounterTypeList}
-          show={EncounterTypeDetail}
-          create={EncounterTypeCreate}
-          edit={EncounterTypeEdit}
-        />
-        <Resource
-          name="organisationConfig"
-          options={{ label: "Organisation Config" }}
-          list={customConfig}
-        />
-        <Resource name="forms" options={{ label: "Forms" }} list={Forms} />
-        <Resource name="concepts" options={{ label: "Concepts" }} list={Concepts} />
-        <Resource name="upload" options={{ label: "Bundle" }} list={UploadImpl} />
-        <Resource
-          name="translations"
-          options={{ label: "Translations" }}
-          list={WithProps({ user, organisation }, Translations)}
-        />
-      </Admin>
+      <JssProvider generateClassName={generateClassName}>
+        <Admin
+          title="Manage Organisation"
+          authProvider={authProvider}
+          history={adminHistory}
+          logoutButton={WithProps({ user }, LogoutButton)}
+          customRoutes={customRoutes}
+        >
+          <Resource
+            name="user"
+            list={WithProps({ organisation }, UserList)}
+            create={WithProps({ organisation }, UserCreate)}
+            show={WithProps({ user }, UserDetail)}
+            edit={WithProps({ user }, UserEdit)}
+          />
+          <Resource
+            name="catchment"
+            list={CatchmentList}
+            show={CatchmentDetail}
+            create={CatchmentCreate}
+            edit={CatchmentEdit}
+          />
+          <Resource
+            name="addressLevelType"
+            options={{ label: "Location Types" }}
+            list={LocationTypeList}
+            show={LocationTypeDetail}
+            create={LocationTypeCreate}
+            edit={LocationTypeEdit}
+          />
+          <Resource
+            name="locations"
+            options={{ label: "Locations" }}
+            list={LocationList}
+            show={LocationDetail}
+            create={LocationCreate}
+            edit={LocationEdit}
+          />
+          <Resource
+            name="program"
+            options={{ label: "Programs" }}
+            list={ProgramList}
+            show={ProgramDetail}
+            create={ProgramCreate}
+            edit={ProgramEdit}
+          />
+          <Resource
+            name="subjectType"
+            options={{ label: "Subject Types" }}
+            list={SubjectTypeList}
+            show={SubjectTypeDetail}
+            create={SubjectTypeCreate}
+            edit={SubjectTypeEdit}
+          />
+          <Resource
+            name="encounterType"
+            options={{ label: "Encounter Types" }}
+            list={EncounterTypeList}
+            show={EncounterTypeDetail}
+            create={EncounterTypeCreate}
+            edit={EncounterTypeEdit}
+          />
+          <Resource
+            name="organisationConfig"
+            options={{ label: "Organisation Config" }}
+            list={customConfig}
+          />
+          <Resource name="forms" options={{ label: "Forms" }} list={Forms} />
+          <Resource name="concepts" options={{ label: "Concepts" }} list={Concepts} />
+          <Resource name="upload" options={{ label: "Bundle" }} list={UploadImpl} />
+          <Resource
+            name="translations"
+            options={{ label: "Translations" }}
+            list={WithProps({ user, organisation }, Translations)}
+          />
+        </Admin>
+      </JssProvider>
     );
   }
 }
