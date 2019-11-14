03defdb2617e864e07c51b83bd6e7556d45348e0
Hiren Thacker <hithacker@gmail.com>
2019-11-14 14:39:31 +0530

    #0 | making code easier to understand

--- src/rootApp/saga.js
+++ src/rootApp/saga.js
@@ -1,11 +1,5 @@
 import { call, put, take, takeLatest } from "redux-saga/effects";
-import {
-  getUserInfo,
-  sendAuthConfigured,
-  sendInitComplete,
-  setUserInfo,
-  types
-} from "./ducks";
+import { getUserInfo, sendAuthConfigured, sendInitComplete, setUserInfo, types } from "./ducks";
 import {
   cognitoConfig as cognitoConfigFromEnv,
   cognitoInDev,
@@ -18,21 +12,23 @@ import { configureAuth } from "./utils";
 const api = {
   fetchCognitoDetails: () =>
     httpClient.fetchJson("/cognito-details").then(response => response.json),
-  fetchUserInfo: () =>
-    httpClient.fetchJson("/me").then(response => response.json)
+  fetchUserInfo: () => httpClient.fetchJson("/me").then(response => response.json)
 };
 
 export function* initialiseCognito() {
-  if (!(isProdEnv || cognitoInDev)) return;
-  yield take(types.INIT_COGNITO);
-  try {
-    const cognitoDetails = cognitoInDev
-      ? cognitoConfigFromEnv
-      : yield call(api.fetchCognitoDetails);
-    yield call(configureAuth, cognitoDetails);
-    yield put(sendAuthConfigured());
-  } catch (e) {
-    yield call(alert, e);
+  if (isProdEnv || cognitoInDev) {
+    yield take(types.INIT_COGNITO);
+    try {
+      const cognitoDetails = cognitoInDev
+        ? cognitoConfigFromEnv
+        : yield call(api.fetchCognitoDetails);
+      yield call(configureAuth, cognitoDetails);
+      yield put(sendAuthConfigured());
+    } catch (e) {
+      yield call(alert, e);
+    }
+  } else {
+    return;
   }
 }
 
