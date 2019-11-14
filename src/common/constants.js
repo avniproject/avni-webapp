89da77d55ccd3cb520bc9c8613a0f07c4ba76d9c
Hiren Thacker <hithacker@gmail.com>
2019-11-14 16:09:18 +0530

    #95 | ability to run compiled build on local machine

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
-export const isDevEnv = process.env.NODE_ENV === "development";
+export const isDevEnv = process.env.REACT_APP_ENVIRONMENT === "development";
 export const cognitoInDev = isDevEnv && process.env.REACT_APP_COGNITO_IN_DEV === "true";
-export const isProdEnv = process.env.NODE_ENV === "production";
+export const isProdEnv = process.env.REACT_APP_ENVIRONMENT === "production";
 
 export const AWS_REGION = process.env.REACT_APP_AWS_REGION || "ap-south-1";
 
