ba6e2e41baf7c2726513a2644f9eb0cc509ce175
Rashmi <rashmi@example.com>
2019-12-10 02:30:14 +0530

    [DataEntryApp] [RegistrationScreen] UI changes
    introduce autosuggest address field in subject registration

--- src/dataEntryApp/components/DateOfBirth.js
+++ src/dataEntryApp/components/DateOfBirth.js
@@ -1,17 +1,9 @@
 import React, { Fragment } from "react";
-import { Box, Checkbox, FormControlLabel, TextField } from "@material-ui/core";
+import { Box, TextField } from "@material-ui/core";
 import moment from "moment/moment";
+import { LineBreak } from "../../../src/common/components/utils";
 
-function isValidMonth(month) {
-  return 0 <= month && month <= 12;
-}
-
-export const DateOfBirth = ({
-  dateOfBirth,
-  dateOfBirthVerified,
-  onChange,
-  markVerified
-}) => {
+export const DateOfBirth = ({ dateOfBirth, onChange }) => {
   const dob = dateOfBirth && dateOfBirth.toISOString().substr(0, 10);
   const [years, setYears] = React.useState("");
   const [months, setMonths] = React.useState("");
@@ -41,79 +33,30 @@ export const DateOfBirth = ({
     );
   };
 
-  const _onMonthsChange = value => {
-    setMonths(value);
-    isValidMonth(value) &&
-      onChange(
-        moment()
-          .subtract(years, "years")
-          .subtract(value, "months")
-          .toDate()
-      );
-  };
-
   return (
     <Fragment>
-      <Box display="flex" flexWrap="wrap" alignContent="flex-start">
-        <Box p={1}>
-          <TextField
-            label="Date of Birth"
-            type="date"
-            required
-            name="dateOfBirth"
-            value={dob}
-            onChange={e => _onChange(e.target.value)}
-            InputLabelProps={{
-              shrink: true
-            }}
-          />
-        </Box>
-        <Box
-          display="flex"
-          flexWrap="wrap"
-          alignContent="flex-end"
-          p={1}
-          px={2}
-        >
-          <Box>OR</Box>
-        </Box>
-        <Box p={1}>
-          <TextField
-            label={"Years"}
-            type={"numeric"}
-            name={"ageYearsPart"}
-            value={years}
-            onChange={e => _onYearsChange(e.target.value)}
-          />
-        </Box>
-        <Box p={1}>
-          <TextField
-            label={"Months"}
-            type={"numeric"}
-            name={"ageMonthsPart"}
-            value={months}
-            error={!isValidMonth(months)}
-            helperText={
-              isValidMonth(months) ? "" : "Months must be between 0 and 12"
-            }
-            onChange={e => _onMonthsChange(e.target.value)}
-          />
-        </Box>
-        <Box p={1}>
-          <FormControlLabel
-            control={
-              <Checkbox
-                checked={dateOfBirthVerified}
-                onChange={e => {
-                  markVerified(e.target.checked);
-                }}
-                value="checkedB"
-                color="primary"
-              />
-            }
-            label="Date of Birth Verified"
-          />
-        </Box>
+      <Box display="flex" flexDirection="column">
+        <TextField
+          label="Date of Birth"
+          type="date"
+          required
+          style={{ width: "30%" }}
+          name="dateOfBirth"
+          value={dob}
+          onChange={e => _onChange(e.target.value)}
+          InputLabelProps={{
+            shrink: true
+          }}
+        />
+        <LineBreak num={1} />
+        <TextField
+          label={"Age"}
+          type={"numeric"}
+          name={"ageYearsPart"}
+          value={years}
+          style={{ width: "30%" }}
+          onChange={e => _onYearsChange(e.target.value)}
+        />
       </Box>
     </Fragment>
   );
