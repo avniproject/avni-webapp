971255809d6b169f876085615af08bdd7e991efa
Rashmi <rashmi@example.com>
2019-12-13 11:14:38 +0530

    extract breadcrumbs to separate component

--- src/dataEntryApp/views/subjectDashBoard/SubjectDashboard.js
+++ src/dataEntryApp/views/subjectDashBoard/SubjectDashboard.js
@@ -1,69 +1,53 @@
-import React, { Fragment, useEffect } from 'react';
-import Typography from '@material-ui/core/Typography';
-import Breadcrumbs from '@material-ui/core/Breadcrumbs';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import { makeStyles } from '@material-ui/core/styles';
-import ProfileDetails from './components/ProfileDetails';
-import TabsPanel from './components/TabsPanel';
+import React, { Fragment, useEffect } from "react";
+import Paper from "@material-ui/core/Paper";
+import { makeStyles } from "@material-ui/core/styles";
+import ProfileDetails from "./components/ProfileDetails";
+import TabsPanel from "./components/TabsPanel";
 import { getSubjectProfile } from "../../reducers/subjectDashboardReducer";
 import { withRouter } from "react-router-dom";
 import { connect } from "react-redux";
-import { withParams } from 'common/components/utils';
+import { withParams } from "common/components/utils";
+import Breadcrumbs from "dataEntryApp/components/Breadcrumbs";
 
 const useStyles = makeStyles(theme => ({
-    root: {
-        padding: theme.spacing(3, 2),
-        margin: theme.spacing(4),
-        flexGrow: 1
-    },
-    Breadcrumbs: {
-        margin: theme.spacing(4)
-    }
+  root: {
+    padding: theme.spacing(3, 2),
+    margin: theme.spacing(4),
+    flexGrow: 1
+  }
 }));
 
-function breadcrumbHandler(event) {
-    event.preventDefault();
-    console.info('You clicked a breadcrumb.');
-}
+const SubjectDashboard = ({ match, getSubjectProfile, subjectProfile }) => {
+  const classes = useStyles();
 
-const SubjectDashboard = ({  match, getSubjectProfile,subjectProfile }) => {
-   
-    const classes = useStyles();
+  useEffect(() => {
+    getSubjectProfile(match.queryParams.uuid);
+  }, []);
 
-    useEffect(()=> {
-        getSubjectProfile(match.queryParams.uuid);
-    }, []);
+  return (
+    <Fragment>
+      <Breadcrumbs />
+      <Paper className={classes.root}>
+        <ProfileDetails />
+        <TabsPanel />
+      </Paper>
+    </Fragment>
+  );
+};
 
-    return (
-        <Fragment>
-            <Breadcrumbs className={classes.Breadcrumbs} aria-label="breadcrumb">
-                <Link color="inherit" href="/" onClick={breadcrumbHandler}>
-                    Home
-      </Link>
-                <Typography color="textPrimary">Shilpa Ingle's Profile</Typography>
-            </Breadcrumbs>
-            <Paper className={classes.root}>
-                <ProfileDetails />
-                <TabsPanel />
-            </Paper>
-        </Fragment>
-    );
-}
-
-
-const mapStateToProps = (state) => ({
-    subjectProfile: state.dataEntry.subjectProfile.subjectProfile
+const mapStateToProps = state => ({
+  subjectProfile: state.dataEntry.subjectProfile.subjectProfile
 });
 
 const mapDispatchToProps = {
-    getSubjectProfile
+  getSubjectProfile
 };
 
-
-export default withRouter(withParams(
+export default withRouter(
+  withParams(
     connect(
-        mapStateToProps,
-        mapDispatchToProps
+      mapStateToProps,
+      mapDispatchToProps
     )(SubjectDashboard)
-));
+  )
+);
