909edbe70bca3fcb45bbff0c629b12592c60d284
Rashmi <rashmi@example.com>
2019-12-13 11:14:38 +0530

    extract breadcrumbs to separate component

---
+++ src/dataEntryApp/components/Breadcrumbs.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+import Breadcrumbs from "@material-ui/core/Breadcrumbs";
+import Link from "@material-ui/core/Link";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(3, 2),
+    margin: theme.spacing(4),
+    flexGrow: 1
+  },
+  Breadcrumbs: {
+    margin: theme.spacing(4)
+  }
+}));
+
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <Breadcrumbs className={classes.Breadcrumbs} aria-label="breadcrumb">
+      <Link color="inherit" href="/" onClick={() => {}}>
+        Home
+      </Link>
+      <Typography color="textPrimary">Shilpa Ingle's Profile</Typography>
+    </Breadcrumbs>
+  );
+};
