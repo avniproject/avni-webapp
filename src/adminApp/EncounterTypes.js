d393bd0cb2f4aad179d9fd5704fc7dad0a35c335
swapnil106111 <swap106111@gmail.com>
2019-12-20 11:57:20 +0530

    #1087 | Add support for EncounterEligibilityCheck rule in UI designer

--- src/adminApp/EncounterTypes.js
+++ src/adminApp/EncounterTypes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Datagrid,
   List,
@@ -8,8 +8,18 @@ import {
   Create,
   Edit,
   SimpleForm,
-  TextInput
+  TextInput,
+  REDUX_FORM_NAME,
+  FormDataConsumer
 } from "react-admin";
+import Editor from "react-simple-code-editor";
+import { highlight, languages } from "prismjs/components/prism-core";
+import "prismjs/components/prism-clike";
+import "prismjs/components/prism-javascript";
+import { change } from "redux-form";
+import FormLabel from "@material-ui/core/FormLabel";
+import Box from "@material-ui/core/Box";
+
 
 const Title = ({ record }) => {
   return (
@@ -20,6 +30,65 @@ const Title = ({ record }) => {
     )
   );
 };
+
+export const RuleEditor = props => {
+  const [ruleCode, setRuleCode] = useState(
+    props.record.encounterEligibilityCheckRule ? props.record.encounterEligibilityCheckRule : ""
+  );
+
+
+  return (
+    <FormDataConsumer>
+      {({ formData, dispatch, ...rest }) => (
+        <>
+          <Box mt={3}>
+            <FormLabel component="legend">Encounter Eligibility Check Rule</FormLabel>
+            <Editor
+              value={ruleCode}
+              onValueChange={value => {
+                dispatch(change(REDUX_FORM_NAME, "encounterEligibilityCheckRule", value));
+                setRuleCode(value);
+              }}
+              highlight={code => highlight(code, languages.js)}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 15,
+                height: "auto",
+                borderStyle: "solid",
+                borderWidth: "1px"
+              }}
+            />
+          </Box>
+        </>
+      )}
+    </FormDataConsumer>
+  );
+};
+
+export const RuleDisplay = props => {
+  return (
+    <>
+      <Box mt={3}>
+        <FormLabel component="legend">Encounter Eligibility Check Rule</FormLabel>
+        <Editor
+          value={props.record.encounterEligibilityCheckRule ? props.record.encounterEligibilityCheckRule : ""}
+          readOnly={true}
+          highlight={code => highlight(code, languages.js)}
+          padding={10}
+          style={{
+            fontFamily: '"Fira code", "Fira Mono", monospace',
+            fontSize: 15,
+            height: "auto",
+            borderStyle: "solid",
+            borderWidth: "1px"
+          }}
+        />
+      </Box>
+    </>
+  );
+};
+
 
 export const EncounterTypeList = props => (
   <List {...props} bulkActions={false}>
@@ -36,6 +105,7 @@ export const EncounterTypeDetail = props => {
       <SimpleShowLayout>
         <TextField source="name" label="Name" />
         <TextField label="Organisation Id" source="encounterTypeOrganisationId" />
+        <RuleDisplay {...props} />
       </SimpleShowLayout>
     </Show>
   );
@@ -46,6 +116,8 @@ export const EncounterTypeCreate = props => {
     <Create title="Add a new Encounter" {...props}>
       <SimpleForm>
         <TextInput source="name" />
+        <TextInput source="enrolmentEligibilityCheckRule" hidden={true} />
+        <RuleEditor {...props} />
       </SimpleForm>
     </Create>
   );
@@ -56,6 +128,8 @@ export const EncounterTypeEdit = props => {
     <Edit undoable={false} title="Edit Encounter" {...props}>
       <SimpleForm>
         <TextInput source="name" />
+        <TextInput source="enrolmentEligibilityCheckRule" hidden={true} />
+        <RuleEditor {...props} />
       </SimpleForm>
     </Edit>
   );
