5678751b43fb13b147d28eb78a7504b6ec68e287
sidtharthan <sidtharthann@samanvayfoundation.org>
2019-12-24 14:23:38 +0530

    #0 | Update openchs_impl user creation migration script
    Update organisation table properly when db is rebuild in dev environment

---
+++ openchs-server-api/src/main/resources/db/migration/V1_133__UpdateOpenchsImplUserCreateScript.sql
@@ -0,0 +1,13 @@
+DO $$
+BEGIN
+  IF NOT EXISTS(SELECT 1 FROM pg_roles WHERE rolname = 'openchs_impl')
+  THEN
+    DROP ROLE IF EXISTS openchs_impl;
+    CREATE ROLE openchs_impl
+      NOINHERIT
+      NOLOGIN
+      NOBYPASSRLS;
+    GRANT openchs_impl TO openchs;
+  END IF;
+  UPDATE organisation SET db_user = 'openchs_impl' where name = 'OpenCHS';
+END$$;
