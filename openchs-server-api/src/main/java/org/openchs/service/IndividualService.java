15b8c383c66f5b8df1fd0e461d414d28bb276580
amolnirmal <amol_nirmal@persistent.com>
2020-01-01 12:31:42 +0530

    Comment added for Program Enrollment

--- openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
+++ openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
@@ -32,6 +32,7 @@ public class IndividualService {
         this.individualRepository = individualRepository;
     }
 
+    //Get Progam Enrollment Information
     public  IndividualContract getSubjectProgramEnrollment(String individualUuid){
         Individual individual = individualRepository.findByUuid(individualUuid);
         if (!Objects.nonNull(individual)) {
