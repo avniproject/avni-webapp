38c0add808814c89d464ab7dfdb6092b3793d246
amarkamthe <amarkamthe123@gmail.com>
2020-01-30 11:52:00 +0530

    #177 - Added get API's for identifier user assignment

--- openchs-server-api/src/main/java/org/openchs/web/IdentifierUserAssignmentController.java
+++ openchs-server-api/src/main/java/org/openchs/web/IdentifierUserAssignmentController.java
@@ -11,12 +11,18 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
 
 @RestController
-public class IdentifierUserAssignmentController extends AbstractController<IdentifierUserAssignment> {
+public class IdentifierUserAssignmentController extends AbstractController<IdentifierUserAssignment> implements RestControllerResourceProcessor<IdentifierUserAssignment> {
     private IdentifierUserAssignmentRepository identifierUserAssignmentRepository;
     private UserRepository userRepository;
     private IdentifierSourceRepository identifierSourceRepository;
@@ -44,6 +50,23 @@ public class IdentifierUserAssignmentController extends AbstractController<Ident
         identifierUserAssignment.setVoided(contract.isVoided());
 
         identifierUserAssignmentRepository.save(identifierUserAssignment);
+    }
+
+    @GetMapping(value = "/web/identifierUserAssignments")
+    @PreAuthorize(value = "hasAnyAuthority('admin','organisation_admin')")
+    @ResponseBody
+    public PagedResources<Resource<IdentifierUserAssignment>> getAll(Pageable pageable) {
+        return wrap(identifierUserAssignmentRepository.findPageByIsVoidedFalse(pageable));
+    }
+
+    @GetMapping(value = "/web/identifierUserAssignments/{id}")
+    @PreAuthorize(value = "hasAnyAuthority('admin','organisation_admin')")
+    @ResponseBody
+    public ResponseEntity getOne(@PathVariable("id") Long id) {
+        IdentifierUserAssignment identifierUserAssignment = identifierUserAssignmentRepository.findOne(id);
+        if (identifierUserAssignment.isVoided())
+            return ResponseEntity.notFound().build();
+        return new ResponseEntity<>(identifierUserAssignment, HttpStatus.OK);
     }
 
 }
