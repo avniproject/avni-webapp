9350559a8aaf3f2786764b6e014e9eb4084aca1c
RamyaEdapalapatiPSL <ramya_edapalapati@persistent.com>
2020-02-24 12:09:54 +0530

    added no details in stepper

--- src/dataEntryApp/views/registration/Stepper.js
+++ src/dataEntryApp/views/registration/Stepper.js
@@ -2,8 +2,9 @@ import React, { Fragment } from "react";
 
 const Stepper = () => {
   return (
-    <Fragment>
+    <Fragment>      
       <h3>Register and enroll - Mother Program</h3>
+      <h3>No Details</h3>
       <h6>Stepper coming soon</h6>
     </Fragment>
   );
