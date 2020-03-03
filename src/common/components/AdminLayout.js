6e91a0ae752076c904b65e700317b37c2e1746cc
swapnil106111 <swap106111@gmail.com>
2020-03-03 11:26:00 +0530

    #224 | Messages are hidden behind the feedback option

--- src/common/components/AdminLayout.js
+++ src/common/components/AdminLayout.js
@@ -1,7 +1,13 @@
 import React from "react";
 import { Layout } from "react-admin";
 import AdminAppBar from "./AdminAppBar";
+import { Notification } from "react-admin";
 
-const AdminLayout = props => <Layout {...props} appBar={AdminAppBar} />;
+const AdminNotification = props => (
+  <Notification {...props} autoHideDuration={5000} style={{ marginBottom: "40px" }} />
+);
+const AdminLayout = props => (
+  <Layout {...props} appBar={AdminAppBar} notification={AdminNotification} />
+);
 
 export default AdminLayout;
