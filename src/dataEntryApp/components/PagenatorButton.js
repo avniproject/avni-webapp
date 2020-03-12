5b43c01186dfb15c69dbce9482008886a77548ae
narendranth-pasupuleti-PSL <narendranath_p@persistent.com>
2020-03-12 11:14:09 +0530

    code changes for pagenatorButton file

--- src/dataEntryApp/components/PagenatorButton.js
+++ src/dataEntryApp/components/PagenatorButton.js
@@ -32,46 +32,12 @@ const useStyles = makeStyles(theme => ({
 
 export default ({ children, ...props }) => {
   let subject = props.formdata;
-
-  const onTestClick=(e)=> {
-    if(subject.subject.firstName === undefined){
-      alert("Please enter First Name")
-      e.preventDefault();   
-      return false;
-    }else if(subject.subject.lastName === undefined){
-      alert("Please enter Last Name")
-      e.preventDefault();   
-      return false;      
-    }else if(subject.subject.dateOfBirth === undefined){
-      alert("Please enter date Of Birth")
-      e.preventDefault();   
-    return false;      
-    }else if(subject.subject.gender.name === ""){
-      alert("Please select gender")
-      e.preventDefault();    
-    return false;      
-    }else if(subject.subject.lowestAddressLevel.title === undefined){
-      alert("Please enter village name")
-      e.preventDefault();   
-    return false;
-    }else{
-      return true;
-    }
-    console.log("12345--->",subject.subject.firstName);
-    console.log("12345--->",subject.subject.lastName);
-    console.log("12345--->",subject.subject.dateOfBirth);
-    console.log("12345--->",subject.subject.gender.name);
-    console.log("12345--->",subject.subject.lowestAddressLevel.title);
-    console.log("12345--->",subject.subject.gender);
-    
-}
-
   const classes = useStyles();
   if(props.type === "text") {
-    return <Typography className={classes.topnav} variant="subtitle1" gutterBottom {...props} onClick={onTestClick}> {children} </Typography>
+    return <Typography className={classes.topnav} variant="subtitle1" gutterBottom {...props}> {children} </Typography>
   }else if(children === "PREVIOUS") {
     return <Button className={classes.privbuttonStyle} type="button" variant="outlined" {...props} >{children} </Button>   
   }else {     
-    return <Button className={classes.nextbuttonStyle} type="button" {...props} onClick={onTestClick}>{children}</Button>
+    return <Button className={classes.nextbuttonStyle} type="button" {...props}>{children}</Button>
   }
 }
