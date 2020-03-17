8de3ac3c7388f36f595640e6d6801e6e7f7cfdc8
RamyaEdapalapatiPSL <ramya_edapalapati@persistent.com>
2020-03-17 17:45:25 +0530

    changes to save observations from session storage

--- src/dataEntryApp/api/browserStore.js
+++ src/dataEntryApp/api/browserStore.js
@@ -1,43 +1,39 @@
-import { Individual, ObservationsHolder, Observation,Concept } from "avni-models";
+import { Individual, ObservationsHolder, Observation, Concept } from "avni-models";
 
 export default class {
   static fetchSubject() {
-    if(sessionStorage.getItem('subject')){                           
-      let subject = Individual.createEmptyInstance();      
-      let localSavedSubject =JSON.parse(sessionStorage.getItem('subject'));    
-      localSavedSubject.uuid = subject.uuid;     
-                     
-
+    if (sessionStorage.getItem('subject')) {
+      let subject = Individual.createEmptyInstance();
+      let localSavedSubject = JSON.parse(sessionStorage.getItem('subject'));
+            
       subject.name = localSavedSubject.name
-      subject.firstName = localSavedSubject.firstName 
-      subject.lastName = localSavedSubject.lastName 
-      subject.dateOfBirth =  localSavedSubject.dateOfBirth 
-      subject.registrationDate  = localSavedSubject.registrationDate 
-      subject.dateOfBirthVerified = localSavedSubject.dateOfBirthVerified 
+      subject.firstName = localSavedSubject.firstName
+      subject.lastName = localSavedSubject.lastName
+      subject.dateOfBirth = localSavedSubject.dateOfBirth
+      subject.registrationDate = localSavedSubject.registrationDate
+      subject.dateOfBirthVerified = localSavedSubject.dateOfBirthVerified
       subject.gender.name = localSavedSubject.gender.name
       subject.gender.uuid = localSavedSubject.gender.uuid
-      subject.lowestAddressLevel = localSavedSubject.lowestAddressLevel      
-      subject.registrationLocation = localSavedSubject.registrationLocation 
-      subject.relationship = localSavedSubject.relationship 
-      subject.subjectType.name = localSavedSubject.subjectType.name    
-      subject.subjectType.uuid = localSavedSubject.subjectType.uuid 
-      
-      
-      
+      subject.lowestAddressLevel = localSavedSubject.lowestAddressLevel
+      subject.registrationLocation = localSavedSubject.registrationLocation
+      subject.relationship = localSavedSubject.relationship
+      subject.subjectType.name = localSavedSubject.subjectType.name
+      subject.subjectType.uuid = localSavedSubject.subjectType.uuid
+
+
       //addOrUpdateObservation
-      
-      const obs = [];              
-      localSavedSubject.observations.map(element =>{        
-          let concept = Concept.create(element.concept.name, element.concept.dataType, element.concept.keyValues, element.concept.uuid)                   
-          obs.push(Observation.create(concept,element.valueJSON,element.abnormal));        
+      const observationHolder = new ObservationsHolder(subject.observations);
+      localSavedSubject.observations.map(element => {
+        let concept = Concept.create(element.concept.name, element.concept.dataType, element.concept.keyValues, element.concept.uuid)
+        observationHolder.addOrUpdateObservation(concept, element.valueJSON.answer)
       })
-      subject.observations = obs;
-      return subject;  
-    }else return;
+      subject.observations = observationHolder.observations;
+      return subject;
+    } else return;
   }
 
   static clear(key) {
-    if(sessionStorage.getItem(key)) {
+    if (sessionStorage.getItem(key)) {
       sessionStorage.clear(key);
     }
   }
