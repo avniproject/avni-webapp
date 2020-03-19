2df42b2a0c92e7af757d391ed1833121445e82c9
swapnil106111 <swap106111@gmail.com>
2020-03-19 11:14:51 +0530

    conflict resolved for voiding form mapping

--- openchs-server-api/src/main/resources/db/migration/V1_146__DropFormIdNotNullConstraint.sql
+++ openchs-server-api/src/main/resources/db/migration/V1_146__DropFormIdNotNullConstraint.sql
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 alter table form_mapping alter column form_id drop not null;
-=======
-alter table form_mapping alter column form_id drop not null;
->>>>>>> 984fb4b38577d16444df8ce125cfa5f25c64cb79
