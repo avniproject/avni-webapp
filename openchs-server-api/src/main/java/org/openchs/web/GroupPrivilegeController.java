f94d571461b91c8c3d7ad439782680e609a115b6
Joy A <itsjoy@gmail.com>
2020-04-03 09:48:35 +0530

    #56 | Group Privileges - Incorporate code review comments

--- openchs-server-api/src/main/java/org/openchs/web/GroupPrivilegeController.java
+++ openchs-server-api/src/main/java/org/openchs/web/GroupPrivilegeController.java
@@ -1,11 +1,11 @@
 package org.openchs.web;
 
-import org.apache.commons.collections4.IterableUtils;
-import org.openchs.application.FormMapping;
 import org.openchs.dao.*;
-import org.openchs.dao.application.FormMappingRepository;
-import org.openchs.domain.*;
-import org.openchs.framework.security.UserContextHolder;
+import org.openchs.domain.Group;
+import org.openchs.domain.GroupPrivilege;
+import org.openchs.domain.Privilege;
+import org.openchs.domain.SubjectType;
+import org.openchs.service.GroupPrivilegeService;
 import org.openchs.web.request.GroupPrivilegeContract;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -15,7 +15,6 @@ import javax.transaction.Transactional;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,9 +26,9 @@ public class GroupPrivilegeController extends AbstractController<GroupPrivilege>
     private ProgramRepository programRepository;
     private EncounterTypeRepository encounterTypeRepository;
     private ChecklistDetailRepository checklistDetailRepository;
-    private FormMappingRepository formMappingRepository;
+    private GroupPrivilegeService groupPrivilegeService;
 
-    public GroupPrivilegeController(GroupPrivilegeRepository groupPrivilegeRepository, GroupRepository groupRepository, PrivilegeRepository privilegeRepository, SubjectTypeRepository subjectTypeRepository, ProgramRepository programRepository, EncounterTypeRepository encounterTypeRepository, ChecklistDetailRepository checklistDetailRepository, FormMappingRepository formMappingRepository) {
+    public GroupPrivilegeController(GroupPrivilegeRepository groupPrivilegeRepository, GroupRepository groupRepository, PrivilegeRepository privilegeRepository, SubjectTypeRepository subjectTypeRepository, ProgramRepository programRepository, EncounterTypeRepository encounterTypeRepository, ChecklistDetailRepository checklistDetailRepository, GroupPrivilegeService groupPrivilegeService) {
         this.groupPrivilegeRepository = groupPrivilegeRepository;
         this.groupRepository = groupRepository;
         this.privilegeRepository = privilegeRepository;
@@ -37,13 +36,13 @@ public class GroupPrivilegeController extends AbstractController<GroupPrivilege>
         this.programRepository = programRepository;
         this.encounterTypeRepository = encounterTypeRepository;
         this.checklistDetailRepository = checklistDetailRepository;
-        this.formMappingRepository = formMappingRepository;
+        this.groupPrivilegeService = groupPrivilegeService;
     }
 
     @RequestMapping(value = "/groups/{id}/privileges", method = RequestMethod.GET)
     @PreAuthorize(value = "hasAnyAuthority('organisation_admin', 'admin')")
     public List<GroupPrivilegeContract> getById(@PathVariable("id") Long id) {
-        List<GroupPrivilege> allPrivileges = getAllGroupPrivileges(id);
+        List<GroupPrivilege> allPrivileges = groupPrivilegeService.getAllGroupPrivileges(id);
         List<GroupPrivilege> groupPrivileges = groupPrivilegeRepository.findByGroup_Id(id);
         groupPrivileges.addAll(allPrivileges);
         return groupPrivileges.stream()
@@ -94,103 +93,4 @@ public class GroupPrivilegeController extends AbstractController<GroupPrivilege>
         return ResponseEntity.ok(groupPrivilegeRepository.saveAll(privilegesToBeAddedOrUpdated));
     }
 
-    private List<GroupPrivilege> getAllGroupPrivileges(Long groupId) {
-
-        List<FormMapping> formMappings = formMappingRepository.findAll();
-        List<SubjectType.SubjectTypeProjection> subjectTypes = subjectTypeRepository.findAllOperational();
-
-        List<Program.ProgramProjection> operationalPrograms = programRepository.findAllOperational();
-        Set<Long> operationalProgramIds = operationalPrograms.stream().map(Program.ProgramProjection::getId).collect(Collectors.toSet());
-
-        List<EncounterType.EncounterTypeProjection> operationalEncounterTypes = encounterTypeRepository.findAllOperational();
-        Set<Long> operationalEncounterTypeIds = operationalEncounterTypes.stream().map(EncounterType.EncounterTypeProjection::getId).collect(Collectors.toSet());
-
-        List<ChecklistDetail> checklistDetails = checklistDetailRepository.findAllByOrganisationId(UserContextHolder.getUserContext().getOrganisationId());
-
-        Group currentGroup = groupRepository.findOne(groupId);
-        List<Privilege> privilegeList = IterableUtils.toList(privilegeRepository.findAll());
-
-        List<FormMapping> operationalFormMappings = formMappings.stream()
-                .filter(formMapping -> (formMapping.getProgram() == null) || (formMapping.getProgram() != null && operationalProgramIds.contains(formMapping.getProgram().getId())))
-                .filter(formMapping -> formMapping.getEncounterType() != null && operationalEncounterTypeIds.contains(formMapping.getEncounterType().getId()))
-                .collect(Collectors.toList());
-
-        List<GroupPrivilege> allPrivileges = new ArrayList<>();
-
-        subjectTypes.forEach(subjectTypeProjection -> {
-            SubjectType subjectType = subjectTypeRepository.findByUuid(subjectTypeProjection.getUuid());
-            privilegeList.stream()
-                    .filter(privilege -> privilege.getEntityType() == EntityType.Subject)
-                    .forEach(subjectPrivilege -> {
-                                GroupPrivilege groupPrivilege = new GroupPrivilege();
-                                groupPrivilege.setGroup(currentGroup);
-                                groupPrivilege.setPrivilege(subjectPrivilege);
-                                groupPrivilege.setSubjectType(subjectType);
-                                groupPrivilege.setAllow(false);
-                                allPrivileges.add(groupPrivilege);
-                            }
-                    );
-
-            operationalFormMappings.forEach(operationalFormMapping -> {
-                if (operationalFormMapping.getSubjectType() != subjectType) return;
-
-                Program program = operationalFormMapping.getProgram();
-                if (program != null) {
-                    privilegeList.stream()
-                            .filter(privilege -> privilege.getEntityType() == EntityType.Enrolment)
-                            .forEach(enrolmentPrivilege -> {
-                                GroupPrivilege groupPrivilege = new GroupPrivilege();
-                                groupPrivilege.setGroup(currentGroup);
-                                groupPrivilege.setPrivilege(enrolmentPrivilege);
-                                groupPrivilege.setSubjectType(subjectType);
-                                groupPrivilege.setProgram(program);
-                                groupPrivilege.setAllow(false);
-                                allPrivileges.add(groupPrivilege);
-                            });
-
-                    EncounterType encounterType = operationalFormMapping.getEncounterType();
-
-                    privilegeList.stream()
-                            .filter(privilege -> privilege.getEntityType() == EntityType.Encounter)
-                            .forEach(encounterPrivilege -> {
-                                GroupPrivilege groupPrivilege = new GroupPrivilege();
-                                groupPrivilege.setGroup(currentGroup);
-                                groupPrivilege.setPrivilege(encounterPrivilege);
-                                groupPrivilege.setSubjectType(subjectType);
-                                groupPrivilege.setProgram(program);
-                                groupPrivilege.setProgramEncounterType(encounterType);
-                                groupPrivilege.setAllow(false);
-                                allPrivileges.add(groupPrivilege);
-                            });
-                } else {
-                    privilegeList.stream()
-                            .filter(privilege -> privilege.getEntityType() == EntityType.Encounter)
-                            .forEach(encounterPrivilege -> {
-                                GroupPrivilege groupPrivilege = new GroupPrivilege();
-                                groupPrivilege.setGroup(currentGroup);
-                                groupPrivilege.setPrivilege(encounterPrivilege);
-                                groupPrivilege.setSubjectType(subjectType);
-                                groupPrivilege.setEncounterType(operationalFormMapping.getEncounterType());
-                                groupPrivilege.setAllow(false);
-                                allPrivileges.add(groupPrivilege);
-                            });
-                }
-            });
-
-            checklistDetails.forEach(checklistDetail ->
-                    privilegeList.stream()
-                            .filter(privilege -> privilege.getEntityType() == EntityType.Checklist)
-                            .forEach(privilege -> {
-                                GroupPrivilege groupPrivilege = new GroupPrivilege();
-                                groupPrivilege.setGroup(currentGroup);
-                                groupPrivilege.setPrivilege(privilege);
-                                groupPrivilege.setSubjectType(subjectType);
-                                groupPrivilege.setChecklistDetail(checklistDetail);
-                                allPrivileges.add(groupPrivilege);
-                            })
-            );
-        });
-
-        return allPrivileges;
-    }
 }
