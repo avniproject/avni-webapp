50b33f545b4b92663af78b61ff60d74d830bd3e7
eshwar <eshwar_joshi@persistent.com>
2020-04-08 21:36:49 +0530

    date and time picker added

--- src/dataEntryApp/components/DateFormElement.js
+++ src/dataEntryApp/components/DateFormElement.js
@@ -1,8 +1,10 @@
 import React, { Fragment } from "react";
-import { TextField } from "@material-ui/core";
-import { isEmpty } from "lodash";
 import DateFnsUtils from "@date-io/date-fns";
-import { MuiPickersUtilsProvider, KeyboardDateTimePicker } from "@material-ui/pickers";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardDateTimePicker,
+  KeyboardDatePicker
+} from "@material-ui/pickers";
 
 export const DateTimeFormElement = ({ formElement: fe, value, update }) => {
   return (
@@ -15,50 +17,27 @@ export const DateTimeFormElement = ({ formElement: fe, value, update }) => {
         value={value}
         onChange={update}
         onError={console.log}
-        disablePast
+        // disablePast
         format="dd/MM/yyyy HH:mm"
-        // style={{ width: "30%" }}
+        style={{ width: "30%" }}
       />
     </MuiPickersUtilsProvider>
   );
 };
 
-const SimpleDateFormElement = ({ formElement: fe, type, value, update }) => {
-  const [date, setDate] = React.useState(value ? value.toISOString() : "");
-
-  /*TODO:
-   * DateFormElement cannot be auto-calculated as of now.
-   * Because the two way binding is not implemented.
-   *
-   * React.useEffect( fun {
-   *   if current element not focused {
-   *     setDate(value ? value.toISOString() : "")
-   *   }
-   * }, [value]);
-   *
-   * */
-
+export const DateFormElement = ({ formElement: fe, value, update }) => {
   return (
-    <Fragment>
-      <TextField
+    <MuiPickersUtilsProvider utils={DateFnsUtils}>
+      <KeyboardDatePicker
+        autoOk
         label={fe.display || fe.name}
-        type={type}
         required={fe.mandatory}
-        name={fe.name}
+        value={value}
+        onChange={update}
+        onError={console.log}
+        format="dd/MM/yyyy"
         style={{ width: "30%" }}
-        fullWidth
-        InputLabelProps={{
-          shrink: true
-        }}
-        value={date}
-        onChange={e => {
-          const value = e.target.value;
-          isEmpty(value) ? setDate("") : setDate(value);
-          isEmpty(value) ? update() : update(value);
-        }}
       />
-    </Fragment>
+    </MuiPickersUtilsProvider>
   );
 };
-
-export const DateFormElement = props => <SimpleDateFormElement type="date" {...props} />;
