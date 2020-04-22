02f2c69a39f5470805fce81aa40c7343e792e365
Vinay Venu <ijkvinay@gmail.com>
2020-04-22 19:59:40 +0530

    Introduce shortcut to create new form when creating an encounter, subject type or program

--- src/adminApp/SubjectType/SelectForm.js
+++ src/adminApp/SubjectType/SelectForm.js
@@ -1,23 +1,54 @@
 import DropDown from "../../common/components/DropDown";
 import _ from "lodash";
-import React from "react";
+import React, { useState } from "react";
+import { Grid } from "@material-ui/core";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
 
-const SelectForm = ({ label, formList, value, onChange }) => {
+const SelectForm = ({ label = "Please select", formList, value, onChange }) => {
+  const [shouldCreateNewForm, setCreateForm] = useState(false);
+
   const convertFormListForDisplay = (list = []) =>
     list.map(form => ({
       name: form.formName,
       value: form
     }));
 
+  const handleCreateNewForm = event => {
+    setCreateForm(event.target.checked);
+    onChange(undefined);
+  };
+
+  const switchLabel = "Create new form";
+
   return (
-    <DropDown
-      name={label || "Please select"}
-      value={value}
-      onChange={selectedFormName =>
-        onChange(_.find(formList, form => form.formName === selectedFormName))
-      }
-      options={convertFormListForDisplay(formList)}
-    />
+    <Grid container direction="row" alignItems="center" spacing={10} justify="flex-start">
+      <Grid item>
+        <DropDown
+          required={!shouldCreateNewForm}
+          disabled={shouldCreateNewForm}
+          name={label}
+          value={value}
+          onChange={selectedFormName =>
+            onChange(_.find(formList, form => form.formName === selectedFormName))
+          }
+          options={convertFormListForDisplay(formList)}
+        />
+      </Grid>
+      <Grid item>OR</Grid>
+      <Grid item>
+        <FormControlLabel
+          control={
+            <Switch
+              color={"primary"}
+              checked={shouldCreateNewForm}
+              onChange={handleCreateNewForm}
+            />
+          }
+          label={switchLabel}
+        />
+      </Grid>
+    </Grid>
   );
 };
 
--- src/common/components/DropDown.js
+++ src/common/components/DropDown.js
@@ -3,24 +3,41 @@ import InputLabel from "@material-ui/core/InputLabel";
 import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
+import { makeStyles } from "@material-ui/core/styles";
 
-export default ({ name, value, onChange, options, style }) => (
-  <FormControl required style={style}>
-    <InputLabel htmlFor={`${name}-required`}>{name}</InputLabel>
-    <Select
-      native
-      value={value}
-      onChange={event => onChange(event.target.value)}
-      name={name}
-      inputProps={{ id: `${name}-required` }}
-    >
-      <option value="" />
-      {options.map((option, index) => (
-        <option key={index} value={option.name}>
-          {option.name}
-        </option>
-      ))}
-    </Select>
-    <FormHelperText>Required</FormHelperText>
-  </FormControl>
-);
+const useStyles = makeStyles(theme => ({
+  formControl: {
+    margin: 0,
+    fullWidth: true,
+    display: "flex",
+    wrap: "nowrap"
+  }
+}));
+
+const DropDown = ({ name, value, onChange, options, style, required = true, disabled = false }) => {
+  const classes = useStyles();
+
+  return (
+    <FormControl required={required} style={style} className={classes.formControl}>
+      <InputLabel htmlFor={`${name}-required`}>{name}</InputLabel>
+      <Select
+        disabled={disabled}
+        native
+        value={value}
+        onChange={event => onChange(event.target.value)}
+        name={name}
+        inputProps={{ id: `${name}-required` }}
+      >
+        <option value="" />
+        {options.map((option, index) => (
+          <option key={index} value={option.name}>
+            {option.name}
+          </option>
+        ))}
+      </Select>
+      {required && <FormHelperText>Required</FormHelperText>}
+    </FormControl>
+  );
+};
+
+export default DropDown;
