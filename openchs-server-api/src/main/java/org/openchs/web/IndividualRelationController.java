639f5a24a944cf25346bd3f7e99121f004316e01
Vinay Venu <ijkvinay@gmail.com>
2020-04-30 16:53:12 +0530

    #170 | Create works. Update not working, delete not tested

--- openchs-server-api/src/main/java/org/openchs/web/IndividualRelationController.java
+++ openchs-server-api/src/main/java/org/openchs/web/IndividualRelationController.java
@@ -3,20 +3,20 @@ package org.openchs.web;
 import org.openchs.dao.GenderRepository;
 import org.openchs.dao.individualRelationship.IndividualRelationGenderMappingRepository;
 import org.openchs.dao.individualRelationship.IndividualRelationRepository;
-import org.openchs.domain.Gender;
 import org.openchs.domain.individualRelationship.IndividualRelation;
 import org.openchs.domain.individualRelationship.IndividualRelationGenderMapping;
 import org.openchs.util.ApiException;
 import org.openchs.web.request.GenderContract;
 import org.openchs.web.request.IndividualRelationContract;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 @RestController
 public class IndividualRelationController {
@@ -34,40 +34,103 @@ public class IndividualRelationController {
     }
 
     @GetMapping(value = "/web/relation")
-    @PreAuthorize(value = "hasAnyAuthority('user', 'admin', 'organisation_admin')")
+    @PreAuthorize(value = "hasAnyAuthority('admin', 'organisation_admin')")
     @ResponseBody
     public List<IndividualRelationContract> getAllIndividualRelations() {
         List<IndividualRelation> individualRelations = individualRelationRepository.findAll();
-        return individualRelations.stream().map(relation ->
-                new IndividualRelationContract(
-                        relation.getId(),
-                        relation.getName(),
-                        relation.getUuid(),
-                        getGenders(relation)))
+        return individualRelations
+                .stream()
+                .map(this::toResponseObject)
                 .collect(Collectors.toList());
     }
 
+    @GetMapping(value = "/web/relation/{id}")
+    @PreAuthorize(value = "hasAnyAuthority('admin', 'organisation_admin')")
+    @ResponseBody
+    public ResponseEntity<IndividualRelationContract> getIndividualRelation(@PathVariable Long id) {
+        Optional<IndividualRelation> relation = individualRelationRepository.findById(id);
+        return relation.map(individualRelation ->
+                ResponseEntity.ok(toResponseObject(individualRelation)))
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
     @PostMapping(value = "/web/relation")
-    @PreAuthorize(value = "hasAnyAuthority('user', 'admin', 'organisation_admin')")
+    @PreAuthorize(value = "hasAnyAuthority('admin', 'organisation_admin')")
     @ResponseBody
     @Transactional
-    public void saveIndividualRelation(@RequestBody IndividualRelationContract individualRelationContract) {
+    public void newIndividualRelation(@RequestBody IndividualRelationContract individualRelationContract) {
         String name = individualRelationContract.getName();
         assertNoExistingRelation(name);
         IndividualRelation relation = createRelation(name);
         saveGenderMappings(individualRelationContract, relation);
     }
 
+    @PostMapping(value = "/web/relation/{id}")
+    @PreAuthorize(value = "hasAnyAuthority('admin', 'organisation_admin')")
+    @ResponseBody
+    @Transactional
+    public ResponseEntity saveIndividualRelation(@PathVariable Long id, @RequestBody IndividualRelationContract individualRelationContract) {
+        Optional<IndividualRelation> relation = individualRelationRepository.findById(id);
+        if (!relation.isPresent()) {
+            return ResponseEntity.notFound().build();
+        }
+        IndividualRelation individualRelation = relation.get();
+        individualRelation.setName(individualRelationContract.getName());
+        saveGenderMappings(individualRelationContract, individualRelation);
+        return ResponseEntity.ok().build();
+    }
+
+    @DeleteMapping(value = "/web/relation/{id}")
+    @PreAuthorize(value = "hasAnyAuthority('admin', 'organisation_admin')")
+    @ResponseBody
+    @Transactional
+    public void deleteIndividualRelation(@PathVariable Long id) {
+        Optional<IndividualRelation> relation = individualRelationRepository.findById(id);
+        if (relation.isPresent()) {
+            IndividualRelation individualRelation = relation.get();
+            individualRelation.setVoided(true);
+            individualRelationRepository.save(individualRelation);
+        }
+    }
+
+    private IndividualRelationContract toResponseObject(IndividualRelation relation) {
+        return new IndividualRelationContract(
+                        relation.getId(),
+                        relation.getName(),
+                        relation.getUuid(),
+                        getGenders(relation));
+    }
+
     private void saveGenderMappings(IndividualRelationContract individualRelationContract, IndividualRelation relation) {
-        individualRelationContract.getGenders()
-                .stream()
-                .forEach(genderContract -> {
-                    genderMappingRepository.save(
-                            new IndividualRelationGenderMapping(
-                                    relation,
-                                    genderRepository.findByName(genderContract.getName())
-                            ));
-                });
+        List<IndividualRelationGenderMapping> existingMappings = genderMappingRepository.findAllByRelation(relation);
+        List<GenderContract> genders = individualRelationContract.getGenders();
+
+        existingMappings.forEach(existingMapping -> {
+            if (genders.stream().anyMatch(genderContract -> genderContract.getName().equals(existingMapping.getGender().getName()))) {
+                existingMapping.setVoided(false);
+            } else {
+                existingMapping.setVoided(true);
+            }
+            genderMappingRepository.save(existingMapping);
+        });
+
+        genders.forEach(genderContract -> {
+            List<IndividualRelationGenderMapping> matchingMappings = existingMappings.stream().filter(
+                    genderMapping -> genderMapping.getGender().getName().equals(genderContract.getName()))
+                    .collect(Collectors.toList());
+            if (matchingMappings.size() > 0) {
+                IndividualRelationGenderMapping matchingMapping = matchingMappings.get(0);
+                matchingMapping.setVoided(false);
+                genderMappingRepository.save(matchingMapping);
+            } else {
+                IndividualRelationGenderMapping mapping = new IndividualRelationGenderMapping(
+                        relation,
+                        genderRepository.findByName(genderContract.getName())
+                );
+                mapping.assignUUID();
+                genderMappingRepository.save(mapping);
+            }
+        });
     }
 
     private IndividualRelation createRelation(String name) {
@@ -89,6 +152,7 @@ public class IndividualRelationController {
         return genderMappingRepository
                 .findAllByRelation(relation)
                 .stream()
+                .filter(genderMapping -> !genderMapping.isVoided())
                 .map(genderMapping -> new GenderContract(
                         genderMapping.getGender().getUuid(),
                         genderMapping.getGender().getName()))
