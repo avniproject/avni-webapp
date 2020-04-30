de63c61ef182830681a7981e95b3a7541963c241
Hiren Thacker <hithacker@gmail.com>
2020-04-30 16:07:46 +0530

    #131 | add address level type id and name

--- openchs-server-api/src/main/java/org/openchs/web/IndividualController.java
+++ openchs-server-api/src/main/java/org/openchs/web/IndividualController.java
@@ -138,16 +138,14 @@ public class IndividualController extends AbstractController<Individual> impleme
         IndividualRepository repo = this.individualRepository;
         if (query != null && !"".equals(query.trim())) {
             return repo.findAll(
-                    where(repo.getFilterSpecForOperatingSubjectScope(userService.getCurrentUser())).and(
                             where(repo.getFilterSpecForAddress(query))
                                     .or(repo.getFilterSpecForObs(query))
                                     .or(repo.getFilterSpecForName(query))
-                    ), pageable)
+                    , pageable)
                     .map(t -> projectionFactory.createProjection(IndividualWebProjection.class, t));
         }
         return repo.findAll(
-                where(repo.getFilterSpecForOperatingSubjectScope(userService.getCurrentUser()))
-                        .and(repo.getFilterSpecForVoid(includeVoided))
+                where(repo.getFilterSpecForVoid(includeVoided))
                         .and(repo.getFilterSpecForName(name))
                         .and(repo.getFilterSpecForObs(obs))
                         .and(repo.getFilterSpecForLocationIds(locationIds))
