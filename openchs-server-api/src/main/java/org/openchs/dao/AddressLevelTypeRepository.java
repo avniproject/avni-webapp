410a592ebe765e8f75820eccee67426442691e77
Hiren Thacker <hithacker@gmail.com>
2020-05-08 09:04:40 +0530

    Revert " #0 | filter voided addressleveltypes from operational modules api response"
    This reverts commit 489bfc8ca7f698fe3c119b7223c9bb1028e8d9d2.

--- openchs-server-api/src/main/java/org/openchs/dao/AddressLevelTypeRepository.java
+++ openchs-server-api/src/main/java/org/openchs/dao/AddressLevelTypeRepository.java
@@ -22,6 +22,6 @@ public interface AddressLevelTypeRepository extends ReferenceDataRepository<Addr
     @Query("select a.name from AddressLevelType a where a.isVoided = false")
     List<String> getAllNames();
 
-    @Query("select st from AddressLevelType st where st.voided = false and st.id not in (select distinct parent from AddressLevelType d where d.parent is not null)")
+    @Query("select st from AddressLevelType st where id not in (select distinct parent from AddressLevelType d where d.parent is not null)")
     List<AddressLevelType> getAllLowestAddressLevelTypes();
 }
