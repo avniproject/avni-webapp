807420ec8a34957c1dec12329fc62e7a24771eb6
narendranath <narendranath_p@persistent.com>
2020-05-21 14:50:21 +0530

    Code changes for completed visits table pagination

--- src/dataEntryApp/views/subjectDashBoard/components/CompletedVisits.js
+++ src/dataEntryApp/views/subjectDashBoard/components/CompletedVisits.js
@@ -8,15 +8,8 @@ import {
   Paper,
   List
 } from "@material-ui/core";
-//import Breadcrumbs from "@material-ui/core/Breadcrumbs";
-import Box from "@material-ui/core/Box";
-import Collapse from "@material-ui/core/Collapse";
-import IconButton from "@material-ui/core/IconButton";
 import Grid from "@material-ui/core/Grid";
-import TableHead from "@material-ui/core/TableHead";
 import Typography from "@material-ui/core/Typography";
-import KeyboardArrowDownIcon from "@material-ui/icons/KeyboardArrowDown";
-import KeyboardArrowUpIcon from "@material-ui/icons/KeyboardArrowUp";
 import { withRouter, Link, useHistory } from "react-router-dom";
 import { connect } from "react-redux";
 import { makeStyles } from "@material-ui/core/styles";
@@ -29,6 +22,11 @@ import Breadcrumbs from "dataEntryApp/components/Breadcrumbs";
 import FilterResult from "../components/FilterResult";
 import { enableReadOnly } from "common/constants";
 import moment from "moment/moment";
+import {
+  EnhancedTableHead,
+  stableSort,
+  getComparator
+} from "../../../../dataEntryApp/components/TableHeaderSorting";
 import { store } from "../../../../common/store/createStore";
 
 const useStyle = makeStyles(theme => ({
@@ -39,8 +37,9 @@ const useStyle = makeStyles(theme => ({
   },
   table: {
     minWidth: 1000,
-    boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.3)",
-    borderRadius: "3px"
+    //  boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.3)",
+    //  borderRadius: "3px",
+    padding: "5px"
   },
   completedVsits: {
     fontWeight: "550",
@@ -85,121 +84,116 @@ const useStyle = makeStyles(theme => ({
     padding: "1.5rem",
     margin: "0rem 1rem"
   },
+  tableBox: {
+    padding: "1.5rem"
+  },
   cellpadding: {
     padding: "14px 40px 14px 0px"
   },
-  // Breadcrumbs: {
-  //   margin: "12px 24px",
-  //   fontSize: "12px"
-  // },
   Typography: {
     fontSize: "12px"
   }
 }));
 
-function Row(props) {
-  const { t } = useTranslation();
-  const { row } = props;
-  const [open, setOpen] = React.useState(false);
-  const classes = useStyle();
-
-  return (
-    row && (
-      <React.Fragment>
-        <TableRow className={classes.root}>
-          {/* <TableCell>
-          <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-            {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-          </IconButton>
-        </TableCell> */}
-          <TableCell component="th" scope="row">
-            {t(row.encounterType.name ? row.encounterType.name : "-")}
-          </TableCell>
-          <TableCell align="left">
-            {row.encounterDateTime ? moment(row.encounterDateTime).format("DD-MM-YYYY") : "-"}
-          </TableCell>
-          <TableCell align="left">
-            {row.earliestVisitDateTime
-              ? moment(row.earliestVisitDateTime).format("DD-MM-YYYY")
-              : "-"}
-          </TableCell>
-          {!enableReadOnly ? (
-            <TableCell align="left">
-              {" "}
-              <Link to="">{t("edit")}</Link> | <Link to="">{t("visit")}</Link>
-            </TableCell>
-          ) : (
-            ""
-          )}
-          <TableCell>
-            <IconButton aria-label="expand row" size="small" onClick={() => setOpen(!open)}>
-              {open ? <KeyboardArrowUpIcon /> : <KeyboardArrowDownIcon />}
-            </IconButton>
-          </TableCell>
-        </TableRow>
-        <TableRow>
-          <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
-            <Collapse in={open} timeout="auto" unmountOnExit>
-              <Box margin={1}>
-                <Typography variant="h6" gutterBottom component="div">
-                  {t("summary")}
-                </Typography>
-                <Table size="small" aria-label="purchases">
-                  <TableBody>
-                    <List>
-                      <Observations
-                        observations={row.observations ? mapObservation(row.observations) : ""}
-                      />
-                    </List>
-                  </TableBody>
-                </Table>
-              </Box>
-            </Collapse>
-          </TableCell>
-        </TableRow>
-      </React.Fragment>
-    )
-  );
-}
-
-const SubjectsTable = ({ allVisits }) => {
+const CompletedVisitsTable = ({ allVisits }) => {
   const classes = useStyle();
   const { t } = useTranslation();
   const [order, setOrder] = React.useState("asc");
   const [orderBy, setOrderBy] = React.useState("calories");
   const [selected, setSelected] = React.useState([]);
-  const [open, setOpen] = React.useState(false);
+  // const [open, setOpen] = React.useState(false);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
-  const [rowsPerPage, setRowsPerPage] = React.useState(20);
-  // let allVisitsListObj = [];
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  let tableHeaderName = [];
+  let allVisitsListObj = [];
+  if (enableReadOnly) {
+    tableHeaderName = [
+      { id: "name", numeric: false, disablePadding: true, label: "visitName", align: "left" },
+      {
+        id: "encounterDateTime",
+        numeric: false,
+        disablePadding: true,
+        label: "visitcompleteddate",
+        align: "left"
+      },
+      {
+        id: "earliestVisitDateTime",
+        numeric: true,
+        disablePadding: false,
+        label: "visitscheduledate",
+        align: "left"
+      }
+    ];
+  } else {
+    tableHeaderName = [
+      { id: "name", numeric: false, disablePadding: true, label: "visitName", align: "left" },
+      {
+        id: "encounterDateTime",
+        numeric: false,
+        disablePadding: true,
+        label: "visitcompleteddate",
+        align: "left"
+      },
+      {
+        id: "earliestVisitDateTime",
+        numeric: true,
+        disablePadding: false,
+        label: "visitscheduledate",
+        align: "left"
+      },
+      {
+        id: "action",
+        numeric: false,
+        disablePadding: true,
+        label: "action",
+        align: "left"
+      }
+    ];
+  }
 
-  // if (allVisits) {
-  //   allVisits.content.forEach(function(a) {
-  //     let sub = {
-  //       name: a.encounterType.name ? a.encounterType.name : "-",
-  //       earliestVisitDateTime: a.earliestVisitDateTime
-  //         ? moment(a.earliestVisitDateTime).format("DD-MM-YYYY")
-  //         : "-",
-  //       encounterDateTime: a.encounterDateTime
-  //         ? moment(a.encounterDateTime).format("DD-MM-YYYY")
-  //         : "-",
-  //       observations: mapObservation(a.observations)
-  //     };
-  //     allVisitsListObj.push(sub);
-  //   });
-  // }
+  if (allVisits && allVisits.content) {
+    allVisits.content.forEach(function(a) {
+      let sub = {
+        uuid: a.uuid,
+        name: a.encounterType.name ? a.encounterType.name : "-",
+        earliestVisitDateTime: a.earliestVisitDateTime
+          ? moment(a.earliestVisitDateTime).format("DD-MM-YYYY")
+          : "-",
+        encounterDateTime: a.encounterDateTime
+          ? moment(a.encounterDateTime).format("DD-MM-YYYY")
+          : "-",
+        observations: mapObservation(a.observations)
+      };
+      allVisitsListObj.push(sub);
+    });
+  }
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === "asc";
+    setOrder(isAsc ? "desc" : "asc");
+    setOrderBy(property);
+  };
 
+  const handleSelectAllClick = event => {
+    if (event.target.checked) {
+      const newSelecteds = allVisitsListObj.map(n => n.name);
+      setSelected(newSelecteds);
+      return;
+    }
+    setSelected([]);
+  };
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
 
   const handleChangeRowsPerPage = event => {
-    setRowsPerPage(parseInt(event.target.value, 20));
+    setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
+  const emptyRows =
+    rowsPerPage - Math.min(rowsPerPage, allVisitsListObj.length - page * rowsPerPage);
 
-  return allVisits && allVisits.content ? (
+  return allVisitsListObj ? (
     <div>
       <Table
         className={classes.table}
@@ -207,35 +201,58 @@ const SubjectsTable = ({ allVisits }) => {
         size={dense ? "small" : "medium"}
         aria-label="enhanced table"
       >
-        <TableHead>
-          <TableRow>
-            <TableCell className={classes.tableheader}>{t("visitName")}</TableCell>
-            <TableCell align="left" className={classes.tableheader}>
-              {t("visitcompleteddate")}
-            </TableCell>
-            <TableCell align="left" className={classes.tableheader}>
-              {t("visitscheduledate")}
-            </TableCell>
-            {!enableReadOnly ? (
-              <TableCell align="left" className={classes.tableheader}>
-                {t("actions")}
-              </TableCell>
-            ) : (
-              ""
-            )}
-            <TableCell />
-          </TableRow>
-        </TableHead>
+        <EnhancedTableHead
+          headername={tableHeaderName}
+          classes={classes}
+          numSelected={selected.length}
+          order={order}
+          orderBy={orderBy}
+          onSelectAllClick={handleSelectAllClick}
+          onRequestSort={handleRequestSort}
+          rowCount={allVisitsListObj.length}
+        />
         <TableBody>
-          {allVisits.content.map(row => (
-            <Row key={row.name} row={row} />
-          ))}
+          {stableSort(allVisitsListObj, getComparator(order, orderBy))
+            .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            .map((row, index) => {
+              return (
+                <TableRow>
+                  <TableCell component="th" scope="row" padding="none" width="30%">
+                    <Link to={`/app/subject/viewVisit?uuid=${row.uuid}`}>{t(row.name)}</Link>
+                  </TableCell>
+
+                  <TableCell align="left" className={classes.cellpadding}>
+                    {row.encounterDateTime}
+                  </TableCell>
+                  <TableCell align="left" className={classes.cellpadding}>
+                    {row.earliestVisitDateTime}
+                  </TableCell>
+
+                  {!enableReadOnly ? (
+                    <TableCell align="left" className={classes.cellpadding}>
+                      {" "}
+                      <Link to="">
+                        {t("edit")} {t("visit")}
+                      </Link>
+                    </TableCell>
+                  ) : (
+                    ""
+                  )}
+                </TableRow>
+              );
+            })}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: (dense ? 33 : 53) * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
         </TableBody>
       </Table>
+
       <TablePagination
-        rowsPerPageOptions={[20, 50, 100]}
+        rowsPerPageOptions={[5, 10, 20]}
         component="div"
-        count={allVisits.content.length}
+        count={allVisitsListObj.length}
         rowsPerPage={rowsPerPage}
         page={page}
         onChangePage={handleChangePage}
@@ -272,32 +289,14 @@ const CompleteVisit = ({
     <div>
       <Fragment>
         <Breadcrumbs path={match.path} />
-        {/* {paperInfo} */}
-
-        {/* <Breadcrumbs aria-label="breadcrumb" className={classes.Breadcrumbs}>
-          <Link color="inherit" onClick={() => history.push("/app")}>
-            App
-          </Link>
-          <Link
-            color="inherit"
-            onClick={() => history.push("/app/subject?uuid=ed34cad1-50da-4356-9bab-1bae77780087")}
-          >
-            Subject Dashboard
-          </Link>
-          <Typography color="textPrimary" className={classes.Typography}>
-            CompleteVisit
-          </Typography>
-        </Breadcrumbs> */}
 
         <Paper className={classes.searchBox}>
           <Grid container spacing={3}>
             <Grid item xs={6} alignItems="left">
               <div align="left">
-                {/* <h1>Completed visits </h1> */}
                 <Typography variant="h6" gutterBottom className={classes.completedVsits}>
                   {t("completedVisits")}
                 </Typography>
-                {/* <h5>20 Results found</h5> */}
                 <Typography variant="subtitle1" gutterBottom className={classes.resultFound}>
                   {completedVisit ? completedVisit.content.length : ""} {t("resultfound")}
                 </Typography>
@@ -307,7 +306,9 @@ const CompleteVisit = ({
               <FilterResult visitTypes={visitTypes} />
             </Grid>
           </Grid>
-          <SubjectsTable allVisits={completedVisit} />
+          <Paper className={classes.tableBox}>
+            <CompletedVisitsTable allVisits={completedVisit} />
+          </Paper>
         </Paper>
       </Fragment>
     </div>
