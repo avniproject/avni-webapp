555265e7605d2bdb9845cef42b499e248bf23b4a
Vinod Deolal <vinoddeolal@gmail.com>
2020-05-22 19:11:04 +0530

    #200 | APIs for doing CRUD operations on video entity

--- openchs-server-api/src/main/java/org/openchs/web/request/VideoContract.java
+++ openchs-server-api/src/main/java/org/openchs/web/request/VideoContract.java
@@ -1,10 +1,34 @@
 package org.openchs.web.request;
 
+import org.openchs.domain.Video;
+import org.openchs.util.S;
+
 public class VideoContract extends CHSRequest {
     private String title;
     private String filePath;
     private Long duration;
     private String description;
+    private String fileName;
+
+    public static VideoContract fromEntity(Video video) {
+        VideoContract videoContract = new VideoContract();
+        videoContract.setId(video.getId());
+        videoContract.setUuid(video.getUuid());
+        videoContract.setDescription(video.getDescription());
+        videoContract.setDuration(video.getDuration());
+        videoContract.setFileName(S.getLastStringAfter(video.getFilePath(), "/"));
+        videoContract.setTitle(video.getTitle());
+        videoContract.setVoided(video.isVoided());
+        return videoContract;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
 
     public String getTitle() {
         return title;
