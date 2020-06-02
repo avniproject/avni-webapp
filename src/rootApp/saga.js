f720717a4454dc6401b69001cab22f1358d36e00
Darshana <darshana_tak@persistent.com>
2020-06-02 10:41:30 +0530

    Removed translation.json file as now translations are fetched from database

--- src/rootApp/saga.js
+++ src/rootApp/saga.js
@@ -19,7 +19,6 @@ import { configureAuth } from "./utils";
 import i18n from "i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
 import { initReactI18next } from "react-i18next";
-import data from "translation";
 import { intersection, isEmpty } from "lodash";
 
 const api = {
