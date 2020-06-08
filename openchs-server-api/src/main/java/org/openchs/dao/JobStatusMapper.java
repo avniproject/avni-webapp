a3cfe05a3688d8cf4939653e4932896c52f0649b
Vinod Deolal <vinoddeolal@gmail.com>
2020-06-08 13:22:31 +0530

    0000 | display dateTime in the job status table

--- openchs-server-api/src/main/java/org/openchs/dao/JobStatusMapper.java
+++ openchs-server-api/src/main/java/org/openchs/dao/JobStatusMapper.java
@@ -10,9 +10,9 @@ public class JobStatusMapper implements RowMapper<JobStatus> {
         JobStatus jobStatus = new JobStatus();
         jobStatus.setStatus(rs.getString("status"));
         jobStatus.setExitStatus(rs.getString("exit_code"));
-        jobStatus.setCreateTime(rs.getDate("create_time"));
-        jobStatus.setStartTime(rs.getDate("start_time"));
-        jobStatus.setEndTime(rs.getDate("end_time"));
+        jobStatus.setCreateTime(rs.getTimestamp("create_time"));
+        jobStatus.setStartTime(rs.getTimestamp("start_time"));
+        jobStatus.setEndTime(rs.getTimestamp("end_time"));
         jobStatus.setUuid(rs.getString("uuid"));
         jobStatus.setFileName(rs.getString("fileName"));
         jobStatus.setNoOfLines(rs.getLong("noOfLines"));
