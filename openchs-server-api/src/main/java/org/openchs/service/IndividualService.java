9d167b4f9a3d2944bcb0d3cfa1800237b24ea498
anandkothariPSL <60173751+anandkothariPSL@users.noreply.github.com>
2020-07-03 12:08:53 +0530

    id and uuid change for delete api

--- openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
+++ openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
@@ -217,6 +217,9 @@ public class IndividualService {
         if (individualRelationship.getExitObservations() != null) {
             relationshipContract.setExitObservations(observationService.constructObservations(individualRelationship.getExitObservations()));
         }
+        if(null!=individualRelationship && null!=individualRelationship.getId())
+            individualRelationshipTypeContract.setId(individualRelationship.getRelationship().getId());
+
         return relationshipContract;
     }
 }
