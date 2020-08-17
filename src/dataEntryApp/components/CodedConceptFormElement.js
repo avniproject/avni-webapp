72364d16c156b2b6734c1992d12ea8e9af5706c3
Vinod Deolal <vinoddeolal@gmail.com>
2020-08-17 13:39:18 +0530

    #260 | order coded answers by answer order

--- src/dataEntryApp/components/CodedConceptFormElement.js
+++ src/dataEntryApp/components/CodedConceptFormElement.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { CodedFormElement } from "./CodedFormElement";
+import { sortBy } from "lodash";
 
 export const CodedConceptFormElement = ({ children: fe, validationResults, uuid, ...props }) => {
   return (
     <CodedFormElement
       groupName={fe.name}
-      items={fe.concept.answers.map(answer => answer.concept)}
+      items={sortBy(fe.concept.answers, "answerOrder").map(answer => answer.concept)}
       multiSelect={fe.type === "MultiSelect"}
       mandatory={fe.mandatory}
       validationResults={validationResults}
