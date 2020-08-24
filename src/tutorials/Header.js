c9f5305585b99bf0d5d4465f5b228d7fa97a81c2
Vinod Deolal <vinoddeolal@gmail.com>
2020-08-24 16:08:42 +0530

    hame header samller

--- src/tutorials/Header.js
+++ src/tutorials/Header.js
@@ -4,7 +4,7 @@ import Typography from "@material-ui/core/Typography";
 
 const Header = () => {
   return (
-    <Box pt={10} pb={10} mt={6.2} mb={5} bgcolor={"#f8f4f4"}>
+    <Box pt={1} pb={6} mt={6.2} mb={5} bgcolor={"#f8f4f4"}>
       <Typography variant="h3" align={"center"}>
         We are here to help!
       </Typography>
