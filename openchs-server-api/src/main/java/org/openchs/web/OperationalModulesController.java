fdd5df2c5e68922e93d894f035ec3c045015cf6b
Vinod Deolal <vinoddeolal@gmail.com>
2020-10-16 15:32:02 +0530

    #486[web-app] | add customRegistrationLocations in the operational modules response this will be used to display the registration location types accordingly if customRegistrationLocations is empty for a subject type we display addressLevelTypes else we display customRegistrationLocations

--- openchs-server-api/src/main/java/org/openchs/web/OperationalModulesController.java
+++ openchs-server-api/src/main/java/org/openchs/web/OperationalModulesController.java
@@ -1,7 +1,10 @@
 package org.openchs.web;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.openchs.application.Form;
 import org.openchs.application.FormMapping;
+import org.openchs.application.KeyType;
 import org.openchs.dao.AddressLevelTypeRepository;
 import org.openchs.dao.EncounterTypeRepository;
 import org.openchs.dao.ProgramRepository;
@@ -9,9 +12,13 @@ import org.openchs.dao.SubjectTypeRepository;
 import org.openchs.dao.application.FormMappingRepository;
 import org.openchs.dao.application.FormRepository;
 import org.openchs.domain.JsonObject;
+import org.openchs.service.OrganisationConfigService;
+import org.openchs.util.ObjectMapperSingleton;
 import org.openchs.web.request.AddressLevelTypeContract;
+import org.openchs.web.request.CustomRegistrationLocationTypeContract;
 import org.openchs.web.request.FormMappingContract;
 import org.openchs.web.request.application.FormContractWeb;
+import org.openchs.web.request.webapp.SubjectTypeSetting;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -30,15 +37,25 @@ public class OperationalModulesController {
     private FormMappingRepository formMappingRepository;
     private FormRepository formRepository;
     private AddressLevelTypeRepository addressLevelTypeRepository;
-    
+    private OrganisationConfigService organisationConfigService;
+    private ObjectMapper objectMapper;
+
     @Autowired
-    public OperationalModulesController(EncounterTypeRepository encounterTypeRepository, SubjectTypeRepository subjectTypeRepository, ProgramRepository programRepository, FormMappingRepository formMappingRepository, FormRepository formRepository,AddressLevelTypeRepository addressLevelTypeRepository) {
+    public OperationalModulesController(EncounterTypeRepository encounterTypeRepository,
+                                        SubjectTypeRepository subjectTypeRepository,
+                                        ProgramRepository programRepository,
+                                        FormMappingRepository formMappingRepository,
+                                        FormRepository formRepository,
+                                        AddressLevelTypeRepository addressLevelTypeRepository,
+                                        OrganisationConfigService organisationConfigService) {
         this.encounterTypeRepository = encounterTypeRepository;
         this.subjectTypeRepository = subjectTypeRepository;
         this.programRepository = programRepository;
         this.formMappingRepository = formMappingRepository;
         this.formRepository = formRepository;
         this.addressLevelTypeRepository = addressLevelTypeRepository;
+        this.organisationConfigService = organisationConfigService;
+        objectMapper = ObjectMapperSingleton.getObjectMapper();
     }
 
     @GetMapping("/web/operationalModules")
@@ -56,12 +73,28 @@ public class OperationalModulesController {
                 .stream()
                 .map(AddressLevelTypeContract::fromAddressLevelType)
                 .collect(Collectors.toList());
+        List<SubjectTypeSetting> customRegistrationLocationTypes = objectMapper.convertValue(organisationConfigService.getSettingsByKey(KeyType.customRegistrationLocations.toString()), new TypeReference<List<SubjectTypeSetting>>() {});
+        List<CustomRegistrationLocationTypeContract> customRegistrationLocationTypeContracts = customRegistrationLocationTypes
+                .stream()
+                .map(this::getCustomRegistrationLocationTypeContract)
+                .collect(Collectors.toList());
         return new JsonObject()
                 .with("subjectTypes", subjectTypeRepository.findAllOperational())
                 .with("programs", programRepository.findAllOperational())
                 .with("encounterTypes", encounterTypeRepository.findAllOperational())
                 .with("formMappings", formMappingContracts)
                 .with("forms", formsWeb)
-                .with("addressLevelTypes", addressLevelTypeContracts);
+                .with("addressLevelTypes", addressLevelTypeContracts)
+                .with("customRegistrationLocations", customRegistrationLocationTypeContracts);
+    }
+
+    private CustomRegistrationLocationTypeContract getCustomRegistrationLocationTypeContract(SubjectTypeSetting lt) {
+        CustomRegistrationLocationTypeContract customRegistrationLocationTypeContract = new CustomRegistrationLocationTypeContract();
+        customRegistrationLocationTypeContract.setSubjectTypeUUID(lt.getSubjectTypeUUID());
+        List<AddressLevelTypeContract> addressLevelTypeContractList = lt.getLocationTypeUUIDs().stream()
+                .map(uuid -> AddressLevelTypeContract.fromAddressLevelType(addressLevelTypeRepository.findByUuid(uuid)))
+                .collect(Collectors.toList());
+        customRegistrationLocationTypeContract.setAddressLevels(addressLevelTypeContractList);
+        return customRegistrationLocationTypeContract;
     }
 }
