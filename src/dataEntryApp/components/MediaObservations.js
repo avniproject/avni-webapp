8ec27bd1d88cf70aea0ec4fa3e4d25e03c9605e4
Joy <itsjoy@gmail.com>
2020-11-20 08:21:18 +0530

    #159 | Show media overlay above content having z-index prop set

--- src/dataEntryApp/components/MediaObservations.js
+++ src/dataEntryApp/components/MediaObservations.js
@@ -2,13 +2,20 @@ import React from "react";
 import ReactImageVideoLightbox from "react-image-video-lightbox";
 
 const MediaObservations = ({ mediaData, currentMediaItemIndex, onClose }) => {
+  React.useEffect(() => {
+    const LightboxContainer = document.querySelector("div.mediaObservationContainer");
+    LightboxContainer.firstChild.style.zIndex = 1;
+  });
+
   return (
-    <ReactImageVideoLightbox
-      data={mediaData}
-      startIndex={currentMediaItemIndex}
-      showResourceCount={true}
-      onCloseCallback={() => onClose()}
-    />
+    <div className={"mediaObservationContainer"}>
+      <ReactImageVideoLightbox
+        data={mediaData}
+        startIndex={currentMediaItemIndex}
+        showResourceCount={true}
+        onCloseCallback={() => onClose()}
+      />
+    </div>
   );
 };
 
