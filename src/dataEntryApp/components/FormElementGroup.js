63d8fe71d9a18cd683670a4debc9b58b2c4b75cb
Hiren Thacker <hithacker@gmail.com>
2020-11-27 09:46:28 +0530

    Revert " #525 | Show no elements notice if the group has no visible questions"
    This reverts commit 29a653695ef8b47c14d6b0634660b8a7e80e3eee.

--- src/dataEntryApp/components/FormElementGroup.js
+++ src/dataEntryApp/components/FormElementGroup.js
@@ -1,9 +1,8 @@
-import { get, isNil, isEmpty } from "lodash";
+import { get, isNil } from "lodash";
 import React from "react";
 import { LineBreak } from "../../common/components/utils";
 import { FormElement } from "./FormElement";
 import { filterFormElements } from "../services/FormElementService";
-import { useTranslation } from "react-i18next";
 
 export const FormElementGroup = ({
   children: feg,
@@ -15,40 +14,37 @@ export const FormElementGroup = ({
   entity,
   renderParent
 }) => {
-  const { t } = useTranslation();
   const formElements = isNil(filteredFormElements)
     ? filterFormElements(feg, entity)
     : filteredFormElements;
-  const emptyNotice = <div>{t("noElements")}</div>;
   return (
     <div>
       <LineBreak num={1} />
       {parentChildren && renderParent ? parentChildren : ""}
-      {isEmpty(formElements)
-        ? emptyNotice
-        : formElements.map(fe => {
-            const observation = obsHolder.findObservation(fe.concept);
-            const observationValue = observation
-              ? observation.concept.isDurationConcept()
-                ? get(observation, "valueJSON")
-                : get(observation, "valueJSON.answer")
-              : null;
-            return (
-              <FormElement
-                key={fe.uuid}
-                concept={fe.concept}
-                obsHolder={obsHolder}
-                value={observationValue}
-                validationResults={validationResults}
-                uuid={fe.uuid}
-                update={value => {
-                  updateObs(fe, value);
-                }}
-              >
-                {fe}
-              </FormElement>
-            );
-          })}
+
+      {formElements.map(fe => {
+        const observation = obsHolder.findObservation(fe.concept);
+        const observationValue = observation
+          ? observation.concept.isDurationConcept()
+            ? get(observation, "valueJSON")
+            : get(observation, "valueJSON.answer")
+          : null;
+        return (
+          <FormElement
+            key={fe.uuid}
+            concept={fe.concept}
+            obsHolder={obsHolder}
+            value={observationValue}
+            validationResults={validationResults}
+            uuid={fe.uuid}
+            update={value => {
+              updateObs(fe, value);
+            }}
+          >
+            {fe}
+          </FormElement>
+        );
+      })}
       <LineBreak num={1} />
     </div>
   );
