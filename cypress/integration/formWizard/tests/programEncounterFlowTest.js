3585a2a8e54b598100cf55e2e64e3a58837b52b5
Vinod Deolal <vinoddeolal@gmail.com>
2020-12-17 15:47:08 +0530

    #0 | login before running each test

--- cypress/integration/formWizard/tests/programEncounterFlowTest.js
+++ cypress/integration/formWizard/tests/programEncounterFlowTest.js
@@ -1,7 +1,13 @@
 import { dashboardPage } from "../dashboardPage";
 import { wizardPage } from "../wizardPage";
+import { setupTest } from "../setup";
+import { formWizardOrgPassword, formWizardOrgUsername } from "../../constants";
 
 describe("Program Encounter Flow tests for form wizard", () => {
+  beforeEach(() => {
+    setupTest.login(formWizardOrgUsername, formWizardOrgPassword);
+    setupTest.cleanAllOptionsFromRegistration("Test Individual");
+  });
   it("Performing new program encounter should not move to next page in case of validation error", () => {
     dashboardPage.performNewProgramEncounter("Test Individual", "Program1", "ProgramEncounter1");
     wizardPage.typeInput("Last FE of first FEG", 123);
