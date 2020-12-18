61ba4f1fb31427175a6d91791e12f9aa72d39a54
Vinod Deolal <vinoddeolal@gmail.com>
2020-12-18 18:03:22 +0530

    fix avni_spec test

--- cypress/integration/avni_spec.js
+++ cypress/integration/avni_spec.js
@@ -2,7 +2,7 @@
 
 describe("Data Entry App", () => {
   it("Can open new registration form", () => {
-    cy.login({ username: "admin@hiren2", password: "password" });
+    cy.login("admin@hiren2", "password");
     cy.contains("Data Entry").click();
     cy.get(":nth-child(2) > .MuiButtonBase-root > .MuiButton-label").click();
     cy.contains("Individual").click();
