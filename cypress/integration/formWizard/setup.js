2c313527d97e27646590d370d46e2e11f11bda4c
Hiren Thacker <hithacker@gmail.com>
2021-01-06 10:32:48 +0530

    #0 | Fixed cypress test merge conflict

--- cypress/integration/formWizard/setup.js
+++ cypress/integration/formWizard/setup.js
@@ -1,25 +1,29 @@
 import { dashboardPage } from "./pages/dashboardPage";
 import { wizardPage } from "./pages/wizardPage";
 
-export const setup = {
-  cleanAllOptionsFromRegistration(subjectName) {
+export const setupTest = {
+  cleanAllOptionsFromPersonRegistration(subjectName) {
     dashboardPage.editProfile(subjectName);
     wizardPage.clickNext();
-    wizardPage.unCheckOptions(
-      "Hide first FEG",
-      "Hide last FEG",
-      "Hide second FEG",
-      "Hide first FE of first FEG",
-      "Hide last FE of last FEG",
-      "Hide all FEs in first FEG",
-      "Hide last FE of first FEG",
-      "Hide first FE of second FEG",
-      "Hide last FE of second FEG",
-      "Hide first FE of last FEG",
-      "Encounter1"
-    );
+    cy.get(":checkbox").uncheck();
+    wizardPage.clickNextNTimes(4);
+    wizardPage.clickSave();
+  },
+  cleanAllOptionsFromIndividualRegistration(subjectName) {
+    dashboardPage.editProfile(subjectName);
+    cy.get(":checkbox").uncheck();
     wizardPage.clickNext();
     wizardPage.clickSave();
+  },
+  cleanAllOptionsFromEnrolment(subjectName, programName) {
+    dashboardPage.openDashboard(subjectName);
+    dashboardPage.editProgramEnrolment(programName);
+    cy.get(":checkbox").uncheck();
+    wizardPage.clickNextNTimes(3);
+    wizardPage.clickSave();
+  },
+  login(username, password) {
+    cy.login(username, password);
   },
   cleanAllOptionsFromNonIndiRegistration(subjectName) {
     dashboardPage.editProfile(subjectName);
