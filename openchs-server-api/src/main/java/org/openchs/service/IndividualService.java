50d3147f702a59adf19d19487dedbc15533a6d73
Vinod Deolal <vinoddeolal@gmail.com>
2021-01-29 14:17:12 +0530

    webapp[#569] | QA comment fix the error message

--- openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
+++ openchs-server-api/src/main/java/org/openchs/service/IndividualService.java
@@ -285,14 +285,14 @@ public class IndividualService {
                 .filter(pe -> !pe.isVoided())
                 .count();
         if (nonVoidedProgramEnrolments != 0) {
-            throw new BadRequestError(String.format("There are non deleted program enrolments for the subject %s", individual.getFirstName()));
+            throw new BadRequestError(String.format("There are non deleted program enrolments for the %s %s", individual.getSubjectType().getOperationalSubjectTypeName(), individual.getFirstName()));
         }
     }
 
     private void assertNoUnVoidedEncounters(Individual individual) {
         long nonVoidedEncounterCount = individual.nonVoidedEncounters().count();
         if (nonVoidedEncounterCount != 0) {
-            throw new BadRequestError(String.format("There are non deleted general encounters for the subject %s", individual.getFirstName()));
+            throw new BadRequestError(String.format("There are non deleted general encounters for the %s %s", individual.getSubjectType().getOperationalSubjectTypeName(), individual.getFirstName()));
         }
     }
 
