f6f49aa59459871aa835ebb6c98043de732d25cc
Vinod Deolal <vinoddeolal@gmail.com>
2021-03-01 13:28:25 +0530

    #0 | add more standard report card types

---
+++ openchs-server-api/src/main/resources/db/migration/V1_174__AddMoreStandardReportCardTypes.sql
@@ -0,0 +1,7 @@
+insert into standard_report_card_type (uuid, name, description, created_date_time, last_modified_date_time)
+values ('27020b32-c21b-43a4-81bd-7b88ad3a6ef0', 'Scheduled visits', 'Scheduled visits', current_timestamp, current_timestamp),
+       ('9f88bee5-2ab9-4ac4-ae19-d07e9715bdb5', 'Overdue visits', 'Overdue visits', current_timestamp, current_timestamp),
+       ('88a7514c-48c0-4d5d-a421-d074e43bb36c', 'Last 24 hours registrations', 'Last 24 hours registrations', current_timestamp, current_timestamp),
+       ('a5efc04c-317a-4823-a203-e62603454a65', 'Last 24 hours enrolments', 'Last 24 hours enrolments', current_timestamp, current_timestamp),
+       ('77b5b3fa-de35-4f24-996b-2842492ea6e0', 'Last 24 hours visits', 'Last 24 hours visits', current_timestamp, current_timestamp),
+       ('1fbcadf3-bf1a-439e-9e13-24adddfbf6c0', 'Total', 'Total', current_timestamp, current_timestamp);
