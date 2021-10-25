4c1523a222318fa2727f107aa48609f518fe3490
hithacker <hithacker@gmail.com>
2021-10-25 16:45:38 +0530

    #451 | Rule Failure Telemetry UI init

--- src/formDesigner/components/RuleFailureTelemetry/RuleFailureTelemetryList.js
+++ src/formDesigner/components/RuleFailureTelemetry/RuleFailureTelemetryList.js
@@ -3,24 +3,44 @@ import Box from "@material-ui/core/Box";
 import MaterialTable from "material-table";
 import { Title } from "react-admin";
 import fetchData from "common/material-table/fetch";
+import http from "common/utils/httpClient";
+import moment from "moment";
 
 const RuleFailureTelemetryList = () => {
   const columns = [
-    {
-      title: "Closed",
-      render: rowData => (rowData.closed ? "True" : "False")
-    },
     {
       title: "Message",
       render: rowData => rowData.errorMessage
     },
     {
-      title: "Stacktrace",
-      render: rowData => rowData.stacktrace
+      title: "Individual UUID",
+      render: rowData => rowData.individualUuid
+    },
+    {
+      title: "Error Date",
+      render: rowData => moment(rowData.errorDateTime).format("YYYY-MM-DD HH:mm")
+    }
+  ];
+
+  const actions = [
+    {
+      tooltip: "Close All Selected Errors",
+      icon: "delete",
+      onClick: (evt, data) => {
+        alert("You want to delete " + data.length + " rows");
+        const request = {
+          params: {
+            ids: data.map(d => d.id).join(","),
+            isClosed: true
+          }
+        };
+        http.put("/web/ruleFailureTelemetry", null, request).then(() => refreshTable(tableRef));
+      }
     }
   ];
 
   const tableRef = React.createRef();
+  const refreshTable = ref => ref.current && ref.current.onQueryChange();
 
   return (
     <>
@@ -34,8 +54,14 @@ const RuleFailureTelemetryList = () => {
             }}
             tableRef={tableRef}
             columns={columns}
+            actions={actions}
             data={fetchData("/ruleFailureTelemetry")}
+            detailPanel={rowData => {
+              return <div>{rowData.stacktrace}</div>;
+            }}
+            onRowClick={(event, rowData, togglePanel) => togglePanel()}
             options={{
+              selection: true,
               addRowPosition: "first",
               sorting: true,
               debounceInterval: 500,
