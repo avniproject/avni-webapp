avniproject/avni-server

Change controllers to pass in list of address levels instead of joining to virtual catchment table

openchs-server-api/src/main/java/org/openchs/dao/CommentRepository.java
@@ -1,6 +1,7 @@
 package org.openchs.dao;
 
 import org.joda.time.DateTime;
+import org.openchs.domain.AddressLevel;
 import org.openchs.domain.Comment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -17,8 +18,8 @@ public interface CommentRepository extends TransactionalDataRepository<Comment>,
 
     List<Comment> findByIsVoidedFalseAndCommentThreadIdOrderByAuditLastModifiedDateTimeAscIdAsc(Long threadId);
 
-    Page<Comment> findBySubjectAddressLevelVirtualCatchmentsIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(
-            long catchmentId,
+    Page<Comment> findBySubjectAddressLevelInAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(
+            List<AddressLevel> addressLevels,
             Long subjectTypeId,
             DateTime lastModifiedDateTime,
             DateTime now,
@@ -32,12 +33,12 @@ public interface CommentRepository extends TransactionalDataRepository<Comment>,
             Pageable pageable);
 
     @Override
-    default Page<Comment> findByCatchmentIndividualOperatingScopeAndFilterByType(long catchmentId, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
-        return findBySubjectAddressLevelVirtualCatchmentsIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(catchmentId, filter, lastModifiedDateTime, now, pageable);
+    default Page<Comment> syncByCatchment(SyncParameters syncParameters) {
+        return findBySubjectAddressLevelInAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(syncParameters.getAddressLevels(), syncParameters.getFilter(), syncParameters.getLastModifiedDateTime(), syncParameters.getNow(), syncParameters.getPageable());
     }
 
     @Override
-    default Page<Comment> findByFacilityIndividualOperatingScopeAndFilterByType(long facilityId, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
-        return findBySubjectFacilityIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(facilityId, filter, lastModifiedDateTime, now, pageable);
+    default Page<Comment> syncByFacility(SyncParameters syncParameters) {
+        return findBySubjectFacilityIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(syncParameters.getCatchmentId(), syncParameters.getFilter(), syncParameters.getLastModifiedDateTime(), syncParameters.getNow(), syncParameters.getPageable());
     }
 }

openchs-server-api/src/main/java/org/openchs/domain/VirtualCatchment.java
@@ -1,0 +1,65 @@
+package org.openchs.domain;
+
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Table(name = "virtual_catchment_address_mapping_table")
+@BatchSize(size = 100)
+public class VirtualCatchment {
+    @Id
+    @Column
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "catchment_id")
+    private Catchment catchment;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "addresslevel_id")
+    private AddressLevel addressLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Catchment getCatchment() {
+        return catchment;
+    }
+
+    public void setCatchment(Catchment catchment) {
+        this.catchment = catchment;
+    }
+
+    public AddressLevel getAddressLevel() {
+        return addressLevel;
+    }
+
+    public void setAddressLevel(AddressLevel addressLevel) {
+        this.addressLevel = addressLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VirtualCatchment that = (VirtualCatchment) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(catchment, that.catchment) &&
+                Objects.equals(addressLevel, that.addressLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, catchment, addressLevel);
+    }
+}

openchs-server-api/src/main/java/org/openchs/service/ScopeBasedSyncService.java
@@ -1,0 +1,43 @@
+package org.openchs.service;
+
+import org.joda.time.DateTime;
+import org.openchs.dao.OperatingIndividualScopeAwareRepository;
+import org.openchs.dao.SyncParameters;
+import org.openchs.dao.VirtualCatchmentRepository;
+import org.openchs.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.openchs.domain.OperatingIndividualScope.ByCatchment;
+import static org.openchs.domain.OperatingIndividualScope.ByFacility;
+
+@Service
+public class ScopeBasedSyncService<T extends CHSEntity> {
+    private VirtualCatchmentRepository virtualCatchmentRepository;
+
+    public ScopeBasedSyncService(VirtualCatchmentRepository virtualCatchmentRepository) {
+        this.virtualCatchmentRepository = virtualCatchmentRepository;
+    }
+
+    public Page<T> getSyncResult(OperatingIndividualScopeAwareRepository<T> repository, User user, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
+        List<VirtualCatchment> virtualCatchments = virtualCatchmentRepository.findByCatchment(user.getCatchment());
+        List<AddressLevel> addressLevels = virtualCatchments.stream().map(virtualCatchment -> virtualCatchment.getAddressLevel()).collect(Collectors.toList());
+
+        OperatingIndividualScope scope = user.getOperatingIndividualScope();
+        Facility userFacility = user.getFacility();
+        Catchment catchment = user.getCatchment();
+        if (ByCatchment.equals(scope)) {
+            return repository.syncByCatchment(new SyncParameters(0, catchment.getId(), lastModifiedDateTime, now, filter, pageable, addressLevels));
+        }
+        if (ByFacility.equals(scope)) {
+            return repository.syncByFacility(new SyncParameters(userFacility.getId(), 0, lastModifiedDateTime, now, filter, pageable, addressLevels));
+        }
+        return new PageImpl<>(Collections.emptyList());
+    }
+}

openchs-server-api/src/main/java/org/openchs/web/ChecklistItemController.java
@@ -6,6 +6,7 @@ import org.openchs.domain.Checklist;
 import org.openchs.domain.ChecklistDetail;
 import org.openchs.domain.ChecklistItem;
 import org.openchs.service.ObservationService;
+import org.openchs.service.ScopeBasedSyncService;
 import org.openchs.service.UserService;
 import org.openchs.web.request.application.ChecklistItemRequest;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,22 +23,24 @@ import javax.transaction.Transactional;
 import java.util.Collections;
 
 @RestController
-public class ChecklistItemController extends AbstractController<ChecklistItem> implements RestControllerResourceProcessor<ChecklistItem>, OperatingIndividualScopeAwareController<ChecklistItem> {
+public class ChecklistItemController extends AbstractController<ChecklistItem> implements RestControllerResourceProcessor<ChecklistItem> {
     private final ObservationService observationService;
     private final ChecklistItemDetailRepository checklistItemDetailRepository;
     private final ChecklistRepository checklistRepository;
     private final ChecklistItemRepository checklistItemRepository;
     private final ChecklistDetailRepository checklistDetailRepository;
     private final UserService userService;
+    private ScopeBasedSyncService<ChecklistItem> scopeBasedSyncService;
 
     @Autowired
-    public ChecklistItemController(ChecklistRepository checklistRepository, ChecklistItemRepository checklistItemRepository, ObservationService observationService, ChecklistItemDetailRepository checklistItemDetailRepository, ChecklistDetailRepository checklistDetailRepository, UserService userService) {
+    public ChecklistItemController(ChecklistRepository checklistRepository, ChecklistItemRepository checklistItemRepository, ObservationService observationService, ChecklistItemDetailRepository checklistItemDetailRepository, ChecklistDetailRepository checklistDetailRepository, UserService userService, ScopeBasedSyncService<ChecklistItem> scopeBasedSyncService) {
         this.checklistRepository = checklistRepository;
         this.checklistItemRepository = checklistItemRepository;
         this.observationService = observationService;
         this.checklistItemDetailRepository = checklistItemDetailRepository;
         this.checklistDetailRepository = checklistDetailRepository;
         this.userService = userService;
+        this.scopeBasedSyncService = scopeBasedSyncService;
     }
 
     @Transactional
@@ -84,7 +87,7 @@ public class ChecklistItemController extends AbstractController<ChecklistItem> i
         if (checklistDetailUuid.isEmpty()) return wrap(new PageImpl<>(Collections.emptyList()));
         ChecklistDetail checklistDetail = checklistDetailRepository.findByUuid(checklistDetailUuid);
         if (checklistDetail == null) return wrap(new PageImpl<>(Collections.emptyList()));
-        return wrap(getCHSEntitiesForUserByLastModifiedDateTimeAndFilterByType(userService.getCurrentUser(), lastModifiedDateTime, now, checklistDetail.getId(), pageable));
+        return wrap(scopeBasedSyncService.getSyncResult(checklistItemRepository, userService.getCurrentUser(), lastModifiedDateTime, now, checklistDetail.getId(), pageable));
     }
 
     @Override
@@ -98,8 +101,4 @@ public class ChecklistItemController extends AbstractController<ChecklistItem> i
         return resource;
     }
 
-    @Override
-    public OperatingIndividualScopeAwareRepository<ChecklistItem> repository() {
-        return checklistItemRepository;
-    }
 }

openchs-server-api/src/main/java/org/openchs/web/CommentController.java
@@ -7,6 +7,7 @@ import org.openchs.domain.CommentThread;
 import org.openchs.domain.Individual;
 import org.openchs.domain.SubjectType;
 import org.openchs.service.CommentService;
+import org.openchs.service.ScopeBasedSyncService;
 import org.openchs.service.UserService;
 import org.openchs.web.request.CommentContract;
 import org.slf4j.LoggerFactory;
@@ -27,7 +28,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class CommentController extends AbstractController<Comment> implements RestControllerResourceProcessor<Comment>, OperatingIndividualScopeAwareController<Comment> {
+public class CommentController extends AbstractController<Comment> implements RestControllerResourceProcessor<Comment> {
 
     private static org.slf4j.Logger logger = LoggerFactory.getLogger(CommentController.class);
     private final CommentRepository commentRepository;
@@ -36,6 +37,7 @@ public class CommentController extends AbstractController<Comment> implements Re
     private final SubjectTypeRepository subjectTypeRepository;
     private final UserService userService;
     private final CommentThreadRepository commentThreadRepository;
+    private ScopeBasedSyncService<Comment> scopeBasedSyncService;
 
     @Autowired
     public CommentController(CommentRepository commentRepository,
@@ -43,13 +45,14 @@ public class CommentController extends AbstractController<Comment> implements Re
                              CommentService commentService,
                              SubjectTypeRepository subjectTypeRepository,
                              UserService userService,
-                             CommentThreadRepository commentThreadRepository) {
+                             CommentThreadRepository commentThreadRepository, ScopeBasedSyncService<Comment> scopeBasedSyncService) {
         this.commentRepository = commentRepository;
         this.individualRepository = individualRepository;
         this.commentService = commentService;
         this.subjectTypeRepository = subjectTypeRepository;
         this.userService = userService;
         this.commentThreadRepository = commentThreadRepository;
+        this.scopeBasedSyncService = scopeBasedSyncService;
     }
 
     @GetMapping(value = "/web/comment")
@@ -119,7 +122,7 @@ public class CommentController extends AbstractController<Comment> implements Re
         if (subjectType == null) {
             return wrap(new PageImpl<>(Collections.emptyList()));
         }
-        return wrap(getCHSEntitiesForUserByLastModifiedDateTimeAndFilterByType(userService.getCurrentUser(), lastModifiedDateTime, now, subjectType.getId(), pageable));
+        return wrap(scopeBasedSyncService.getSyncResult(commentRepository, userService.getCurrentUser(), lastModifiedDateTime, now, subjectType.getId(), pageable));
     }
 
     @Override
@@ -132,11 +135,6 @@ public class CommentController extends AbstractController<Comment> implements Re
             resource.add(new Link(commentThread.getUuid(), "commentThreadUUID"));
         }
         return resource;
-    }
-
-    @Override
-    public OperatingIndividualScopeAwareRepository<Comment> repository() {
-        return commentRepository;
     }
 
 }

avniproject/avni-webapp

avni-server-api/src/main/java/org/avni/dao/CommentRepository.java
@@ -1,7 +1,8 @@
 package org.avni.dao;
 
-import org.joda.time.DateTime;
+import org.avni.domain.AddressLevel;
 import org.avni.domain.Comment;
+import org.joda.time.DateTime;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -17,8 +18,8 @@ public interface CommentRepository extends TransactionalDataRepository<Comment>,
 
     List<Comment> findByIsVoidedFalseAndCommentThreadIdOrderByAuditLastModifiedDateTimeAscIdAsc(Long threadId);
 
-    Page<Comment> findBySubjectAddressLevelVirtualCatchmentsIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(
-            long catchmentId,
+    Page<Comment> findBySubjectAddressLevelInAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(
+            List<AddressLevel> addressLevels,
             Long subjectTypeId,
             DateTime lastModifiedDateTime,
             DateTime now,
@@ -42,13 +43,13 @@ public interface CommentRepository extends TransactionalDataRepository<Comment>,
             DateTime lastModifiedDateTime);
 
     @Override
-    default Page<Comment> findByCatchmentIndividualOperatingScopeAndFilterByType(long catchmentId, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
-        return findBySubjectAddressLevelVirtualCatchmentsIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(catchmentId, filter, lastModifiedDateTime, now, pageable);
+    default Page<Comment> syncByCatchment(SyncParameters syncParameters) {
+        return findBySubjectAddressLevelInAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(syncParameters.getAddressLevels(), syncParameters.getFilter(), syncParameters.getLastModifiedDateTime(), syncParameters.getNow(), syncParameters.getPageable());
     }
 
     @Override
-    default Page<Comment> findByFacilityIndividualOperatingScopeAndFilterByType(long facilityId, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
-        return findBySubjectFacilityIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(facilityId, filter, lastModifiedDateTime, now, pageable);
+    default Page<Comment> syncByFacility(SyncParameters syncParameters) {
+        return findBySubjectFacilityIdAndSubjectSubjectTypeIdAndAuditLastModifiedDateTimeIsBetweenOrderByAuditLastModifiedDateTimeAscIdAsc(syncParameters.getCatchmentId(), syncParameters.getFilter(), syncParameters.getLastModifiedDateTime(), syncParameters.getNow(), syncParameters.getPageable());
     }
 
     @Override

avni-server-api/src/main/java/org/avni/domain/VirtualCatchment.java
@@ -1,0 +1,65 @@
+package org.avni.domain;
+
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Table(name = "virtual_catchment_address_mapping_table")
+@BatchSize(size = 100)
+public class VirtualCatchment {
+    @Id
+    @Column
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "catchment_id")
+    private Catchment catchment;
+
+    @ManyToOne(cascade = {CascadeType.ALL})
+    @JoinColumn(name = "addresslevel_id")
+    private AddressLevel addressLevel;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Catchment getCatchment() {
+        return catchment;
+    }
+
+    public void setCatchment(Catchment catchment) {
+        this.catchment = catchment;
+    }
+
+    public AddressLevel getAddressLevel() {
+        return addressLevel;
+    }
+
+    public void setAddressLevel(AddressLevel addressLevel) {
+        this.addressLevel = addressLevel;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        VirtualCatchment that = (VirtualCatchment) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(catchment, that.catchment) &&
+                Objects.equals(addressLevel, that.addressLevel);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, catchment, addressLevel);
+    }
+}

avni-server-api/src/main/java/org/avni/service/ScopeBasedSyncService.java
@@ -1,0 +1,43 @@
+package org.avni.service;
+
+import org.joda.time.DateTime;
+import org.avni.dao.OperatingIndividualScopeAwareRepository;
+import org.avni.dao.SyncParameters;
+import org.avni.dao.VirtualCatchmentRepository;
+import org.avni.domain.*;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.avni.domain.OperatingIndividualScope.ByCatchment;
+import static org.avni.domain.OperatingIndividualScope.ByFacility;
+
+@Service
+public class ScopeBasedSyncService<T extends CHSEntity> {
+    private VirtualCatchmentRepository virtualCatchmentRepository;
+
+    public ScopeBasedSyncService(VirtualCatchmentRepository virtualCatchmentRepository) {
+        this.virtualCatchmentRepository = virtualCatchmentRepository;
+    }
+
+    public Page<T> getSyncResult(OperatingIndividualScopeAwareRepository<T> repository, User user, DateTime lastModifiedDateTime, DateTime now, Long filter, Pageable pageable) {
+        List<VirtualCatchment> virtualCatchments = virtualCatchmentRepository.findByCatchment(user.getCatchment());
+        List<AddressLevel> addressLevels = virtualCatchments.stream().map(virtualCatchment -> virtualCatchment.getAddressLevel()).collect(Collectors.toList());
+
+        OperatingIndividualScope scope = user.getOperatingIndividualScope();
+        Facility userFacility = user.getFacility();
+        Catchment catchment = user.getCatchment();
+        if (ByCatchment.equals(scope)) {
+            return repository.syncByCatchment(new SyncParameters(0, catchment.getId(), lastModifiedDateTime, now, filter, pageable, addressLevels));
+        }
+        if (ByFacility.equals(scope)) {
+            return repository.syncByFacility(new SyncParameters(userFacility.getId(), 0, lastModifiedDateTime, now, filter, pageable, addressLevels));
+        }
+        return new PageImpl<>(Collections.emptyList());
+    }
+}

avni-server-api/src/main/java/org/avni/web/ChecklistItemController.java
@@ -1,13 +1,17 @@
 package org.avni.web;
 
-import org.joda.time.DateTime;
-import org.avni.dao.*;
+import org.avni.dao.ChecklistDetailRepository;
+import org.avni.dao.ChecklistItemDetailRepository;
+import org.avni.dao.ChecklistItemRepository;
+import org.avni.dao.ChecklistRepository;
 import org.avni.domain.Checklist;
 import org.avni.domain.ChecklistDetail;
 import org.avni.domain.ChecklistItem;
 import org.avni.service.ObservationService;
+import org.avni.service.ScopeBasedSyncService;
 import org.avni.service.UserService;
 import org.avni.web.request.application.ChecklistItemRequest;
+import org.joda.time.DateTime;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -22,22 +26,24 @@ import javax.transaction.Transactional;
 import java.util.Collections;
 
 @RestController
-public class ChecklistItemController extends AbstractController<ChecklistItem> implements RestControllerResourceProcessor<ChecklistItem>, OperatingIndividualScopeAwareController<ChecklistItem> {
+public class ChecklistItemController extends AbstractController<ChecklistItem> implements RestControllerResourceProcessor<ChecklistItem> {
     private final ObservationService observationService;
     private final ChecklistItemDetailRepository checklistItemDetailRepository;
     private final ChecklistRepository checklistRepository;
     private final ChecklistItemRepository checklistItemRepository;
     private final ChecklistDetailRepository checklistDetailRepository;
     private final UserService userService;
+    private ScopeBasedSyncService<ChecklistItem> scopeBasedSyncService;
 
     @Autowired
-    public ChecklistItemController(ChecklistRepository checklistRepository, ChecklistItemRepository checklistItemRepository, ObservationService observationService, ChecklistItemDetailRepository checklistItemDetailRepository, ChecklistDetailRepository checklistDetailRepository, UserService userService) {
+    public ChecklistItemController(ChecklistRepository checklistRepository, ChecklistItemRepository checklistItemRepository, ObservationService observationService, ChecklistItemDetailRepository checklistItemDetailRepository, ChecklistDetailRepository checklistDetailRepository, UserService userService, ScopeBasedSyncService<ChecklistItem> scopeBasedSyncService) {
         this.checklistRepository = checklistRepository;
         this.checklistItemRepository = checklistItemRepository;
         this.observationService = observationService;
         this.checklistItemDetailRepository = checklistItemDetailRepository;
         this.checklistDetailRepository = checklistDetailRepository;
         this.userService = userService;
+        this.scopeBasedSyncService = scopeBasedSyncService;
     }
 
     @Transactional
@@ -84,7 +90,7 @@ public class ChecklistItemController extends AbstractController<ChecklistItem> i
         if (checklistDetailUuid.isEmpty()) return wrap(new PageImpl<>(Collections.emptyList()));
         ChecklistDetail checklistDetail = checklistDetailRepository.findByUuid(checklistDetailUuid);
         if (checklistDetail == null) return wrap(new PageImpl<>(Collections.emptyList()));
-        return wrap(getCHSEntitiesForUserByLastModifiedDateTimeAndFilterByType(userService.getCurrentUser(), lastModifiedDateTime, now, checklistDetail.getId(), pageable));
+        return wrap(scopeBasedSyncService.getSyncResult(checklistItemRepository, userService.getCurrentUser(), lastModifiedDateTime, now, checklistDetail.getId(), pageable));
     }
 
     @Override
@@ -98,8 +104,4 @@ public class ChecklistItemController extends AbstractController<ChecklistItem> i
         return resource;
     }
 
-    @Override
-    public OperatingIndividualScopeAwareRepository<ChecklistItem> repository() {
-        return checklistItemRepository;
-    }
 }

avni-server-api/src/main/java/org/avni/web/CommentController.java
@@ -1,14 +1,18 @@
 package org.avni.web;
 
-import org.joda.time.DateTime;
-import org.avni.dao.*;
+import org.avni.dao.CommentRepository;
+import org.avni.dao.CommentThreadRepository;
+import org.avni.dao.IndividualRepository;
+import org.avni.dao.SubjectTypeRepository;
 import org.avni.domain.Comment;
 import org.avni.domain.CommentThread;
 import org.avni.domain.Individual;
 import org.avni.domain.SubjectType;
 import org.avni.service.CommentService;
+import org.avni.service.ScopeBasedSyncService;
 import org.avni.service.UserService;
 import org.avni.web.request.CommentContract;
+import org.joda.time.DateTime;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageImpl;
@@ -27,7 +31,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-public class CommentController extends AbstractController<Comment> implements RestControllerResourceProcessor<Comment>, OperatingIndividualScopeAwareController<Comment> {
+public class CommentController extends AbstractController<Comment> implements RestControllerResourceProcessor<Comment> {
 
     private static org.slf4j.Logger logger = LoggerFactory.getLogger(CommentController.class);
     private final CommentRepository commentRepository;
@@ -36,6 +40,7 @@ public class CommentController extends AbstractController<Comment> implements Re
     private final SubjectTypeRepository subjectTypeRepository;
     private final UserService userService;
     private final CommentThreadRepository commentThreadRepository;
+    private ScopeBasedSyncService<Comment> scopeBasedSyncService;
 
     @Autowired
     public CommentController(CommentRepository commentRepository,
@@ -43,13 +48,14 @@ public class CommentController extends AbstractController<Comment> implements Re
                              CommentService commentService,
                              SubjectTypeRepository subjectTypeRepository,
                              UserService userService,
-                             CommentThreadRepository commentThreadRepository) {
+                             CommentThreadRepository commentThreadRepository, ScopeBasedSyncService<Comment> scopeBasedSyncService) {
         this.commentRepository = commentRepository;
         this.individualRepository = individualRepository;
         this.commentService = commentService;
         this.subjectTypeRepository = subjectTypeRepository;
         this.userService = userService;
         this.commentThreadRepository = commentThreadRepository;
+        this.scopeBasedSyncService = scopeBasedSyncService;
     }
 
     @GetMapping(value = "/web/comment")
@@ -119,7 +125,7 @@ public class CommentController extends AbstractController<Comment> implements Re
         if (subjectType == null) {
             return wrap(new PageImpl<>(Collections.emptyList()));
         }
-        return wrap(getCHSEntitiesForUserByLastModifiedDateTimeAndFilterByType(userService.getCurrentUser(), lastModifiedDateTime, now, subjectType.getId(), pageable));
+        return wrap(scopeBasedSyncService.getSyncResult(commentRepository, userService.getCurrentUser(), lastModifiedDateTime, now, subjectType.getId(), pageable));
     }
 
     @Override
@@ -132,11 +138,6 @@ public class CommentController extends AbstractController<Comment> implements Re
             resource.add(new Link(commentThread.getUuid(), "commentThreadUUID"));
         }
         return resource;
-    }
-
-    @Override
-    public OperatingIndividualScopeAwareRepository<Comment> repository() {
-        return commentRepository;
     }
 
 }