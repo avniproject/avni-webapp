6b250250401cfd01757195d99a170cb5d22f45f6
Vinod Deolal <vinoddeolal@gmail.com>
2022-05-12 11:53:40 +0530

    #745 | instead of popup use show more or show less to handle the help text which is more than 2 lines

--- src/common/components/HelpText.js
+++ src/common/components/HelpText.js
@@ -1,44 +1,32 @@
-import React, { useState, Fragment } from "react";
+import React from "react";
 import { isEmpty } from "lodash";
 import makeStyles from "@material-ui/core/styles/makeStyles";
-import Box from "@material-ui/core/Box";
-import Dialog from "@material-ui/core/Dialog";
-import DialogContent from "@material-ui/core/DialogContent";
+import ShowMoreText from "react-show-more-text";
 
 const useStyles = makeStyles(() => ({
-  customBox: {
-    display: "-webkit-box",
-    boxOrient: "vertical",
-    lineClamp: 2,
-    wordBreak: "break-all",
-    overflow: "hidden",
+  container: {
+    width: "50%",
     marginBottom: "20px",
-    color: "rgba(0, 0, 0, 0.54)",
-    "&:hover": {
-      cursor: "pointer"
-    }
+    color: "rgba(69,69,69,0.54)"
   }
 }));
 
 export const HelpText = ({ text, t }) => {
   const classes = useStyles();
-  const [showDialog, setShowDialog] = useState(false);
-
   const renderText = () => (
-    <Fragment>
-      <Box
-        onClick={() => setShowDialog(true)}
-        width={"50%"}
-        fontSize="0.9rem"
-        component="div"
-        classes={{ root: classes.customBox }}
+    <div className={classes.container}>
+      <ShowMoreText
+        lines={2}
+        more="Show more"
+        less="Show less"
+        className="content-css"
+        anchorClass="my-anchor-css-class"
+        expanded={false}
+        truncatedEndingComponent={"... "}
       >
         {t(text)}
-      </Box>
-      <Dialog onClose={() => setShowDialog(false)} open={showDialog}>
-        <DialogContent>{t(text)}</DialogContent>
-      </Dialog>
-    </Fragment>
+      </ShowMoreText>
+    </div>
   );
 
   return isEmpty(text) ? null : renderText();
