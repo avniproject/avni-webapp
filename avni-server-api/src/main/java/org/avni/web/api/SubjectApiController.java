e0f28a7fc2a51fa64cf4ee8f5d023ef56494c8d1
Vinay Venu <ijkvinay@gmail.com>
2022-07-18 11:12:23 +0530

    #0 | Add null check for external id

--- avni-server-api/src/main/java/org/avni/web/api/SubjectApiController.java
+++ avni-server-api/src/main/java/org/avni/web/api/SubjectApiController.java
@@ -137,7 +137,9 @@ public class SubjectApiController {
         if (!addressLevel.isPresent()) {
             throw new IllegalArgumentException(String.format("Address '%s' not found", request.getAddress()));
         }
-        subject.setLegacyId(request.getExternalId().trim());
+        if (request.getExternalId() != null && !request.getExternalId().isEmpty()) {
+            subject.setLegacyId(request.getExternalId().trim());
+        }
         subject.setSubjectType(subjectType);
         subject.setFirstName(request.getFirstName());
         subject.setLastName(request.getLastName());
