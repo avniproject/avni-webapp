653d6808846a0ed800ef24ff88ed2f03314247bb
Vinod Deolal <vinoddeolal@gmail.com>
2022-08-04 18:25:01 +0530

    #0 | added new standard card types for the task

---
+++ avni-server-api/src/main/resources/db/migration/V1_244__AddStandardCardsForTaskTypes.sql
@@ -0,0 +1,13 @@
+insert into standard_report_card_type (uuid, name, description, created_date_time, last_modified_date_time)
+values ('3e287567-3b1d-450b-a877-d4f87a31ce83',
+        'Call tasks',
+        'Call tasks',
+        now(),
+        now());
+
+insert into standard_report_card_type (uuid, name, description, created_date_time, last_modified_date_time)
+values ('26176da9-ea2b-4469-9bd5-ac7639a64d81',
+        'Open subject tasks',
+        'Open subject tasks',
+        now(),
+        now());
