048484a423b2590e670f5f68e81c83f05177d316
Maha <maha@Mahas-MacBook-Pro.local>
2022-08-26 12:43:22 +0530

    #635 | Vinod/Maha | Returned proper error messages on Exotel call failure.

--- avni-server-api/src/main/java/org/avni/web/external/ExotelRestClient.java
+++ avni-server-api/src/main/java/org/avni/web/external/ExotelRestClient.java
@@ -1,6 +1,11 @@
 package org.avni.web.external;
 
+import com.bugsnag.Bugsnag;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.avni.domain.JsonObject;
+import org.avni.util.ObjectMapperSingleton;
 import org.avni.web.request.ExotelRequest;
 import org.avni.web.response.ExotelResponse;
 import org.slf4j.Logger;
@@ -10,9 +15,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.http.*;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.ConnectException;
+import java.util.Map;
 
 import static java.lang.String.format;
 
@@ -20,10 +27,14 @@ import static java.lang.String.format;
 public class ExotelRestClient {
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
     private final RestTemplate restTemplate;
+    private final ObjectMapper objectMapper;
+    private final Bugsnag bugsnag;
 
     @Autowired
-    public ExotelRestClient(RestTemplateBuilder builder) {
+    public ExotelRestClient(RestTemplateBuilder builder, Bugsnag bugsnag) {
         this.restTemplate = builder.build();
+        this.bugsnag = bugsnag;
+        this.objectMapper = ObjectMapperSingleton.getObjectMapper();
     }
 
     public ExotelResponse callMasking(JsonObject exotelConfig, ExotelRequest exotelRequest) throws ConnectException {
@@ -46,16 +57,39 @@ public class ExotelRestClient {
             ResponseEntity<String> exotelResponse = restTemplate.postForEntity(url.toString(), request, String.class);
             logger.info(format("Exotel Response: Response Code: %s", exotelResponse.getStatusCode()));
             logger.debug(format("Exotel Response Body: %s", exotelResponse.getBody()));
-
             if (exotelResponse.getStatusCode().equals(HttpStatus.OK)) {
-                return new ExotelResponse(true, "Masking call successful.");
-            } else {
-                logger.error(format("Error connecting for Request to Exotel: %s", exotelRequest));
-                logger.error(format("Exotel Error response: %s, %s", exotelResponse.getStatusCode(), exotelResponse.getBody()));
-                throw new ConnectException(format("Error connecting to Exotel. %s : %s", exotelResponse.getStatusCode(), exotelResponse.getBody()));
+                return new ExotelResponse(true, "Call masking successful.");
             }
+            else
+                return new ExotelResponse(false, "Call masking failed.");
+        } catch (HttpStatusCodeException e) {
+            String baseErrorMessage = "Error while connecting the call.";
+            if (e.getStatusCode() == HttpStatus.BAD_REQUEST) {
+                baseErrorMessage += "This could be because of incorrect phone number.";
+            } else {
+                baseErrorMessage = getErrorMessageString(e.getResponseBodyAsString(), baseErrorMessage);
+            }
+            bugsnag.notify(e);
+            e.printStackTrace();
+            throw new ConnectException(baseErrorMessage);
         } catch (Exception e) {
+            bugsnag.notify(e);
+            e.printStackTrace();
             throw new ConnectException(format("Error connecting to Exotel. %s", e.getMessage()));
         }
+    }
+
+    private String getErrorMessageString(String responseBody, String baseErrorMessage) {
+        if (responseBody != null) {
+            try {
+                Map<String, Object> responseMap = objectMapper.readValue(responseBody, new TypeReference<Map<String, Object>>() {});
+                Map<String, Object> restException = objectMapper.convertValue(responseMap.get("RestException"), new TypeReference<Map<String, Object>>() {});
+                String message = (String) restException.getOrDefault("Message", "");
+                return format("%s %s", baseErrorMessage, message);
+            } catch (JsonProcessingException e) {
+                return baseErrorMessage;
+            }
+        }
+        return baseErrorMessage;
     }
 }
