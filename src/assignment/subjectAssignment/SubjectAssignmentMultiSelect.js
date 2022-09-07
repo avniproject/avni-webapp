2a338c45656836d3b4fe6af311544a18bdb24b36
himeshr <himeshl@samanvayfoundation.org>
2022-09-07 16:22:30 +0530

    #793 | Himesh/maha | Sort dropdown list of users

--- src/assignment/subjectAssignment/SubjectAssignmentMultiSelect.js
+++ src/assignment/subjectAssignment/SubjectAssignmentMultiSelect.js
@@ -1,9 +1,20 @@
 import React, { useState } from "react";
 import ReactMultiSelectCheckboxes from "react-multiselect-checkboxes";
 import { updateUserAssignmentToSubject } from "./SubjectAssignmentData";
+import _ from "lodash";
 
 const SubjectAssignmentMultiSelect = props => {
-  const [selectedOptions, setSelectedOptions] = useState(props.selectedOptions);
+  const [selectedOptions, setSelectedOptions] = useState(
+    getSortedDropdownList(props.selectedOptions)
+  );
+
+  function getSortedDropdownList(unsortedList) {
+    return _.sortBy(unsortedList, [
+      function(o) {
+        return _.upperCase(o.label);
+      }
+    ]);
+  }
 
   function onChange(value, event) {
     updateUserAssignmentToSubject(event);
@@ -11,13 +22,23 @@ const SubjectAssignmentMultiSelect = props => {
     this.setState(value);
   }
 
+  function getDropdownButtonLabel({ placeholderButtonLabel, value }) {
+    if (value.length === 0) {
+      return `${placeholderButtonLabel}: None`;
+    }
+    return `${placeholderButtonLabel} ${value.length} users: ${value
+      .map(entry => entry.label)
+      .join(", ")} `.substring(0, 255);
+  }
+
   return (
     <ReactMultiSelectCheckboxes
-      options={props.options}
+      options={getSortedDropdownList(props.options)}
       placeholderButtonLabel="Assigned to"
       value={selectedOptions}
       onChange={onChange}
       setState={setSelectedOptions}
+      // getDropdownButtonLabel={getDropdownButtonLabel}
     />
   );
 };
