97484fe5ea87ffc3f3265cf4b4dffcf494a07738
Vinod Deolal <vinoddeolal@gmail.com>
2022-09-08 09:25:14 +0530

    Revert "Revert "#460 | API to execute the saved queries""
    This reverts commit c83e27c6bca463290962c87e9247d378fcf43001.

---
+++ avni-server-api/src/main/java/org/avni/web/CustomQueryController.java
@@ -0,0 +1,33 @@
+package org.avni.web;
+
+
+import org.avni.dao.QueryRepository;
+import org.avni.web.request.CustomQueryRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import javax.transaction.Transactional;
+
+@RestController
+public class CustomQueryController {
+
+    private final QueryRepository queryRepository;
+
+    @Autowired
+    public CustomQueryController(QueryRepository queryRepository) {
+        this.queryRepository = queryRepository;
+    }
+
+    @RequestMapping(name = "/executeQuery", method = RequestMethod.POST)
+    @PreAuthorize(value = "hasAnyAuthority('user')")
+    @ResponseBody
+    @Transactional
+    public ResponseEntity<?> executeQuery(@RequestBody CustomQueryRequest customQueryRequest) {
+        if (customQueryRequest.getName() == null) {
+            return ResponseEntity.badRequest().body("No query name passed in the request");
+        }
+        return queryRepository.runQuery(customQueryRequest);
+    }
+}
