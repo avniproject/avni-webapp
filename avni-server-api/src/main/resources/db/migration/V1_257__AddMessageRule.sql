6bbe5ba422f15e64aec125b3ea99c8fa165b05e2
Maha <maha@Mahas-MacBook-Pro.local>
2022-10-05 10:49:31 +0530

    #813 | Add migration to store message rules.

--- avni-server-api/src/main/resources/db/migration/V1_257__AddMessageRule.sql
+++ avni-server-api/src/main/resources/db/migration/V1_257__AddMessageRule.sql
@@ -1,11 +1,15 @@
-create table custom_query
+create table message_rule
 (
     id                      SERIAL PRIMARY KEY,
     uuid                    varchar(255)                NOT NULL,
     name                    text                        NOT NULL,
-    query                   text                        NOT NULL,
+    message_rule            text,
+    schedule_rule           text,
     organisation_id         integer                     NOT NULL references organisation (id),
     is_voided               boolean                     NOT NULL DEFAULT FALSE,
+    entity_type             text                        NOT NULL,
+    entity_type_id          text                        NOT NULL,
+    message_template_id     text                        NOT NULL,
     version                 integer                     not null,
     created_by_id           bigint                      not null references users (id),
     last_modified_by_id     bigint                      not null references users (id),
@@ -13,7 +17,7 @@ create table custom_query
     last_modified_date_time timestamp(3) with time zone not null
 );
 
-alter table custom_query
+alter table message_rule
     add unique (uuid, organisation_id);
 
-select enable_rls_on_tx_table('custom_query');
+select enable_rls_on_tx_table('message_rule');
