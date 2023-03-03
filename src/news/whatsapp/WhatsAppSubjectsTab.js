895645b7e4904e5811ee7c60e1b3dd74d246f14e
Vivek Singh <petmongrels@gmail.com>
2023-03-03 15:07:51 +0530

    #916 - extracted choosing of subject into a component

--- src/news/whatsapp/WhatsAppSubjectsTab.js
+++ src/news/whatsapp/WhatsAppSubjectsTab.js
@@ -1,10 +1,13 @@
-import React, {useState} from "react";
+import React from "react";
 import ChooseSubject from "./ChooseSubject";
 import _ from 'lodash';
 import {Box} from "@material-ui/core";
 
 export default function () {
   return <Box style={{marginLeft: 20}}>
-    <ChooseSubject onCancel={_.noop} onSubjectChosen={() => {}}/>
+    <ChooseSubject onCancel={_.noop}
+                   onSubjectChosen={() => {
+                   }}
+                   busy={false}/>
   </Box>;
 }
