fc288874a653c6956202246a8de645a69062b923
Sachin Kadam <Sachinkadamsk01@gmail.com>
2023-06-08 09:14:42 +0530

    #978 | Fix passing token in extensions url

--- src/dataEntryApp/views/subjectDashBoard/components/extension/ExtensionOption.js
+++ src/dataEntryApp/views/subjectDashBoard/components/extension/ExtensionOption.js
@@ -55,7 +55,7 @@ export const ExtensionOption = ({
   const serverURL = isDevEnv ? "http://localhost:8021" : window.location.origin;
 
   const clickHandler = async fileName => {
-    const token = httpClient.idp.getTokenParam();
+    const token = await httpClient.idp.getTokenParam();
     const params = `subjectUUIDs=${subjectUUIDs}&${token}`;
     window.open(`${serverURL}/extension/${fileName}?${params}`, "_blank");
   };
