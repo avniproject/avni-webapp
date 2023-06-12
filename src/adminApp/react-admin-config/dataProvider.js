00f8e8849a42b37eb8f745fdcf874482e90d64cd
Vivek Singh <petmongrels@gmail.com>
2023-06-12 18:47:34 +0530

    #48 - if etl service is down then display no value. generalised to org and org group data provider. navigation/busy indicator.

--- src/adminApp/react-admin-config/dataProvider.js
+++ src/adminApp/react-admin-config/dataProvider.js
@@ -13,7 +13,7 @@ import {
 import { UrlPartsGenerator } from "./requestUtils";
 import SpringResponse from "./SpringResponse";
 import { httpClient } from "../../common/utils/httpClient";
-import OrganisationDataProvider from "./resourceSpecificProviders/OrganisationDataProvider";
+import OrganisationAndOrgGroupDataProvider from "./resourceSpecificProviders/OrganisationAndOrgGroupDataProvider";
 
 /**
  * Maps react-admin queries to a simple REST API
@@ -53,7 +53,8 @@ const urlMapping = {
 };
 
 const resourceSpecificProviders = {
-  organisation: OrganisationDataProvider
+  organisation: OrganisationAndOrgGroupDataProvider,
+  organisationGroup: OrganisationAndOrgGroupDataProvider
 };
 
 export default apiUrl => {
@@ -167,7 +168,7 @@ export default apiUrl => {
       .then(response => convertHTTPResponse(response, type, resource, params));
     const resourceSpecificProvider = resourceSpecificProviders[resource];
     if (resourceSpecificProvider && resourceSpecificProvider.supportsOperation(type))
-      return resourceSpecificProvider.execute(type, params, promise);
+      return resourceSpecificProvider.execute(type, params, resource, promise);
     return promise;
   };
 };
--- src/adminApp/react-admin-config/resourceSpecificProviders/OrganisationAndOrgGroupDataProvider.js
+++ src/adminApp/react-admin-config/resourceSpecificProviders/OrganisationAndOrgGroupDataProvider.js
@@ -2,36 +2,34 @@ import { GET_LIST, GET_ONE } from "react-admin";
 import CollectionUtil from "../../../common/utils/CollectionUtil";
 import EtlJobService from "../../service/etl/EtlJobService";
 
-class OrganisationDataProvider {
+class OrganisationAndOrgGroupDataProvider {
   static SupportedOperations = [GET_LIST, GET_ONE];
 
   static supportsOperation(operation) {
     return this.SupportedOperations.includes(operation);
   }
 
-  static execute(type, params, resourcePromise) {
+  static execute(type, params, resource, resourcePromise) {
     if (type === GET_LIST) {
       return resourcePromise.then(raListResponse => {
-        const organisationUUIDs = raListResponse.data.map(x => x.uuid);
-        return EtlJobService.getJobStatuses(organisationUUIDs).then(organisationJobStatuses => {
-          const map = CollectionUtil.toObject(
-            organisationJobStatuses.data,
-            "organisationUUID",
-            "analyticsEnabled"
-          );
+        const entityUUIDs = raListResponse.data.map(x => x.uuid);
+        return EtlJobService.getJobStatuses(entityUUIDs).then(jobStatuses => {
+          const map = CollectionUtil.toObject(jobStatuses.data, "entityUUID", "analyticsEnabled");
           raListResponse.data.forEach(x => (x["analyticsDataSyncActive"] = map[x["uuid"]]));
           return raListResponse;
         });
       });
     } else if (type === GET_ONE) {
       return resourcePromise.then(raResource => {
-        return EtlJobService.getJob(raResource.data.uuid).then(jobSummary => {
-          raResource.data["analyticsDataSyncActive"] = jobSummary != null;
-          return raResource;
-        });
+        return EtlJobService.getJob(raResource.data.uuid, resource)
+          .then(jobSummary => {
+            raResource.data["analyticsDataSyncActive"] = jobSummary != null;
+            return raResource;
+          })
+          .catch(() => raResource);
       });
     }
   }
 }
 
-export default OrganisationDataProvider;
+export default OrganisationAndOrgGroupDataProvider;
