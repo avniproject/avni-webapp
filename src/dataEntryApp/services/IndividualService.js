61293c66477a5abbe08301c854fa98c193e3ce5b
Suhas Vishwanath <suhasv@samanvayfoundation.org>
2023-06-21 13:57:08 +0530

    #1033 | Introduce dummy method IndividualService.getSubjectByUUIDAndType

--- src/dataEntryApp/services/IndividualService.js
+++ src/dataEntryApp/services/IndividualService.js
@@ -2,6 +2,10 @@ class IndividualService {
   getSubjectsInLocation(addressLevel, subjectTypeName) {
     throw Error("getSubjectsInLocation method is not supported for DEA");
   }
+
+  getSubjectByUUIDAndType(uuid, subjectTypeName) {
+    throw Error("getSubjectByUUIDAndType method is not supported for DEA");
+  }
 }
 
 export const individualService = new IndividualService();
