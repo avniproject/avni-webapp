b983511cd34a418abfb67f714a02e4a8002085c2
Suhas Vishwanath <suhasv@samanvayfoundation.org>
2023-07-20 17:00:21 +0530

    #887 | Visibility of cards on home page depends on privileges assigned to current user

--- src/rootApp/views/Homepage.js
+++ src/rootApp/views/Homepage.js
@@ -15,6 +15,8 @@ const Homepage = ({ user }) => {
   useEffect(() => {
     const fetchOrgID = async () => {
       const resp = await axios("/web/userInfo");
+      // resp.data.hasAllPrivileges = false; //Uncomment for testing
+      // resp.data.privileges = [Privilege.PrivilegeType.EditOrganisationConfiguration]; //Uncomment for testing
       setUserData(resp.data);
     };
     fetchOrgID();
@@ -23,13 +25,24 @@ const Homepage = ({ user }) => {
     Privilege.PrivilegeType.EditTask,
     Privilege.PrivilegeType.DeleteTask
   ]);
+  const canEditOrganisationConfig = CurrentUserService.isAllowedToAccess(userData, [
+    Privilege.PrivilegeType.EditOrganisationConfiguration
+  ]);
+  const showDocumentation = CurrentUserService.isAllowedToAccess(userData, [
+    Privilege.PrivilegeType.EditDocumentation
+  ]);
+  const showReports = CurrentUserService.isAllowedToAccess(userData, [
+    Privilege.PrivilegeType.Reports
+  ]);
 
   return (
     <ScreenWithAppBar appbarTitle={"Avni Web Console"}>
       <Grid container justify="center">
         <HomePageCard href={"/#/admin/user"} name={"Admin"} customIcon={"supervisor_account"} />
         <HomePageCard href={"/#/appdesigner"} name={"App Designer"} customIcon={"architecture"} />
-        <HomePageCard href={"/#/documentation"} name={"Documentation"} customIcon={"article"} />
+        {showDocumentation && (
+          <HomePageCard href={"/#/documentation"} name={"Documentation"} customIcon={"article"} />
+        )}
         {showAssignment && (
           <HomePageCard
             href={"/#/assignment"}
@@ -37,13 +50,19 @@ const Homepage = ({ user }) => {
             customIcon={"assignment_turned_in"}
           />
         )}
-        <HomePageCard
-          href={"/#/broadcast"}
-          name={"Broadcast"}
-          customIconComponent={<SurroundSound color="primary" style={{ fontSize: 100 }} />}
-        />
-        <HomePageCard href={"/#/translations"} name={"Translations"} customIcon={"translate"} />
-        <HomePageCard href={"/#/export"} name={"Reports"} customIcon={"assessment"} />
+        {canEditOrganisationConfig && (
+          <HomePageCard
+            href={"/#/broadcast"}
+            name={"Broadcast"}
+            customIconComponent={<SurroundSound color="primary" style={{ fontSize: 100 }} />}
+          />
+        )}
+        {canEditOrganisationConfig && (
+          <HomePageCard href={"/#/translations"} name={"Translations"} customIcon={"translate"} />
+        )}
+        {showReports && (
+          <HomePageCard href={"/#/export"} name={"Reports"} customIcon={"assessment"} />
+        )}
         <HomePageCard href={"/#/app"} name={"Data Entry App"} customIcon={"keyboard"} />
         <HomePageCard
           href={`/avni-media?orgID=${userData && userData.organisationId}&username=${userData &&
