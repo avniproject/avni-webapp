d1cff9b61328f89622ca6ec18917c102ccc8266b
Suhas Vishwanath <suhasv@samanvayfoundation.org>
2023-08-07 12:09:30 +0530

    #984 | EditForm privilege check is not needed

--- src/formDesigner/views/Forms.js
+++ src/formDesigner/views/Forms.js
@@ -9,62 +9,43 @@ import FormListing from "../components/FormListing";
 import Box from "@material-ui/core/Box";
 import { Title } from "react-admin";
 import { CreateComponent } from "../../common/components/CreateComponent";
-import { connect } from "react-redux";
-import UserInfo from "../../common/model/UserInfo";
-import { Privilege } from "openchs-models";
 
 const Forms = props => {
   const [showNewFormDialog, setShowNewFormDialog] = useState(false);
   const openNewFormDialog = () => setShowNewFormDialog(true);
   const closeNewFormDialog = () => setShowNewFormDialog(false);
 
-  const canEditForms = UserInfo.hasPrivilege(props.userInfo, Privilege.PrivilegeType.EditForm);
-  console.log(
-    "Can edit forms",
-    canEditForms,
-    "User privilege",
-    props.userInfo,
-    "Need",
-    Privilege.PrivilegeType.EditForm
-  );
   return (
     <Box boxShadow={2} p={3} bgcolor="background.paper">
       <Title title="Forms" />
       <div className="container">
         <div>
-          {canEditForms && (
-            <div style={{ float: "right", right: "50px", marginTop: "15px" }}>
-              <CreateComponent onSubmit={openNewFormDialog} name="New Form" />
-            </div>
-          )}
-          {canEditForms && (
-            <Dialog
-              fullWidth
-              maxWidth="xs"
-              onClose={closeNewFormDialog}
-              aria-labelledby="customized-dialog-title"
-              open={showNewFormDialog}
-            >
-              <DialogTitle id="customized-dialog-title" onClose={closeNewFormDialog}>
-                New Form
-                <IconButton style={{ float: "right" }} onClick={closeNewFormDialog}>
-                  <CloseIcon />
-                </IconButton>
-              </DialogTitle>
+          <div style={{ float: "right", right: "50px", marginTop: "15px" }}>
+            <CreateComponent onSubmit={openNewFormDialog} name="New Form" />
+          </div>
+          <Dialog
+            fullWidth
+            maxWidth="xs"
+            onClose={closeNewFormDialog}
+            aria-labelledby="customized-dialog-title"
+            open={showNewFormDialog}
+          >
+            <DialogTitle id="customized-dialog-title" onClose={closeNewFormDialog}>
+              New Form
+              <IconButton style={{ float: "right" }} onClick={closeNewFormDialog}>
+                <CloseIcon />
+              </IconButton>
+            </DialogTitle>
 
-              <DialogContent dividers>
-                <NewFormModal {...props} />
-              </DialogContent>
-            </Dialog>
-          )}
+            <DialogContent dividers>
+              <NewFormModal {...props} />
+            </DialogContent>
+          </Dialog>
           <FormListing />
         </div>
       </div>
     </Box>
   );
 };
-const mapStateToProps = state => ({
-  userInfo: state.app.userInfo
-});
 
-export default connect(mapStateToProps)(Forms);
+export default Forms;
