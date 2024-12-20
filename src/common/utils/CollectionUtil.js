85da8cc0c18584de40a81cc791dca3219e0aef97
himeshr <himeshl@samanvayfoundation.org>
2024-12-20 11:53:34 +0530

    #1374 | Make use of sorted array before performing switch operations Remove unnecessary iteration of array and validate indexes are within range

--- src/common/utils/CollectionUtil.js
+++ src/common/utils/CollectionUtil.js
@@ -13,24 +13,14 @@ class CollectionUtil {
   }
 
   static switchItemPosition(array, startIndex, endIndex, fieldToUpdate) {
-    const workingItems = [...array];
-    let removedElement;
-    array.forEach((mapping, index) => {
-      if (index === startIndex) {
-        removedElement = _.pullAt(workingItems, index)[0];
-      }
-    });
-
-    array.forEach((mapping, index) => {
-      if (index === endIndex) {
-        workingItems.splice(index, 0, removedElement);
-      }
-    });
-
+    const workingItems = _.sortBy(array, [fieldToUpdate]);
+    if (_.inRange(startIndex, 0, array.length) && _.inRange(endIndex, 0, array.length)) {
+      let removedElement = _.pullAt(workingItems, startIndex)[0];
+      workingItems.splice(endIndex, 0, removedElement);
+    }
     workingItems.forEach((item, index) => {
       item[fieldToUpdate] = index + 1;
     });
-
     array.splice(0, array.length, ...workingItems);
   }
 }
