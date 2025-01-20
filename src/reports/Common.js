2b0a7eaede7bb4a3e6fda9f96b93081b806f0e06
Joy A <itsjoy@gmail.com>
2025-01-20 12:55:49 +0530

    #1414 | Merge - Hide self service reports - fix unused import

--- src/reports/Common.js
+++ src/reports/Common.js
@@ -2,7 +2,7 @@ import PlayForWorkIcon from "@material-ui/icons/PlayForWork";
 
 export const reportSideBarOptions = [
   { href: "#/export", name: "Longitudinal Export", Icon: PlayForWorkIcon },
-  { href: "#/newExport", name: "New Longitudinal Export", Icon: PlayForWorkIcon },
+  { href: "#/newExport", name: "New Longitudinal Export", Icon: PlayForWorkIcon }
   // { href: "#/cannedreports", name: "Canned Reports", Icon: AssessmentIcon },
-  { href: "#/selfservicereports", name: "Self-service Reports", Icon: ExploreIcon }
+  // { href: "#/selfservicereports", name: "Self-service Reports", Icon: ExploreIcon }
 ];
