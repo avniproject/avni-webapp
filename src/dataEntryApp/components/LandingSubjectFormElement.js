e92c390c11892dbbaa53a543d24656aedadead91
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2025-03-05 18:15:17 +0530

    #1342 | (1)Removed the search option (2) Only in forms linked to Group and Household subject type 'Display all group members' is shown (3) AttendanceFormElement is displayed when displayAllGroupMembers set to true and SubjectFormElement by default

--- src/dataEntryApp/components/LandingSubjectFormElement.js
+++ src/dataEntryApp/components/LandingSubjectFormElement.js
@@ -4,14 +4,11 @@ import AttendanceFormElement from "./AttendanceFormElement";
 import SubjectFormElement from "./SubjectFormElement";
 
 const LandingSubjectFormElement = props => {
-  const displayAllGroupMembers = props.formElement.recordValueByKey(
-    FormElement.keys.displayAllGroupMembers
-  );
-  const searchOptions = props.formElement.recordValueByKey(FormElement.keys.searchOptions);
-  return searchOptions ? (
-    <SubjectFormElement {...props} />
-  ) : (
+  const displayAllGroupMembers = props.formElement.recordValueByKey(FormElement.keys.displayAllGroupMembers);
+  return displayAllGroupMembers ? (
     <AttendanceFormElement displayAllGroupMembers={displayAllGroupMembers} {...props} />
+  ) : (
+    <SubjectFormElement {...props} />
   );
 };
 
