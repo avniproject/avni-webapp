fde1e887b8771033417fa281e0b95b74e0831e37
Leo-Stephen <leostephen.maduri@gmail.com>
2025-04-16 22:32:06 +0530

    Fix: Catchment dropdown floating issue by using SelectInput

--- src/adminApp/components/CatchmentSelectInput.js
+++ src/adminApp/components/CatchmentSelectInput.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { AutocompleteInput } from "react-admin";
+import { SelectInput } from "react-admin";
 
 export const CatchmentSelectInput = props => {
-  return <AutocompleteInput {...props} choices={props.choices} optionText="name" />;
+  return <SelectInput {...props} choices={props.choices} optionText="name" />;
 };
