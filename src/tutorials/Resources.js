c24d33167f398b9ecca26903fa130e2b4765a67e
Joy A <itsjoy@gmail.com>
2025-06-12 17:29:07 +0530

    #0 | Change in-app community link from gitter to discord

--- src/tutorials/Resources.js
+++ src/tutorials/Resources.js
@@ -18,9 +18,7 @@ const Resources = () => {
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <TutorialCard
           title={"Self service help articles"}
-          content={
-            "Are you trying out Avni for the first time? Here's all the information you'll need"
-          }
+          content={"Are you trying out Avni for the first time? Here's all the information you'll need"}
           href={"https://avni.readme.io/docs/implementers-concept-guide-introduction"}
           iconComponent={<Description color="primary" style={{ fontSize: 60, color: "#009688" }} />}
         />
@@ -28,13 +26,9 @@ const Resources = () => {
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <TutorialCard
           title={"Avni youtube channel"}
-          content={
-            "Check out our youtube channel for the latest tutorials. We cover everything you could need to get started"
-          }
+          content={"Check out our youtube channel for the latest tutorials. We cover everything you could need to get started"}
           href={"https://www.youtube.com/channel/UCShsfKJlw0B3B6Pg2DmQkSQ"}
-          iconComponent={
-            <VideoLibrary color="primary" style={{ fontSize: 60, color: "#d50000" }} />
-          }
+          iconComponent={<VideoLibrary color="primary" style={{ fontSize: 60, color: "#d50000" }} />}
         />
       </Grid>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
@@ -49,22 +43,16 @@ const Resources = () => {
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <TutorialCard
           title={"Submit a support ticket"}
-          content={
-            "Can't find answer in our help article? Click here to submit a ticket to the Avni support team"
-          }
+          content={"Can't find answer in our help article? Click here to submit a ticket to the Avni support team"}
           href={"https://avni.freshdesk.com/support/home"}
-          iconComponent={
-            <ConfirmationNumber color="primary" style={{ fontSize: 60, color: "#66bb6a" }} />
-          }
+          iconComponent={<ConfirmationNumber color="primary" style={{ fontSize: 60, color: "#66bb6a" }} />}
         />
       </Grid>
       <Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
         <TutorialCard
           title={"Ask the community"}
-          content={
-            "Ask question, post tips and even answer each other's question through community forums"
-          }
-          href={"https://gitter.im/avniproject/avni"}
+          content={"Ask question, post tips and even answer each other's question through community forums"}
+          href={"https://discord.gg/4pcgcQW8pk"}
           iconComponent={<Forum color="primary" style={{ fontSize: 60, color: "#ad1457" }} />}
         />
       </Grid>
