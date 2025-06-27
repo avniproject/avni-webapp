55572926d6f283466f534667df3f391579d7188d
himeshr <himeshl@samanvayfoundation.org>
2025-06-27 19:33:09 +0530

    #1515 | modify notification to come at the top for clear view

--- src/common/components/AdminLayout.js
+++ src/common/components/AdminLayout.js
@@ -1,13 +1,22 @@
 import React from "react";
-import { Layout } from "react-admin";
+import { Layout, Notification } from "react-admin";
 import AdminAppBar from "./AdminAppBar";
-import { Notification } from "react-admin";
+import { Portal } from "@material-ui/core";
 
+// Simple notification component with Portal for highest z-index
 const AdminNotification = props => (
-  <Notification {...props} autoHideDuration={5000} style={{ marginBottom: "40px" }} />
+  <Portal>
+    <div
+      style={{
+        marginBottom: "60px",
+        zIndex: 9999
+      }}
+    >
+      <Notification {...props} autoHideDuration={5000} style={{ marginBottom: "60px" }} />
+    </div>
+  </Portal>
 );
-const AdminLayout = props => (
-  <Layout {...props} appBar={AdminAppBar} notification={AdminNotification} />
-);
+
+const AdminLayout = props => <Layout {...props} appBar={AdminAppBar} notification={AdminNotification} />;
 
 export default AdminLayout;
