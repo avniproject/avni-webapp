6a88ee0f85b3999c220f1238e9425d6c1fc6110c
Om Bhardwaj <115864495+ombhardwajj@users.noreply.github.com>
2025-07-16 13:02:50 +0530

    #1506 | Upgrade from MUI v6 to v7 (Phase 3) - Fixed RuleSummary.js

--- src/dataEntryApp/views/subjectDashBoard/components/RuleSummary.js
+++ src/dataEntryApp/views/subjectDashBoard/components/RuleSummary.js
@@ -1,16 +1,14 @@
 import { styled } from "@mui/material/styles";
-import { Accordion, AccordionSummary, AccordionDetails, Typography, Box } from "@mui/material";
+import { Accordion, AccordionSummary, AccordionDetails, Typography, CircularProgress, Grid } from "@mui/material";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import Observations from "dataEntryApp/components/Observations";
-import ScheduledVisitsTable from "dataEntryApp/components/ScheduledVisitsTable";
 import { useTranslation } from "react-i18next";
 import { isEmpty } from "lodash";
-import CustomizedBackdrop from "dataEntryApp/components/CustomizedBackdrop";
 
 const StyledAccordion = styled(Accordion)({
   marginBottom: "11px",
   borderRadius: "5px",
-  boxShadow: "0px 0px 3px 0px rgba(0,0,0,0.4), 0px 1px 1px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)"
+  boxShadow: "0px 0px 3px 1px rgba(0,0,0,0.2), 0px 1px 2px 0px rgba(0,0,0,0.14), 0px 2px 1px -1px rgba(0,0,0,0.12)"
 });
 
 const StyledAccordionSummary = styled(AccordionSummary)({
@@ -19,75 +17,41 @@ const StyledAccordionSummary = styled(AccordionSummary)({
   }
 });
 
-const StyledAccordionDetails = styled(AccordionDetails)({
-  padding: "16px",
-  display: "block"
-});
-
-const StyledHeading = styled(Typography)(({ theme }) => ({
-  fontSize: theme.typography.pxToRem(16),
-  fontWeight: 500,
+const StyledAccordionSummaryTitle = styled("p")({
+  fontSize: "1rem",
   flexBasis: "33.33%",
   flexShrink: 0,
+  fontWeight: 500,
   margin: 0
-}));
-
-const StyledBox = styled(Box)({
-  marginBottom: "16px"
 });
 
-const RuleSummary = ({ isFetching, title, summaryObservations }) => {
+const RuleSummary = ({ isFetching, title, summaryObservations = [] }) => {
   const { t } = useTranslation();
 
-  if (isFetching) {
-    return <CustomizedBackdrop load={false} />;
-  }
+  const renderNotFound = () => <Typography component="span">{t("summaryNotFound")}</Typography>;
 
-  const noContent =
-    isEmpty(summaryObservations?.decisionObservations) &&
-    isEmpty(summaryObservations?.visitSchedules) &&
-    isEmpty(summaryObservations?.observations);
+  const renderObs = () => (
+    <Grid item xs={12}>
+      {isEmpty(summaryObservations) ? renderNotFound() : <Observations observations={summaryObservations} />}
+    </Grid>
+  );
+
+  const renderProgress = () => (
+    <Grid item container xs={12} justifyContent="center">
+      <CircularProgress />
+    </Grid>
+  );
 
   return (
     <StyledAccordion>
       <StyledAccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls="registrationPanelbh-content" id="summary">
-        <StyledHeading component="span">{t(title)}</StyledHeading>
+        <Typography component="span">
+          <StyledAccordionSummaryTitle>{t(title)}</StyledAccordionSummaryTitle>
+        </Typography>
       </StyledAccordionSummary>
-
-      <StyledAccordionDetails>
-        {!isEmpty(summaryObservations?.decisionObservations) && (
-          <StyledBox>
-            <Typography variant="button" display="block" gutterBottom>
-              {t("systemRecommendations")}
-            </Typography>
-            <Observations observations={summaryObservations.decisionObservations} additionalRows={[]} highlight />
-          </StyledBox>
-        )}
-
-        {!isEmpty(summaryObservations?.visitSchedules) && (
-          <StyledBox>
-            <Typography variant="button" display="block" gutterBottom>
-              {t("visitsBeingScheduled")}
-            </Typography>
-            <ScheduledVisitsTable visitSchedules={summaryObservations.visitSchedules} />
-          </StyledBox>
-        )}
-
-        {!isEmpty(summaryObservations?.observations) && (
-          <StyledBox>
-            <Typography variant="button" display="block" gutterBottom>
-              {t("observations")}
-            </Typography>
-            <Observations observations={summaryObservations.observations} additionalRows={[]} form={null} />
-          </StyledBox>
-        )}
-
-        {noContent && (
-          <StyledBox>
-            <Typography component="span">{t("summaryNotFound")}</Typography>
-          </StyledBox>
-        )}
-      </StyledAccordionDetails>
+      <AccordionDetails>
+        <Grid>{isFetching ? renderProgress() : renderObs()}</Grid>
+      </AccordionDetails>
     </StyledAccordion>
   );
 };
