b691ef72194778080564bd09d72157db40429514
prit007p <pritbaldaniya@gmail.com>
2025-10-01 20:20:17 +0530

    Update ReportCardList.jsx

--- src/formDesigner/components/ReportCard/ReportCardList.jsx
+++ src/formDesigner/components/ReportCard/ReportCardList.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { Privilege } from "openchs-models";
 import { get } from "lodash";
-import {useNavigate} from "react-router-dom";
 
 const ReportCardList = () => {
 const navigate = useNavigate();
